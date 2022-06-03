f8ab897154cc9681d80f36a944b3397c3e7be5f8
Hendrik Mans <hendrik@mans.de>
2022-06-04 00:53:10 +0200

    Cleanup

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <p>moo</p>;
+  return <p>moo</p>
 }
 
-export default App;
+export default App
