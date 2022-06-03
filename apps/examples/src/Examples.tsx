248dd7eacb73b7c6aac8d7a8b21a50ce8e2700a9
Hendrik Mans <hendrik@mans.de>
2022-06-04 00:55:19 +0200

    Add r3f

--- apps/examples/src/Examples.tsx
+++ apps/examples/src/Examples.tsx
@@ -1,1 +1,14 @@
-export default () => <p>moo</p>
+import { OrbitControls } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+
+export default () => (
+  <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <mesh>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color="red" />
+    </mesh>
+    <OrbitControls />
+  </Canvas>
+)
