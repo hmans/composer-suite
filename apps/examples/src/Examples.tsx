60c5ac64e5efede44ccd85534826ada285099373
Hendrik Mans <hendrik@mans.de>
2022-06-04 01:12:34 +0200

    Rotate the cube

--- apps/examples/src/Examples.tsx
+++ apps/examples/src/Examples.tsx
@@ -1,14 +1,29 @@
 import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+import { Mesh } from "three"
+
+const Thingy = () => {
+  const mesh = useRef<Mesh>(null!)
+
+  useFrame((_, dt) => {
+    mesh.current.rotation.x += 0.1 * dt
+    mesh.current.rotation.y += 0.2 * dt
+  })
+
+  return (
+    <mesh ref={mesh} scale={3}>
+      <boxBufferGeometry />
+      <meshStandardMaterial color="#333" />
+    </mesh>
+  )
+}
 
 export default () => (
   <Canvas>
     <ambientLight intensity={0.2} />
     <pointLight position={[10, 10, 10]} intensity={0.8} />
-    <mesh scale={3}>
-      <boxBufferGeometry />
-      <meshStandardMaterial color="#333" />
-    </mesh>
+    <Thingy />
     <OrbitControls />
   </Canvas>
 )
