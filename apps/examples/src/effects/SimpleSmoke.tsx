e6e3864e400408a3ca505330638eec16e101aad0
Hendrik Mans <hendrik@mans.de>
2022-06-04 21:25:40 +0200

    Eh?

--- apps/examples/src/effects/SimpleSmoke.tsx
+++ apps/examples/src/effects/SimpleSmoke.tsx
@@ -2,22 +2,14 @@ import {
   Emitter,
   MeshParticles,
   MeshParticlesProps,
-  ParticlesAPI,
   ParticlesMaterial
 } from "@hmans/vfx"
-import { useRef } from "react"
 
-export default (props: MeshParticlesProps) => {
-  const ref = useRef<ParticlesAPI>(null!)
+export default (props: MeshParticlesProps) => (
+  <MeshParticles {...props} scale={0.2}>
+    <ParticlesMaterial color="white" />
+    <sphereBufferGeometry args={[1, 8, 8]} />
 
-  return (
-    <>
-      <MeshParticles {...props} scale={0.2} ref={ref}>
-        <ParticlesMaterial color="white" />
-        <sphereBufferGeometry args={[1, 8, 8]} />
-
-        <Emitter spawnCount={3} burstCount={10} burstDelay={0.025} />
-      </MeshParticles>
-    </>
-  )
-}
+    <Emitter spawnCount={3} burstCount={10} burstDelay={0.025} />
+  </MeshParticles>
+)
