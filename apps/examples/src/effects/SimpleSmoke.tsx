de6e6b783be484b21b2e8d933cb759e0b7ff3faf
Hendrik Mans <hendrik@mans.de>
2022-06-05 00:27:13 +0200

    Dirt

--- apps/examples/src/effects/SimpleSmoke.tsx
+++ apps/examples/src/effects/SimpleSmoke.tsx
@@ -3,7 +3,7 @@ import { GroupProps } from "@react-three/fiber"
 import { between, plusMinus } from "randomish"
 import { Vector3 } from "three"
 
-const gravity = new Vector3(0, -9.81, 0)
+const gravity = new Vector3(0, -20, 0)
 const direction = new Vector3()
 
 const SmokeRing = ({ delay = 0 }) => (
@@ -29,6 +29,36 @@ const SmokeRing = ({ delay = 0 }) => (
 
         scaleStart.setScalar(1 + plusMinus(0.3))
         scaleEnd.setScalar(0)
+      }}
+    />
+  </MeshParticles>
+)
+
+const Dirt = ({ delay = 0 }) => (
+  <MeshParticles>
+    <boxGeometry />
+    <ParticlesMaterial color="#444" />
+
+    <Emitter
+      initialDelay={delay}
+      spawnCount={() => between(50, 100)}
+      burstCount={5}
+      burstDelay={0.025}
+      setup={({ position, velocity, acceleration, scaleStart, scaleEnd }) => {
+        direction
+          .set(1, 0.5, 0)
+          .applyAxisAngle(new Vector3(0, 1, 0), between(0, Math.PI * 2))
+
+        position.copy(direction).multiplyScalar(3 + plusMinus(0.2))
+
+        velocity
+          .copy(direction)
+          .multiplyScalar(10 + Math.pow(Math.random(), 3) * 10)
+
+        acceleration.copy(gravity)
+
+        scaleStart.setScalar(between(0.2, 0.4))
+        scaleEnd.copy(scaleStart)
       }}
     />
   </MeshParticles>
@@ -88,6 +118,7 @@ const SmokeCloud = ({ delay = 0 }) => (
 const SimpleSmoke = (props: GroupProps) => (
   <group {...props} scale={0.2}>
     <SmokeRing />
+    <Dirt />
     <Fire />
     <SmokeCloud delay={0.1} />
   </group>
