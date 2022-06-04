5ec080fc19ee7bb1b5e3a2c89a55b08aad6f65b7
Hendrik Mans <hendrik@mans.de>
2022-06-04 21:38:55 +0200

    Nicer effect

--- apps/examples/src/effects/SimpleSmoke.tsx
+++ apps/examples/src/effects/SimpleSmoke.tsx
@@ -1,15 +1,27 @@
-import {
-  Emitter,
-  MeshParticles,
-  MeshParticlesProps,
-  ParticlesMaterial
-} from "@hmans/vfx"
+import { Emitter, MeshParticles, ParticlesMaterial } from "@hmans/vfx"
+import { GroupProps } from "@react-three/fiber"
 
-export default (props: MeshParticlesProps) => (
-  <MeshParticles {...props} scale={0.2}>
-    <ParticlesMaterial color="white" />
-    <sphereBufferGeometry args={[1, 8, 8]} />
+export default (props: GroupProps) => (
+  <group {...props} scale={0.2}>
+    {/* Fire */}
+    <MeshParticles scale={0.5}>
+      <boxGeometry />
+      <ParticlesMaterial color="red" />
 
-    <Emitter spawnCount={3} burstCount={10} burstDelay={0.025} />
-  </MeshParticles>
+      <Emitter spawnCount={10} burstCount={10} burstDelay={0.01} />
+    </MeshParticles>
+
+    {/* Smoke */}
+    <MeshParticles>
+      <sphereBufferGeometry args={[1, 8, 8]} />
+      <ParticlesMaterial color="white" />
+
+      <Emitter
+        initialDelay={0.1}
+        spawnCount={3}
+        burstCount={10}
+        burstDelay={0.025}
+      />
+    </MeshParticles>
+  </group>
 )
