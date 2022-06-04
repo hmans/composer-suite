55015bb8c2a6ad099bb5140b20c3f8699068d99f
Hendrik Mans <hendrik@mans.de>
2022-06-04 17:58:53 +0200

    Cleanup

--- packages/vfx/src/ParticlesMaterial.tsx
+++ packages/vfx/src/ParticlesMaterial.tsx
@@ -4,6 +4,13 @@ import CustomShaderMaterial from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 import * as shader from "./shader"
 
+/*
+This is a custom material that is derived from MeshStandardMaterial,
+but with our custom vertex and fragment shader code injected.
+This is extremely and very WIP, since not all mesh particles will be using
+this kind of material.
+*/
+
 export const ParticlesMaterial = forwardRef<CustomShaderMaterialImpl, any>(
   (props, ref) => {
     return (
