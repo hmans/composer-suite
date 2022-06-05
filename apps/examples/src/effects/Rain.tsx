1862a17f98c34e0caad86df9dfea8afc66d15d04
Hendrik Mans <hendrik@mans.de>
2022-06-05 14:06:12 +0200

    Use meshparticles

--- apps/examples/src/effects/Rain.tsx
+++ apps/examples/src/effects/Rain.tsx
@@ -1,8 +1,20 @@
+import { between } from "randomish"
+import { Emitter, MeshParticles, ParticlesMaterial } from "vfx"
+
 export default () => {
   return (
-    <points>
-      <dodecahedronGeometry args={[15]} />
-      <pointsMaterial color="#0f0" />
-    </points>
+    <MeshParticles>
+      <dodecahedronBufferGeometry />
+      <ParticlesMaterial color="#fff" />
+
+      <Emitter
+        spawnCount={100}
+        setup={(c) => {
+          c.position.randomDirection().multiplyScalar(between(0, 30))
+          c.alphaEnd = 1
+          c.lifetime = 60
+        }}
+      />
+    </MeshParticles>
   )
 }
