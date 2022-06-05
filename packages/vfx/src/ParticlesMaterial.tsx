2d80b946e565e07611cb2051d6e69e7f7750897f
Hendrik Mans <hendrik@mans.de>
2022-06-05 20:55:55 +0200

    Remove obsolete warning

--- packages/vfx/src/ParticlesMaterial.tsx
+++ packages/vfx/src/ParticlesMaterial.tsx
@@ -5,13 +5,6 @@ import { iCSMProps } from "three-custom-shader-material/types"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 import * as shader from "./shader"
 
-/*
-This is a custom material that is derived from MeshStandardMaterial,
-but with our custom vertex and fragment shader code injected.
-This is extremely and very WIP, since not all mesh particles will be using
-this kind of material.
-*/
-
 type ParticlesMaterialProps = Omit<iCSMProps, "ref"> & {
   billboard?: boolean
 }
