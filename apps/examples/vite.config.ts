32e44992cdf97b335bfa8f02b074c2a3c4360aaa
Hendrik Mans <hendrik@mans.de>
2022-06-06 09:50:53 +0200

    Exclude drei in vite optimizations to enable local development

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: { exclude: ["vfx"], include: ["react/jsx-runtime"] }
+  optimizeDeps: {
+    exclude: ["vfx", "@react-three/drei"],
+    include: ["react/jsx-runtime"]
+  }
 })
