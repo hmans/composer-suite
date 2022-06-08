55f1d9aa56cba5550c621c94727a63ace8ed4289
Hendrik Mans <hendrik@mans.de>
2022-06-08 17:40:16 +0200

    Tweak vite config

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["vfx", "@react-three/drei"],
-    include: ["react/jsx-runtime"]
-  }
+  plugins: [react()]
 })
