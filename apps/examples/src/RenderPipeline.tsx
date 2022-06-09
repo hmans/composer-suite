40914134bc20ad4fb49e21b69019fba01c411d35
Hendrik Mans <hendrik@mans.de>
2022-06-09 11:05:40 +0200

    Use three's own effects

---
+++ apps/examples/src/RenderPipeline.tsx
@@ -0,0 +1,39 @@
+import { Effects } from "@react-three/drei"
+import { extend } from "@react-three/fiber"
+import { HalfFloatType, LinearEncoding, Vector2 } from "three"
+import { AdaptiveToneMappingPass } from "three/examples/jsm/postprocessing/AdaptiveToneMappingPass.js"
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js"
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js"
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
+import { VignetteShader } from "three/examples/jsm/shaders/VignetteShader.js"
+
+extend({ UnrealBloomPass, AdaptiveToneMappingPass, ShaderPass, RenderPass })
+
+type EffectsPassProps<T extends { new (...args: any): any }> = {
+  args?: ConstructorParameters<T>
+  enabled?: boolean
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      unrealBloomPass: EffectsPassProps<typeof UnrealBloomPass>
+      adaptiveToneMappingPass: EffectsPassProps<typeof AdaptiveToneMappingPass>
+    }
+  }
+}
+
+export const RenderPipeline = ({
+  bloom = false,
+  toneMapping = false,
+  vignette = false
+}) => (
+  <Effects disableGamma encoding={LinearEncoding} type={HalfFloatType}>
+    <unrealBloomPass
+      args={[new Vector2(512, 512), 2, 0.05, 1]}
+      enabled={bloom}
+    />
+    <adaptiveToneMappingPass args={[true, 256]} enabled={toneMapping} />
+    <shaderPass args={[VignetteShader]} enabled={vignette} />
+  </Effects>
+)
