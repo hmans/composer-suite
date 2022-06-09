8f13c34d350866ad6f49838c4cd765e0c7f27e7d
Hendrik Mans <hendrik@mans.de>
2022-06-09 16:12:17 +0200

    Cleanup

--- apps/examples/src/examples/Fog.tsx
+++ apps/examples/src/examples/Fog.tsx
@@ -1,7 +1,7 @@
 import { between } from "randomish"
 import { useMemo } from "react"
 import { AdditiveBlending, MeshStandardMaterial, TextureLoader } from "three"
-import { Emitter, MeshParticles, ParticlesMaterial } from "vfx"
+import { Emitter, MeshParticles, ParticlesMaterial, VisualEffect } from "vfx"
 
 export const Fog = () => {
   const texture = useMemo(
@@ -10,30 +10,32 @@ export const Fog = () => {
   )
 
   return (
-    <MeshParticles renderOrder={1}>
-      <planeGeometry />
+    <VisualEffect renderOrder={1}>
+      <MeshParticles>
+        <planeGeometry />
 
-      <ParticlesMaterial
-        baseMaterial={MeshStandardMaterial}
-        map={texture}
-        blending={AdditiveBlending}
-        depthTest={true}
-        depthWrite={false}
-        billboard
-      />
+        <ParticlesMaterial
+          baseMaterial={MeshStandardMaterial}
+          map={texture}
+          blending={AdditiveBlending}
+          depthTest={true}
+          depthWrite={false}
+          billboard
+        />
 
-      <Emitter
-        count={100}
-        setup={(c) => {
-          c.position.randomDirection().multiplyScalar(between(0, 50))
-          c.velocity.randomDirection().multiplyScalar(between(0, 3))
-          c.lifetime = Infinity
-          c.scaleStart.setScalar(between(1, 50))
-          c.scaleEnd.setScalar(0)
-          c.alphaStart = between(0.05, 0.1)
-          c.alphaEnd = 0
-        }}
-      />
-    </MeshParticles>
+        <Emitter
+          count={100}
+          setup={(c) => {
+            c.position.randomDirection().multiplyScalar(between(0, 50))
+            c.velocity.randomDirection().multiplyScalar(between(0, 3))
+            c.lifetime = Infinity
+            c.scaleStart.setScalar(between(1, 50))
+            c.scaleEnd.setScalar(0)
+            c.alphaStart = between(0.05, 0.1)
+            c.alphaEnd = 0
+          }}
+        />
+      </MeshParticles>
+    </VisualEffect>
   )
 }
