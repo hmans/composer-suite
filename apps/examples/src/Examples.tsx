5e2ba29a24c4c4fa360d007b5e727c09177a7905
Hendrik Mans <hendrik@mans.de>
2022-06-09 14:49:25 +0200

    Add a new stage

--- apps/examples/src/Examples.tsx
+++ apps/examples/src/Examples.tsx
@@ -1,37 +1,10 @@
-import { OrbitControls, PerspectiveCamera, Plane } from "@react-three/drei"
-import { Canvas, useFrame } from "@react-three/fiber"
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
 import { Perf } from "r3f-perf"
-import { useRef } from "react"
-import { LinearEncoding, Mesh } from "three"
-import spawnEffect from "./actions/spawnEffect"
+import { LinearEncoding } from "three"
 import Effects from "./Effects"
 import Fog from "./effects/Fog"
 import { RenderPipeline } from "./RenderPipeline"
-import ageSystem from "./systems/ageSystem"
-import flushQueueSystem from "./systems/flushQueueSystem"
-import maxAgeSystem from "./systems/maxAgeSystem"
-
-const Ground = () => {
-  return (
-    <Plane
-      args={[1000, 1000]}
-      rotation-x={-Math.PI / 2}
-      onClick={(e) => spawnEffect(e.point)}
-    >
-      <meshStandardMaterial color="#888" />
-    </Plane>
-  )
-}
-
-const Systems = () => {
-  useFrame((_, dt) => {
-    ageSystem(dt)
-    maxAgeSystem()
-    flushQueueSystem()
-  })
-
-  return null
-}
 
 const Examples = () => (
   <Canvas
@@ -49,25 +22,38 @@ const Examples = () => (
     <ambientLight intensity={0.4} />
     <directionalLight position={[10, 10, 10]} intensity={1} />
     <fogExp2 attach="fog" args={["#000", 0.005]} />
-    <PerspectiveCamera position={[0, 30, 100]} makeDefault />
+    <PerspectiveCamera position={[0, 5, 50]} makeDefault />
     <OrbitControls
       autoRotate
-      enablePan={false}
-      enableRotate={false}
-      enableZoom={false}
+      // enablePan={false}
+      // enableRotate={false}
+      // enableZoom={false}
       makeDefault
     />
 
     {/* Scene objects */}
-    <Ground />
     <Fog />
     <Effects />
+    <Stage />
+
+    <mesh>
+      <dodecahedronGeometry />
+      <meshStandardMaterial color="#f00" />
+    </mesh>
 
     {/* Rendering, ECS, etc. */}
     <RenderPipeline bloom vignette toneMapping />
-    <Systems />
     <Perf />
   </Canvas>
+)
+
+const Stage = () => (
+  <group>
+    <mesh position-y={-64}>
+      <cylinderGeometry args={[16, 16, 128, 64]} />
+      <meshStandardMaterial color="#888" />
+    </mesh>
+  </group>
 )
 
 export default Examples
