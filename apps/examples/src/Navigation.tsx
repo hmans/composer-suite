c6ab32137ceefbcd6780a4e5175d8970e10e686d
Hendrik Mans <hendrik@mans.de>
2022-06-09 16:50:21 +0200

    Include link to repo

--- apps/examples/src/Navigation.tsx
+++ apps/examples/src/Navigation.tsx
@@ -3,19 +3,33 @@ import Examples from "./examples"
 
 export const Navigation = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        zIndex: 1,
-        top: 10,
-        left: 10
-      }}
-    >
-      {Examples.map(({ path, name }) => (
-        <Link key={path} href={`/${path}`}>
-          {name}
-        </Link>
-      ))}
-    </div>
+    <>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          top: 10,
+          left: 10
+        }}
+      >
+        {Examples.map(({ path, name }) => (
+          <Link key={path} href={`/${path}`}>
+            {name}
+          </Link>
+        ))}
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          bottom: 10,
+          left: 10
+        }}
+      >
+        <a href="https://github.com/hmans/vfx" target="_blank">
+          github.com/hmans/vfx
+        </a>
+      </div>
+    </>
   )
 }
