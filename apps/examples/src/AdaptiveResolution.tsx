2c63b80d233c11fa4c9ef129d239ecfb042e8b20
Hendrik Mans <hendrik@mans.de>
2022-06-10 01:35:53 +0200

    Cleanup

--- apps/examples/src/AdaptiveResolution.tsx
+++ apps/examples/src/AdaptiveResolution.tsx
@@ -4,20 +4,21 @@ import { FC, useRef } from "react"
 type Props = {
   targetFPS?: number
   speed?: number
+  smoothness?: number
 }
 
 export const AdaptiveResolution: FC<Props> = ({
   targetFPS = 60,
-  speed = 0.02
+  speed = 0.02,
+  smoothness = 20
 }) => {
   const initialDpr = useThree((state) => state.viewport.initialDpr)
   const setDpr = useThree((state) => state.setDpr)
 
   const currentDpr = useRef(initialDpr)
 
-  const times = useRef([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  const times = useRef(Array(smoothness).fill(0))
   const sum = useRef(0)
-  const length = times.current.length
   const targetTime = 1 / targetFPS
 
   useFrame((_, dt) => {
@@ -25,7 +26,7 @@ export const AdaptiveResolution: FC<Props> = ({
     times.current.push(dt)
     sum.current += dt
 
-    const averageTime = sum.current / length
+    const averageTime = sum.current / smoothness
 
     if (averageTime > targetTime * 1.1) {
       currentDpr.current = Math.max(currentDpr.current - speed, 0.1)
