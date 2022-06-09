26c3c865fd85db38a76bafb41dfed07bec031b52
Hendrik Mans <hendrik@mans.de>
2022-06-09 15:02:15 +0200

    Stage explosion effect

--- apps/examples/src/Examples.tsx
+++ apps/examples/src/Examples.tsx
@@ -2,7 +2,9 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Perf } from "r3f-perf"
 import { LinearEncoding } from "three"
+import { Repeat } from "vfx"
 import Effects from "./Effects"
+import Explosion from "./effects/Explosion"
 import Fog from "./effects/Fog"
 import { RenderPipeline } from "./RenderPipeline"
 import { Stage } from "./Stage"
@@ -24,22 +26,16 @@ const Examples = () => (
     <directionalLight position={[10, 10, 10]} intensity={1} />
     <fog attach="fog" args={["#000", 20, 180]} />
     <PerspectiveCamera position={[0, 20, 50]} makeDefault />
-    <OrbitControls
-      autoRotate
-      enablePan={false}
-      enableZoom={false}
-      makeDefault
-    />
+    <OrbitControls enablePan={false} enableZoom={false} makeDefault />
 
     {/* Scene objects */}
-    <Fog />
     <Effects />
     <Stage />
 
-    <mesh>
-      <dodecahedronGeometry />
-      <meshStandardMaterial color="#f00" />
-    </mesh>
+    {/* The actual effect */}
+    <Repeat times={Infinity} interval={3}>
+      <Explosion />
+    </Repeat>
 
     {/* Rendering, ECS, etc. */}
     <RenderPipeline bloom vignette toneMapping />
