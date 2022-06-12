d3855777d3b10ba0aa53d044b5c5ee24f0900e3f
Hendrik Mans <hendrik@mans.de>
2022-06-12 16:12:58 +0200

    Configuration!

--- apps/examples/src/examples/Composable.tsx
+++ apps/examples/src/examples/Composable.tsx
@@ -24,48 +24,44 @@ const generateModuleInvocation = ({ name }: ShaderModule) =>
 
 const float = (num: number) => num.toFixed(8)
 
+const wobble: ShaderModule = {
+  name: "wobble_462378", // needs to be unique, will automate this
+  chunk: /*glsl*/ `
+    offset.y += 8.0 + cos(u_time * 13.0) * 2.0;
+    offset.x += 0.0 + sin(u_time * 7.0) * 2.0;
+    offset.z += 0.0 + cos(u_time * 5.0) * 2.0;
+  `
+}
+
+const makeShake = (
+  axis: "x" | "y" | "z",
+  frequency = 1.234,
+  amplitude = 1
+): ShaderModule => ({
+  name: "shake_48932749", // needs to be unique, will automate this
+  chunk: /*glsl*/ `
+    offset.${axis} += cos(u_time * ${float(frequency)}) * ${float(amplitude)};
+  `
+})
+
 class ParticlesMaterial extends CustomShaderMaterial {
-  constructor(opts: iCSMParams) {
+  constructor(opts: iCSMParams, public modules: ShaderModule[] = []) {
     super(opts)
     this.update(this.generateShaders())
   }
 
   generateShaders() {
-    const wobble: ShaderModule = {
-      name: "wobble_462378", // needs to be unique, will automate this
-      chunk: /*glsl*/ `
-        offset.y += 8.0 + cos(u_time * 13.0) * 2.0;
-        offset.x += 0.0 + sin(u_time * 7.0) * 2.0;
-        offset.z += 0.0 + cos(u_time * 5.0) * 2.0;
-      `
-    }
-
-    const makeShake = (
-      axis: "x" | "y" | "z",
-      frequency = 1.234,
-      amplitude = 1
-    ): ShaderModule => ({
-      name: "shake_48932749", // needs to be unique, will automate this
-      chunk: /*glsl*/ `
-        offset.${axis} += cos(u_time * ${float(frequency)}) * ${float(
-        amplitude
-      )};
-      `
-    })
-
-    const configuration = [wobble, makeShake("x", 6, 8)]
-
     const vertexShader = /*glsl*/ `
       uniform float u_time;
 
-      ${configuration.map(generateModuleFunction).join("\n")}
+      ${this.modules.map(generateModuleFunction).join("\n")}
 
       void main() {
         /* Start with an origin offset */
         vec3 offset = vec3(0.0, 0.0, 0.0);
 
         /* Invoke custom chunks */
-        ${configuration.map(generateModuleInvocation).join("\n")}
+        ${this.modules.map(generateModuleInvocation).join("\n")}
 
         /* Apply the instance matrix. */
         offset *= mat3(instanceMatrix);
@@ -87,11 +83,16 @@ class ParticlesMaterial extends CustomShaderMaterial {
 }
 
 export const Composable = () => {
+  // const configuration = [wobble, makeShake("x", 6, 8)]
+
   const material = useMemo(
     () =>
-      new ParticlesMaterial({
-        baseMaterial: new MeshStandardMaterial({ color: "white" })
-      }),
+      new ParticlesMaterial(
+        {
+          baseMaterial: new MeshStandardMaterial({ color: "white" })
+        },
+        [wobble, makeShake("x", 6, 8)]
+      ),
     []
   )
 
