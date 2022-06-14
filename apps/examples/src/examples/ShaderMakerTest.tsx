a5f0a94c5cb1425a1beef6cda4027cab6379f1c5
Hendrik Mans <hendrik@mans.de>
2022-06-14 20:27:00 +0200

    fresnel

--- apps/examples/src/examples/ShaderMakerTest.tsx
+++ apps/examples/src/examples/ShaderMakerTest.tsx
@@ -1,6 +1,6 @@
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
-import { MeshStandardMaterial } from "three"
+import { Color, MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 import { compileShader, float, makeShaderModule } from "three-shadermaker"
@@ -48,6 +48,37 @@ const wobbleScale = () =>
   `
   })
 
+const makeFresnel = () =>
+  makeShaderModule({
+    uniforms: {
+      u_color: { type: "vec3", value: new Color("white") },
+      u_alpha: { type: "float", value: 1 },
+      u_bias: { type: "float", value: 0.1 },
+      u_intensity: { type: "float", value: 1 },
+      u_power: { type: "float", value: 2 },
+      u_factor: { type: "float", value: 1 }
+    },
+
+    varyings: {
+      v_worldPosition: {
+        type: "vec3",
+        value: "vec3(-viewMatrix[0][2], -viewMatrix[1][2], -viewMatrix[2][2])"
+      },
+      v_worldNormal: {
+        type: "vec3",
+        value:
+          "normalize( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal )"
+      }
+    },
+
+    fragmentMain: `
+      float f_a = (u_factor  + dot(v_worldPosition, v_worldNormal));
+      float f_fresnel = u_bias + u_intensity * pow(abs(f_a), u_power);
+      f_fresnel = clamp(f_fresnel, 0.0, 1.0);
+      csm_FragColor = vec4(f_fresnel * u_color, u_alpha);
+    `
+  })
+
 export const ShaderMakerTest = () => {
   const material = useRef<CustomShaderMaterialImpl>(null!)
 
@@ -61,11 +92,14 @@ export const ShaderMakerTest = () => {
   const { callback, ...shader } = compileShader(
     makeTime(),
     pretty,
+    makeFresnel(),
     wobbleScale(),
     makeWobble({ axis: "x", frequency: 7 }),
     makeWobble({ axis: "y", frequency: 5 }),
     makeWobble({ axis: "z", frequency: 3 })
   )
+
+  console.log(shader.uniforms)
 
   /* Update time */
   useFrame((_, dt) => callback(material.current, dt))
@@ -77,6 +111,7 @@ export const ShaderMakerTest = () => {
   return (
     <mesh position-y={8}>
       <sphereGeometry args={[5]} />
+      {/* <dodecahedronGeometry args={[7]} /> */}
       <CustomShaderMaterial
         baseMaterial={MeshStandardMaterial}
         {...shader}
