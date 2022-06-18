08ceb044fbc81fbdabb8bcbead16fb2a65c7569e
Hendrik Mans <hendrik@mans.de>
2022-06-18 16:01:34 +0200

    Make Emitter an object3D

--- packages/vfx/src/Emitter.tsx
+++ packages/vfx/src/Emitter.tsx
@@ -1,18 +1,19 @@
+import { Object3DProps } from "@react-three/fiber"
 import { FC, useEffect } from "react"
 import { SpawnSetup, useParticles } from "./MeshParticles"
 import { getValue, ValueFactory } from "./ValueFactory"
 
-export type EmitterProps = {
+export type EmitterProps = Object3DProps & {
   count?: ValueFactory<number>
   setup?: SpawnSetup
 }
 
-export const Emitter: FC<EmitterProps> = ({ count = 0, setup }) => {
+export const Emitter: FC<EmitterProps> = ({ count = 0, setup, ...props }) => {
   const { spawnParticle } = useParticles()
 
   useEffect(() => {
     spawnParticle(getValue(count), setup)
   }, [])
 
-  return null
+  return <object3D {...props} />
 }
