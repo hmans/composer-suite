e6e7a26c7f582f3aa9dbdce1b08899fa485c774d
Hendrik Mans <hendrik@mans.de>
2022-06-18 12:58:37 +0200

    time module

--- packages/vfx/src/shader.ts
+++ packages/vfx/src/shader.ts
@@ -5,17 +5,12 @@ const uniformsChunk = /*glsl*/ `
 
 /* Varyings */
 const varyingsChunk = /*glsl*/ `
-    varying float v_timeStart;
-    varying float v_timeEnd;
-    varying float v_progress;
-    varying float v_age;
     varying vec4 v_colorStart;
     varying vec4 v_colorEnd;
   `
 
 /* Attributes */
 const attributesChunk = /*glsl*/ `
-    attribute vec2 time;
     attribute vec3 velocity;
     attribute vec3 acceleration;
     attribute vec4 colorStart;
@@ -81,6 +76,37 @@ export const createShader = ({
     })
   )
 
+  /* Particle lifetime */
+  addModule(
+    module({
+      vertexHeader: `
+        attribute vec2 time;
+        varying float v_timeStart;
+        varying float v_timeEnd;
+        varying float v_progress;
+        varying float v_age;
+      `,
+      vertexMain: `
+        v_timeStart = time.x;
+        v_timeEnd = time.y;
+        v_age = u_time - v_timeStart;
+        v_progress = v_age / (v_timeEnd - v_timeStart);
+      `,
+      fragmentHeader: `
+        varying float v_timeStart;
+        varying float v_timeEnd;
+        varying float v_progress;
+        varying float v_age;
+      `,
+      fragmentMain: `
+        /* Lifetime management: discard this instance if it is not in the current time range */
+        if (u_time < v_timeStart || u_time > v_timeEnd) {
+          discard;
+        }
+      `
+    })
+  )
+
   addModule(
     module({
       vertexHeader: `
@@ -92,12 +118,8 @@ export const createShader = ({
 
       /* Set the varyings we want to forward */
       void setVaryings() {
-        v_timeStart = time.x;
-        v_timeEnd = time.y;
         v_colorStart = colorStart;
         v_colorEnd = colorEnd;
-        v_age = u_time - v_timeStart;
-        v_progress = v_age / (v_timeEnd - v_timeStart);
       }
     `,
       vertexMain: `
@@ -125,10 +147,6 @@ export const createShader = ({
       ${varyingsChunk}
     `,
       fragmentMain: `
-      /* Lifetime management: discard this instance if it is not in the current time range */
-      if (u_time < v_timeStart || u_time > v_timeEnd) {
-        discard;
-      }
 
       /* Get diffuse color */
       vec4 diffuse4 = vec4(diffuse, 1.0);
