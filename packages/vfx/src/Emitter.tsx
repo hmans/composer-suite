12262376ccd4c152cae67d1db8ecb199bbd6dba6
Hendrik Mans <hendrik@mans.de>
2022-06-18 16:25:30 +0200

    Continuous mode for emitters

--- packages/vfx/src/Emitter.tsx
+++ packages/vfx/src/Emitter.tsx
@@ -1,4 +1,4 @@
-import { Object3DProps } from "@react-three/fiber"
+import { Object3DProps, useFrame } from "@react-three/fiber"
 import { FC, useEffect } from "react"
 import { SpawnSetup, useParticles } from "./MeshParticles"
 import { getValue, ValueFactory } from "./ValueFactory"
@@ -6,14 +6,28 @@ import { getValue, ValueFactory } from "./ValueFactory"
 export type EmitterProps = Object3DProps & {
   count?: ValueFactory<number>
   setup?: SpawnSetup
+  continuous?: boolean
 }
 
-export const Emitter: FC<EmitterProps> = ({ count = 0, setup, ...props }) => {
+export const Emitter: FC<EmitterProps> = ({
+  count = 0,
+  setup,
+  continuous = false,
+  ...props
+}) => {
   const { spawnParticle } = useParticles()
 
   useEffect(() => {
+    if (continuous) return
+
     spawnParticle(getValue(count), setup)
   }, [])
+
+  useFrame(() => {
+    if (continuous) {
+      spawnParticle(getValue(count), setup)
+    }
+  })
 
   return <object3D {...props} />
 }
