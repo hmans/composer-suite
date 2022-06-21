f683b61ce224b9e4faf59f4138939331b9af32ec
Hendrik Mans <hendrik@mans.de>
2022-06-21 19:23:28 +0200

    Cleanup

--- packages/vfx/src/MeshParticles/MeshParticlesMaterial.tsx
+++ packages/vfx/src/MeshParticles/MeshParticlesMaterial.tsx
@@ -1,7 +1,7 @@
 import { useFrame } from "@react-three/fiber"
 import React, { forwardRef, useMemo, useRef } from "react"
 import mergeRefs from "react-merge-refs"
-import { AddEquation, CustomBlending, DepthTexture } from "three"
+import { DepthTexture } from "three"
 import CustomShaderMaterial, { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 import { composableShader, modules } from "../shaders/"
