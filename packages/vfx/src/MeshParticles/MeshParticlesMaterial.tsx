17c80bb15a8fd443cdb4ce4f87914253ddef6280
Hendrik Mans <hendrik@mans.de>
2022-06-21 22:05:12 +0200

    Always a unique cache key

--- packages/vfx/src/MeshParticles/MeshParticlesMaterial.tsx
+++ packages/vfx/src/MeshParticles/MeshParticlesMaterial.tsx
@@ -1,7 +1,7 @@
 import { useFrame } from "@react-three/fiber"
 import React, { forwardRef, useMemo, useRef } from "react"
 import mergeRefs from "react-merge-refs"
-import { DepthTexture } from "three"
+import { DepthTexture, MathUtils } from "three"
 import CustomShaderMaterial, { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 import { composableShader, modules } from "../shaders/"
@@ -60,6 +60,7 @@ export const MeshParticlesMaterial = forwardRef<
     return (
       <CustomShaderMaterial
         ref={mergeRefs([material, ref])}
+        cacheKey={() => MathUtils.generateUUID()}
         {...shader}
         {...props}
       />
