75171365b5a4372bb8593507c7569891ed645dec
Hendrik Mans <hendrik@mans.de>
2022-06-22 17:24:09 +0200

    Cleanup

--- packages/vfx/src/shaders/modules/scale.ts
+++ packages/vfx/src/shaders/modules/scale.ts
@@ -2,26 +2,28 @@ import { Vector3 } from "three"
 import { module } from ".."
 
 export default function scale(fun = "v_progress") {
-  return module<{ min: Vector3; max: Vector3 }>({
+  return module<{ scale: { min: Vector3; max: Vector3 } }>({
     attributes: {
       scale0: { itemSize: 3 },
       scale1: { itemSize: 3 }
     },
 
     vertexHeader: `
-    attribute vec3 scale0;
-    attribute vec3 scale1;
-  `,
+      attribute vec3 scale0;
+      attribute vec3 scale1;
+    `,
     vertexMain: `
-    csm_Position *= mix(scale0, scale1, ${fun});
-  `,
+      csm_Position *= mix(scale0, scale1, ${fun});
+    `,
 
     configurator: () => ({
-      min: new Vector3(),
-      max: new Vector3()
+      scale: {
+        min: new Vector3(),
+        max: new Vector3()
+      }
     }),
 
-    setup: ({ attributes }, index, { min, max }) => {
+    setup: ({ attributes }, index, { scale: { min, max } }) => {
       attributes.scale0.setXYZ(index, ...min.toArray())
       attributes.scale1.setXYZ(index, ...max.toArray())
     }
