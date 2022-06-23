c58c9b04f112c37624bb222076234652f2e904b6
Hendrik Mans <hendrik@mans.de>
2022-06-23 14:56:38 +0200

    Cleanup

--- packages/vfx/src/layers/billboarding.ts
+++ packages/vfx/src/layers/billboarding.ts
@@ -3,16 +3,15 @@ import { createShader } from "../shaders"
 export default function() {
   return createShader({
     vertexHeader: `
-    /* Billboard helper */
-    vec3 billboard(vec2 v, mat4 view){
-      vec3 up = vec3(view[0][1], view[1][1], view[2][1]);
-      vec3 right = vec3(view[0][0], view[1][0], view[2][0]);
-      vec3 p = right * v.x + up * v.y;
-      return p;
-    }
-  `,
+      vec3 billboard(vec2 v, mat4 view){
+        vec3 up = vec3(view[0][1], view[1][1], view[2][1]);
+        vec3 right = vec3(view[0][0], view[1][0], view[2][0]);
+        vec3 p = right * v.x + up * v.y;
+        return p;
+      }
+    `,
     vertexMain: `
-    csm_Position = billboard(csm_Position.xy, viewMatrix);
-  `
+      csm_Position = billboard(csm_Position.xy, viewMatrix);
+    `
   })
 }
