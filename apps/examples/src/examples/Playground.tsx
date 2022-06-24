383dbd640e13a4a4de261654096b1cad468a2988
Hendrik Mans <hendrik@mans.de>
2022-06-24 16:03:30 +0200

    Let's get that compile on

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,11 +1,30 @@
 import { MeshStandardMaterial } from "three"
 import { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterial from "three-custom-shader-material"
+import { useMemo } from "react"
+import { ShaderNode } from "./shadenfreude/types"
+import { compileShaderNode } from "./shadenfreude/compilers"
 
 type ModularShaderMaterialProps = Omit<iCSMProps, "ref">
 
+function useShader() {
+  return useMemo(() => {
+    const node: ShaderNode = {
+      name: "Test",
+      vertexHeader: "",
+      vertexBody: "",
+      fragmentHeader: "",
+      fragmentBody: "csm_DiffuseColor.rgb = vec3(1.0, 0.5, 0.0);"
+    }
+
+    return compileShaderNode(node)
+  }, [])
+}
+
 function MyMaterial({ children, ...props }: ModularShaderMaterialProps) {
-  return <CustomShaderMaterial {...props} />
+  const shaderProps = useShader()
+
+  return <CustomShaderMaterial {...props} {...shaderProps} />
 }
 
 export default function Playground() {
--- apps/examples/src/examples/shadenfreude/compilers.ts
+++ apps/examples/src/examples/shadenfreude/compilers.ts
@@ -1,8 +1,20 @@
 import { ShaderNode } from "./types"
 
 export function compileShaderNode(node: ShaderNode) {
-  const vertexShader = ""
-  const fragmentShader = ""
+  const vertexShader = `
+    ${node.vertexHeader}
+
+    void main() {
+      ${node.vertexBody}
+    }`
+
+  const fragmentShader = `
+    ${node.fragmentHeader}
+
+    void main() {
+      ${node.fragmentBody}
+    }`
+
   const uniforms = {}
 
   return { vertexShader, fragmentShader, uniforms }
