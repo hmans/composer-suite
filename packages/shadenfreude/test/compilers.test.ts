4730bd4c9ccf75e32eedf5db6e29cd91bdcac98c
Hendrik Mans <hendrik@mans.de>
2022-06-26 15:10:35 +0200

    Move helpers into helpers

--- packages/shadenfreude/test/compilers.test.ts
+++ packages/shadenfreude/test/compilers.test.ts
@@ -1,35 +1,7 @@
 import { Vector3 } from "three"
 import { node, variable } from "../src"
 import { compileShader, compileVariable } from "../src/compilers"
-
-export const glsl = (glsl: string) => glsl.replace(/\s+/g, " ").trim()
-
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeGLSL(expected: string): R
-    }
-  }
-}
-
-/* TODO: move this into some global support file */
-expect.extend({
-  toBeGLSL(received, expected) {
-    const pass = glsl(received) === glsl(expected)
-
-    if (pass) {
-      return {
-        message: () => `expected ${received} to not be equal to ${expected}`,
-        pass: true
-      }
-    }
-
-    return {
-      message: () => `expected ${received} to be equal to ${expected}`,
-      pass: false
-    }
-  }
-})
+import "./helpers"
 
 describe("compileShader", () => {
   it("should compile a shader", () => {
