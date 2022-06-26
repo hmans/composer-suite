397754dca7d6e7edf2cbb8226781af17520d8dce
Hendrik Mans <hendrik@mans.de>
2022-06-26 14:36:14 +0200

    More tests

--- packages/shadenfreude/test/compilers.test.ts
+++ packages/shadenfreude/test/compilers.test.ts
@@ -1,3 +1,4 @@
+import { Vector3 } from "three"
 import { node, variable } from "../src"
 import { compileShader, compileVariable } from "../src/compilers"
 
@@ -24,6 +25,11 @@ describe("compileVariable", () => {
   it("uses string values verbatim (assuming they are a reference to a variable)", () => {
     const v = variable("float", "foo")
     expect(compileVariable(v)).toEqual(`float ${v.name} = foo;`)
+  })
+
+  it("renders non-scalar values using their GLSL representation", () => {
+    const v = variable("vec3", new Vector3(1, 2, 3))
+    expect(compileVariable(v)).toEqual(`vec3 ${v.name} = vec3(1.0, 2.0, 3.0);`)
   })
 
   it("resolves variable references", () => {
