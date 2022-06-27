fce6146bd9cf67879f20eba2750fa2de6cedc8fd
Hendrik Mans <hendrik@mans.de>
2022-06-27 12:09:41 +0200

    Composition

--- packages/shadenfreude/src/nodes/inputs.ts
+++ packages/shadenfreude/src/nodes/inputs.ts
@@ -1,4 +1,5 @@
 import { nodeFactory, variable, node, float, vec3 } from "../factories"
+import { Value } from "../types"
 
 export const TimeNode = nodeFactory(() => {
   const u_time = variable("float", 0)
@@ -15,16 +16,13 @@ export const TimeNode = nodeFactory(() => {
 })
 
 export const VertexPositionNode = nodeFactory(() => ({
-  name: "Vertex Position",
+  ...VaryingNode({ value: "position" }),
+  name: "Vertex Position"
+}))
 
-  varyings: { v_position: vec3() },
+export const VaryingNode = nodeFactory<{ value: string }>(({ value }) => ({
+  varyings: { v: vec3() },
   outputs: { value: vec3() },
-
-  vertex: {
-    body: `value = v_position = position;`
-  },
-
-  fragment: {
-    body: `value = v_position;`
-  }
+  vertex: { body: `value = v = ${value};` },
+  fragment: { body: `value = v;` }
 }))
