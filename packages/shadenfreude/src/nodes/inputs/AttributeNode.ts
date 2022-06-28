1079a5b2f76e762647dbdf3a1242f28c63dae7ce
Hendrik Mans <hendrik@mans.de>
2022-06-28 15:48:47 +0200

    AttributeNode now uses VaryingNode

--- packages/shadenfreude/src/nodes/inputs/AttributeNode.ts
+++ packages/shadenfreude/src/nodes/inputs/AttributeNode.ts
@@ -1,21 +1,18 @@
 import { nodeFactory } from "../../factories"
 import { GLSLType } from "../../types"
 import { variable } from "../../variables"
+import { VaryingNode } from "../util"
 
 export const AttributeNode = nodeFactory<{ name: string; type: GLSLType }>(
   ({ name, type }) => ({
-    varyings: {
-      v_value: variable(type)
+    inputs: {
+      v_value: variable(type, VaryingNode({ type, source: name }))
     },
     outputs: {
-      value: variable(type)
+      value: variable(type, "v_value")
     },
     vertex: {
-      header: `attribute ${type} ${name};`,
-      body: `value = v_value = ${name};`
-    },
-    fragment: {
-      body: `value = v_value;`
+      header: `attribute ${type} ${name};`
     }
   })
 )
