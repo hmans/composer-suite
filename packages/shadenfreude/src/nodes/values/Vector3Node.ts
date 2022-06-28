8ae1cde90892c4e2a31f192ac22e76347d0a42e4
Hendrik Mans <hendrik@mans.de>
2022-06-28 13:48:23 +0200

    Export individual components from Vector3Node

--- packages/shadenfreude/src/nodes/values/Vector3Node.ts
+++ packages/shadenfreude/src/nodes/values/Vector3Node.ts
@@ -1,10 +1,13 @@
-import { nodeFactory, Value, vec3 } from "../.."
+import { nodeFactory, Value, vec3, float } from "../.."
 
 export const Vector3Node = nodeFactory<{ value: Value<"vec3"> }>(
   ({ value }) => ({
     name: "Constant Vector3 Value",
     outputs: {
-      value: vec3(value)
+      value: vec3(value),
+      x: float("value.x"),
+      y: float("value.y"),
+      z: float("value.z")
     }
   })
 )
