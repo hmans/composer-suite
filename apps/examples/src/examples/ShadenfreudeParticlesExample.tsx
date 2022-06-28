57ea970e73910050c2f43159eca2046682ef3f22
Hendrik Mans <hendrik@mans.de>
2022-06-28 14:48:30 +0200

    Cleanup example

--- apps/examples/src/examples/ShadenfreudeParticlesExample.tsx
+++ apps/examples/src/examples/ShadenfreudeParticlesExample.tsx
@@ -1,63 +1,17 @@
-import { useMemo } from "react"
-import {
-  BlendNode,
-  ColorNode,
-  compileShader,
-  CSMMasterNode,
-  FresnelNode,
-  MultiplyNode
-} from "shadenfreude"
-import { Color, MeshStandardMaterial } from "three"
-import { iCSMProps } from "three-custom-shader-material"
-import {
-  Emitter,
-  MeshParticles,
-  MeshParticlesMaterial,
-  Repeat
-} from "three-vfx"
-
-type ModularShaderMaterialProps = Omit<iCSMProps, "ref">
-
-function useShader() {
-  return useMemo(() => {
-    const baseColor = ColorNode({
-      color: new Color("#800")
-    })
-
-    const highlight = ColorNode({
-      color: new Color(2, 2, 2)
-    })
-
-    const fresnel = MultiplyNode({
-      a: highlight,
-      b: FresnelNode()
-    })
-
-    const diffuseColor = BlendNode({
-      a: baseColor,
-      b: fresnel,
-      opacity: 1
-    })
-
-    const root = CSMMasterNode({
-      diffuseColor
-    })
-
-    return compileShader(root)
-  }, [])
-}
+import { MeshStandardMaterial } from "three"
+import { Emitter, Particles, ParticlesMaterial, Repeat } from "three-vfx"
 
 export default function() {
   return (
     <group position-y={15}>
-      <MeshParticles>
+      <Particles>
         <sphereGeometry args={[0.25]} />
-        <MeshParticlesMaterial baseMaterial={MeshStandardMaterial} />
+        <ParticlesMaterial baseMaterial={MeshStandardMaterial} />
 
-        <Repeat interval={0.2}>
+        <Repeat interval={0.25}>
           <Emitter count={1} />
         </Repeat>
-      </MeshParticles>
+      </Particles>
     </group>
   )
 }
