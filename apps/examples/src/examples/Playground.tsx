5530ef030313c65cd40e476d725ddca7a3bb7e6d
Hendrik Mans <hendrik@mans.de>
2022-06-29 13:11:05 +0200

    Clear

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,79 +1,24 @@
-import { useFrame } from "@react-three/fiber"
 import { useMemo, useRef } from "react"
-import {
-  AddNode,
-  BlendNode,
-  compileShader,
-  CSMMasterNode,
-  float,
-  FresnelNode,
-  MultiplyNode,
-  node,
-  TimeNode,
-  Value,
-  vec3,
-  VertexPositionNode
-} from "shadenfreude"
-import { Color, MeshStandardMaterial } from "three"
+import { MeshStandardMaterial } from "three"
 import CustomShaderMaterial, { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 
 type ModularShaderMaterialProps = Omit<iCSMProps, "ref">
 
-type WobbleProps = {
-  amplitude?: Value<"float">
-  frequency?: Value<"float">
-  x?: Value<"float">
-}
-
-const WobbleNode = ({ amplitude = 1, frequency = 1, x }: WobbleProps) =>
-  node({
-    name: "Wobble",
-    inputs: {
-      amplitude: float(amplitude),
-      frequency: float(frequency),
-      x: float(x)
-    },
-    outputs: { value: vec3() },
-    vertex: { body: `value.x = sin(x * frequency) * amplitude;` }
-  })
-
 function useShader() {
-  return useMemo(() => {
-    const root = CSMMasterNode({
-      diffuseColor: BlendNode({
-        a: new Color("#dd8833"),
-        b: MultiplyNode({
-          a: new Color("#ffffff"),
-          b: FresnelNode()
-        }),
-        opacity: 1
-      }),
-
-      position: AddNode({
-        a: VertexPositionNode(),
-        b: WobbleNode({
-          x: TimeNode(),
-          amplitude: 3,
-          frequency: 10
-        })
-      })
-    })
-
-    return compileShader(root)
-  }, [])
+  return useMemo(() => {}, [])
 }
 
 function MyMaterial({ children, ...props }: ModularShaderMaterialProps) {
-  const { update, ...shaderProps } = useShader()
+  // const { update, ...shaderProps } = useShader()
   const material = useRef<CustomShaderMaterialImpl>(null!)
 
   // console.log(shaderProps.vertexShader)
-  console.log(shaderProps.fragmentShader)
+  // console.log(shaderProps.fragmentShader)
 
-  useFrame(update)
+  // useFrame(update)
 
-  return <CustomShaderMaterial {...props} {...shaderProps} ref={material} />
+  return <CustomShaderMaterial {...props} ref={material} />
 }
 
 export default function Playground() {
