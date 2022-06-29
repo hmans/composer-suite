960d54dd282e07402d55a7569ad1ae304abc33ac
Hendrik Mans <hendrik@mans.de>
2022-06-29 13:28:36 +0200

    get the class ball rolling

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,24 +1,32 @@
 import { useMemo, useRef } from "react"
+import { ShaderNode } from "shadenfreude"
 import { MeshStandardMaterial } from "three"
 import CustomShaderMaterial, { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 
 type ModularShaderMaterialProps = Omit<iCSMProps, "ref">
 
+class Dummy extends ShaderNode {
+  vertex = { body: "csm_Position.x += 1.0;" }
+}
+
 function useShader() {
-  return useMemo(() => {}, [])
+  return useMemo(() => {
+    const root = new Dummy()
+    return root.compile()
+  }, [])
 }
 
 function MyMaterial({ children, ...props }: ModularShaderMaterialProps) {
-  // const { update, ...shaderProps } = useShader()
+  const { ...shaderProps } = useShader()
   const material = useRef<CustomShaderMaterialImpl>(null!)
 
-  // console.log(shaderProps.vertexShader)
-  // console.log(shaderProps.fragmentShader)
+  console.log(shaderProps.vertexShader)
+  console.log(shaderProps.fragmentShader)
 
   // useFrame(update)
 
-  return <CustomShaderMaterial {...props} ref={material} />
+  return <CustomShaderMaterial {...props} {...shaderProps} ref={material} />
 }
 
 export default function Playground() {
