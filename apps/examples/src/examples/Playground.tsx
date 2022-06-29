f00eb1801c74b27345bef1397e6f2397ca5d8bd3
Hendrik Mans <hendrik@mans.de>
2022-06-29 19:43:56 +0200

    Get something going

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,111 +1,30 @@
-import { useFrame } from "@react-three/fiber"
 import { useMemo, useRef } from "react"
-import { Compiler, GLSLType, node, RootNode, ShaderNode } from "shadenfreude"
+import { Compiler, ShaderNodeFactory } from "shadenfreude"
 import { MeshStandardMaterial } from "three"
 import CustomShaderMaterial, { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 
 type ModularShaderMaterialProps = Omit<iCSMProps, "ref">
 
-class UniformNode<T extends GLSLType> extends ShaderNode<{
-  type: T
-  name: string
-}> {
-  name = "Uniform " + this.opts.name
-
-  outputs = {
-    value: this.variable(this.opts.type, this.opts.name)
-  }
-
-  vertex = {
-    header: `uniform ${this.opts.type} ${this.opts.name};`
-  }
-
-  fragment = {
-    header: `uniform ${this.opts.type} ${this.opts.name};`
-  }
-}
-
-class TimeNode extends ShaderNode {
-  name = "Time Node"
-
-  inputs = {
-    uniform: this.float(
-      new UniformNode({}, { name: "u_time", type: "float" }).outputs.value
-    )
-  }
-
-  outputs = {
-    /* The absolute time, in seconds */
-    value: this.float("in_uniform"),
-
-    /** Sine of the times */
-    sin: this.float("sin(in_uniform)"),
-
-    /** Cosine of the times */
-    cos: this.float("cos(in_uniform)")
-  }
-}
-
-class Vector3Node extends ShaderNode {
-  name = "Vector3 Value"
-
-  inputs = {
-    vector: this.vec3()
-  }
-
-  outputs = {
-    value: this.vec3(this.inputs.vector)
-  }
-}
-
-class FloatNode extends ShaderNode {
-  name = "Float Value"
-
-  inputs = {
-    float: this.float()
-  }
-
-  outputs = {
-    value: this.float(this.inputs.float)
-  }
-}
-
-class Root extends RootNode {
-  name = "Root"
-
-  inputs = {
-    offset: this.float()
-  }
-
-  vertex = { body: "csm_Position.x += in_offset;" }
-}
+const RootNode: ShaderNodeFactory = () => ({
+  name: "Root Node"
+})
 
 function useShader() {
   return useMemo(() => {
-    const float = new FloatNode()
-    const vector3 = new Vector3Node()
-    const u_time = new UniformNode({}, { type: "float", name: "u_fooooo" })
-
-    const floaty = node(FloatNode, { float: 123 })
-
-    const time = new TimeNode()
-    const root = new Root()
-
-    root.inputs.offset.set(time.outputs.cos)
-
-    return new Compiler(root).compile()
+    const root = RootNode()
+    return Compiler(root)
   }, [])
 }
 
 function MyMaterial({ children, ...props }: ModularShaderMaterialProps) {
-  const { update, ...shaderProps } = useShader()
+  const { ...shaderProps } = useShader()
   const material = useRef<CustomShaderMaterialImpl>(null!)
 
   console.log(shaderProps.vertexShader)
   // console.log(shaderProps.fragmentShader)
 
-  useFrame((_, dt) => update(dt))
+  // useFrame((_, dt) => update(dt))
 
   return <CustomShaderMaterial {...props} {...shaderProps} ref={material} />
 }
--- packages/shadenfreude/src/shadenfreude.ts
+++ packages/shadenfreude/src/shadenfreude.ts
@@ -52,9 +52,15 @@ export type Value<T extends GLSLType> =
   | Variable<T>
   | GLSLChunk
 
-export type Variables = { [localName: string]: Variable<GLSLType> }
+export type Variable<T extends GLSLType> = {
+  __variable: true
+  globalName: string
+  type: T
+  value?: Value<T>
+  node?: ShaderNode
+}
 
-export type ShaderNodeOptions = { [key: string]: any }
+export type Variables = { [localName: string]: Variable<GLSLType> }
 
 /*
 
@@ -68,64 +74,15 @@ export type ShaderNodeOptions = { [key: string]: any }
 
 */
 
-export type ShaderNodeProps<T extends ShaderNode> = {
-  [Key in keyof T["inputs"]]: T["inputs"][Key]["value"]
-}
-
-export function node<T extends ShaderNode>(
-  klass: new (...args: any[]) => T,
-  props: ShaderNodeProps<T>
-): T {
-  const node = new klass(props)
-  Object.entries(props).forEach(([key, value]) => {
-    node.inputs[key].set(value)
-  })
-  return node
-}
-
-export abstract class ShaderNode<O extends ShaderNodeOptions = {}> {
-  constructor(protected props: any = {}, protected opts: O = {} as O) {}
-
-  /** Human-readable name. */
-  name: string = "Unnamed Shader Node"
-
-  /** Machine-readable name; will be used as part of GLSL variable names. */
+export type ShaderNode = {
+  name?: string
   slug?: string
 
-  vertex: Program = {}
-  fragment: Program = {}
-  inputs: Variables = {}
-  outputs: Variables = {}
-
-  /** Returns this node's immediate dependencies. */
-  getDependencies() {
-    const dependencies = new Set<ShaderNode>()
-
-    for (const input of Object.values(this.inputs)) {
-      if (input.value instanceof Variable) {
-        dependencies.add(input.value.node)
-      }
-    }
-
-    return [...dependencies]
-  }
-
-  /** Creates a variable of the specified type, linked to this node. */
-  variable<T extends GLSLType>(type: T, value?: Value<T>): Variable<T> {
-    const variable = new Variable(this, type, value)
-    return variable
-  }
-
-  float(value?: Value<"float">) {
-    return this.variable("float", value)
-  }
-
-  vec3(value?: Value<"vec3">) {
-    return this.variable("vec3", value)
-  }
+  vertex?: Program
+  fragment?: Program
 }
 
-export abstract class RootNode extends ShaderNode {}
+export type ShaderNodeFactory = () => ShaderNode
 
 /*
 
@@ -139,54 +96,6 @@ export abstract class RootNode extends ShaderNode {}
 
 */
 
-export class Variable<T extends GLSLType = any> {
-  node: ShaderNode
-  type: T
-  value?: Value<T>
-  globalName: string
-
-  constructor(node: ShaderNode, type: T, value?: Value<T>) {
-    this.node = node
-    this.type = type
-    this.value = value
-    this.globalName = `var_${Math.floor(Math.random() * 1000000)}`
-  }
-
-  set(value: Value<T> | { outputs: { value: Value<T> } }) {
-    if (value instanceof ShaderNode) {
-      this.value = value.outputs.value as Value<T>
-    } else {
-      this.value = value as Value<T>
-    }
-  }
-
-  renderValue(): string | undefined {
-    /* TODO: add support for Vector3 and other Three types */
-
-    if (this.value === undefined) {
-      return undefined
-    } else if (typeof this.value === "number") {
-      /* TODO: use the better algorithm here that maintains precision */
-      return this.value.toFixed(5)
-    } else if (typeof this.value === "string") {
-      return this.value
-    } else if (this.value instanceof Variable) {
-      return this.value.globalName
-    } else {
-      throw new Error("Could not render value: " + this.value)
-    }
-  }
-
-  renderWithName(name: string, includeValue = true) {
-    const value = includeValue ? this.renderValue() : undefined
-    return `${this.type} ${name}${value ? ` = ${value}` : ""};`
-  }
-
-  render(includeValue = true) {
-    return this.renderWithName(this.globalName, includeValue)
-  }
-}
-
 /*
 
  _______  _______  __   __  _______  ___   ___      _______  ______    _______
@@ -199,140 +108,9 @@ export class Variable<T extends GLSLType = any> {
 
 */
 
-export class Compiler {
-  constructor(public root: ShaderNode) {}
+export const Compiler = (root: ShaderNode) => {
+  const vertexShader = "/* yo */"
+  const fragmentShader = "/* yo */"
 
-  private renderedDependencies: ShaderNode[] = []
-  private nodeCounter = 0
-
-  compile() {
-    this.prepareNode()
-
-    const vertexShader = this.compileProgram("vertex")
-    const fragmentShader = this.compileProgram("fragment")
-
-    const uniforms = { u_time: { value: 0 } }
-
-    const update = (dt: number) => {
-      uniforms.u_time.value += dt
-    }
-
-    return { vertexShader, fragmentShader, uniforms, update }
-  }
-
-  private prepareNode(node: ShaderNode = this.root) {
-    /* Generate slug if none is given */
-    node.slug =
-      node.slug ||
-      node.name
-        .replace(/[^a-zA-Z0-9_]/g, "_")
-        .replace(/_{2,}/g, "_")
-        .toLowerCase()
-
-    /* Assign better names to output variables */
-    for (const [localName, variable] of Object.entries(node.outputs)) {
-      variable.globalName = [
-        node.slug,
-        this.nodeCounter++,
-        variable.type,
-        localName
-      ].join("_")
-    }
-
-    node.getDependencies().forEach((dependency) => this.prepareNode(dependency))
-  }
-
-  private compileProgram(programType: ProgramType) {
-    this.renderedDependencies = []
-    const header = this.compileProgramHeader(this.root, programType)
-
-    this.renderedDependencies = []
-    const body = this.compileProgramBody(this.root, programType)
-
-    return `
-      ${header}
-
-      void main() {
-        ${body}
-      }
-    `
-  }
-
-  private compileProgramHeader(
-    node: ShaderNode,
-    programType: ProgramType
-  ): string {
-    if (this.renderedDependencies.includes(node)) return ""
-    this.renderedDependencies.push(node)
-
-    const dependencies = node.getDependencies()
-
-    return `
-      ${dependencies
-        .map((dependency) => this.compileProgramHeader(dependency, programType))
-        .join("\n\n")}
-
-      /*** BEGIN: ${node.name} ***/
-      ${node[programType].header ?? ""}
-      /*** END: ${node.name} ***/
-    `
-  }
-
-  private compileProgramBody(
-    node: ShaderNode,
-    programType: ProgramType
-  ): string {
-    if (this.renderedDependencies.includes(node)) return ""
-    this.renderedDependencies.push(node)
-
-    const dependencies = node.getDependencies()
-
-    return `
-      ${dependencies
-        .map((dependency) => this.compileProgramBody(dependency, programType))
-        .join("\n\n\n")}
-
-      /*** BEGIN: ${node.name} ***/
-
-      /* Output Variables */
-      ${this.renderVariables(node.outputs, (localName, variable) =>
-        variable.render(false)
-      )}
-
-      {
-        /* Input Variables */
-        ${this.renderVariables(node.inputs, (localName, variable) =>
-          variable.renderWithName("in_" + localName, true)
-        )}
-
-        /* Local Output Variables */
-        ${this.renderVariables(node.outputs, (localName, variable) =>
-          variable.renderWithName("out_" + localName)
-        )}
-
-        /* Body Chunk */
-        {
-          ${node[programType].body ?? ""}
-        }
-
-        /* TODO: Assign Output Variables */
-        ${this.renderVariables(
-          node.outputs,
-          (localName, variable) => `
-        ${variable.globalName} = out_${localName};`
-        )}
-      }
-
-      /*** END: ${node.name} ***/
-    `
-  }
-
-  private renderVariables(
-    variables: Variables,
-    callback: (localName: string, variable: Variable) => string
-  ) {
-    return Object.entries(variables)
-      .map(([name, variable]) => callback(name, variable))
-      .join("\n")
-  }
+  return { vertexShader, fragmentShader }
 }
