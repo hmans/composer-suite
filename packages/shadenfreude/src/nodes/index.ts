081bf9be9303e1f105c1da26da5ec6c177d6051d
Hendrik Mans <hendrik@mans.de>
2022-07-01 13:00:40 +0200

    Move TimeNode into inputs

--- packages/shadenfreude/src/nodes/index.ts
+++ packages/shadenfreude/src/nodes/index.ts
@@ -1,27 +1,3 @@
-import { Factory, float } from "../shadenfreude"
 export * from "./inputs"
 export * from "./math"
 export * from "./values"
-
-export const TimeNode = Factory(() => ({
-  name: "Time",
-
-  out: {
-    /** The absolute time, in seconds */
-    value: float("u_time"),
-
-    /** Sine of the times */
-    sin: float("sin(u_time)"),
-
-    /** Cosine of the times */
-    cos: float("cos(u_time)")
-  },
-
-  vertex: {
-    header: "uniform float u_time;"
-  },
-
-  fragment: {
-    header: "uniform float u_time;"
-  }
-}))
--- packages/shadenfreude/src/nodes/inputs.ts
+++ packages/shadenfreude/src/nodes/inputs.ts
@@ -1,4 +1,4 @@
-import { Factory, vec3 } from "../shadenfreude"
+import { Factory, float, vec3 } from "../shadenfreude"
 
 export const PositionNode = Factory(() => ({
   name: "Position",
@@ -7,5 +7,28 @@ export const PositionNode = Factory(() => ({
   },
   out: {
     value: vec3("v_position")
+  }
+}))
+
+export const TimeNode = Factory(() => ({
+  name: "Time",
+
+  out: {
+    /** The absolute time, in seconds */
+    value: float("u_time"),
+
+    /** Sine of the times */
+    sin: float("sin(u_time)"),
+
+    /** Cosine of the times */
+    cos: float("cos(u_time)")
+  },
+
+  vertex: {
+    header: "uniform float u_time;"
+  },
+
+  fragment: {
+    header: "uniform float u_time;"
   }
 }))
