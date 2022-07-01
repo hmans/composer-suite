03fe5c5a7c5872cd70d7be8bc1785d94288bcc31
Hendrik Mans <hendrik@mans.de>
2022-07-01 14:01:45 +0200

    ColorNode

--- packages/shadenfreude/src/nodes/values.ts
+++ packages/shadenfreude/src/nodes/values.ts
@@ -1,7 +1,7 @@
 import { Factory, ValueType, variable } from "../shadenfreude"
 
 const ValueNode = <T extends ValueType>(type: T) => ({
-  name: "Float Value",
+  name: `Value (${type})`,
   in: { value: variable(type) },
   out: { value: variable(type, "in_value") }
 })
@@ -10,6 +10,7 @@ export const BooleanNode = Factory(() => ValueNode("bool"))
 export const FloatNode = Factory(() => ValueNode("float"))
 export const Vector2Node = Factory(() => ValueNode("vec2"))
 export const Vector3Node = Factory(() => ValueNode("vec3"))
+export const ColorNode = Factory(() => ValueNode("vec3"))
 export const Vector4Node = Factory(() => ValueNode("vec4"))
 export const Matrix3Node = Factory(() => ValueNode("mat3"))
 export const Matrix4Node = Factory(() => ValueNode("mat4"))
