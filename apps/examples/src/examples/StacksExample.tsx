6585d9ee4867a1edf06490ebfa95c01f8c22aafa
Hendrik Mans <hendrik@mans.de>
2022-07-03 19:11:57 +0200

    useShader

--- apps/examples/src/examples/StacksExample.tsx
+++ apps/examples/src/examples/StacksExample.tsx
@@ -17,6 +17,7 @@ import {
 import { Color, MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
+import { useShader } from "./useShader"
 
 const ScaleWithTime = (axis = "xyz") =>
   Factory(() => ({
@@ -70,47 +71,42 @@ const MoveWithTime = (axis = "xyz") =>
     }
   }))
 
-function useShader() {
-  const root = CustomShaderMaterialMasterNode({
-    position: ShaderNode({
-      name: "Animation Stack",
-      outputs: { value: vec3(VertexPositionNode()) },
-      filters: [
-        SqueezeWithTime({ frequency: 0.1 }),
-        ScaleWithTime("x")({ frequency: 0.2 }),
-        ScaleWithTime("y")({ frequency: 0.1 }),
-        ScaleWithTime("z")({ frequency: 0.3 }),
-        MoveWithTime("x")({ frequency: 0.8, amplitude: 2 }),
-        MoveWithTime("y")({ frequency: 0.6, amplitude: 1 }),
-        MoveWithTime("z")({ frequency: 0.3, amplitude: 2 })
-      ]
-    }),
-
-    diffuseColor: ShaderNode({
-      name: "Color Stack",
-      outputs: { value: vec3(new Color("#3dd")) },
-      filters: [
-        BlendNode({
-          mode: "softlight",
-          opacity: 0.8,
-          b: MultiplyNode({
-            a: new Color(2, 2, 2) as Parameter<"vec3">,
-            b: FresnelNode()
-          })
-        })
-      ]
-    })
-  })
-
-  const [shader, update] = compileShader(root)
-
-  useFrame((_, dt) => update(dt))
-
-  return shader
-}
-
 export default function StacksExample() {
-  const shader = useShader()
+  const shader = useShader(
+    () =>
+      CustomShaderMaterialMasterNode({
+        position: ShaderNode({
+          name: "Animation Stack",
+          outputs: { value: vec3(VertexPositionNode()) },
+          filters: [
+            SqueezeWithTime({ frequency: 0.1 }),
+            ScaleWithTime("x")({ frequency: 0.2 }),
+            ScaleWithTime("y")({ frequency: 0.1 }),
+            ScaleWithTime("z")({ frequency: 0.3 }),
+            MoveWithTime("x")({ frequency: 0.8, amplitude: 2 }),
+            MoveWithTime("y")({ frequency: 0.6, amplitude: 1 }),
+            MoveWithTime("z")({ frequency: 0.3, amplitude: 2 })
+          ]
+        }),
+
+        diffuseColor: ShaderNode({
+          name: "Color Stack",
+          outputs: { value: vec3(new Color("#3dd")) },
+          filters: [
+            BlendNode({
+              mode: "softlight",
+              opacity: 0.8,
+              b: MultiplyNode({
+                a: new Color(2, 2, 2) as Parameter<"vec3">,
+                b: FresnelNode()
+              })
+            })
+          ]
+        })
+      }),
+    []
+  )
+
   const material = useRef<CustomShaderMaterialImpl>(null!)
 
   console.log(shader.vertexShader)
