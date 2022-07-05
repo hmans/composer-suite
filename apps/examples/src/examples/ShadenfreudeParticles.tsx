a30ae31e3bbeed92ff84e2fbbf9b882d9b6f082f
Hendrik Mans <hendrik@mans.de>
2022-07-05 16:04:59 +0200

    Set up color stack

--- apps/examples/src/examples/ShadenfreudeParticles.tsx
+++ apps/examples/src/examples/ShadenfreudeParticles.tsx
@@ -1,10 +1,40 @@
-import { useLayoutEffect, useRef } from "react"
-import { Matrix4, MeshStandardMaterial } from "three"
+import { useFrame } from "@react-three/fiber"
+import { useLayoutEffect, useMemo, useRef } from "react"
+import {
+  compileShader,
+  CustomShaderMaterialMasterNode,
+  IShaderNode,
+  ShaderNode,
+  vec3
+} from "shadenfreude"
+import { Color, Matrix4, MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 import { Particles } from "three-vfx"
 
+const useShader = (fac: () => IShaderNode) => {
+  const [shader, update] = useMemo(() => compileShader(fac()), [])
+  useFrame((_, dt) => update(dt))
+  return shader
+}
+
 export default function ShadenfreudeParticles() {
   const imesh = useRef<Particles>(null!)
+
+  const shader = useShader(() => {
+    const diffuseColor = ShaderNode({
+      name: "Color Stack",
+      inputs: {
+        a: vec3(new Color("hotpink"))
+      },
+      outputs: {
+        value: vec3("inputs.a")
+      }
+    })
+
+    return CustomShaderMaterialMasterNode({
+      diffuseColor
+    })
+  })
 
   useLayoutEffect(() => {
     /* Spawn a single particle */
@@ -16,7 +46,7 @@ export default function ShadenfreudeParticles() {
     <group position-y={15}>
       <Particles ref={imesh}>
         <sphereGeometry />
-        <CustomShaderMaterial baseMaterial={MeshStandardMaterial} />
+        <CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} />
       </Particles>
     </group>
   )
