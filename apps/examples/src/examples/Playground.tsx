25e3224777397aec1467146a6b6813f0ea3b74de
Hendrik Mans <hendrik@mans.de>
2022-07-05 13:11:18 +0200

    Cleanup

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,77 +1,13 @@
-import { useFrame } from "@react-three/fiber"
-import { useMemo, useRef } from "react"
+import { useRef } from "react"
 import {
   AddNode,
-  BlendNode,
   ColorNode,
-  compileShader,
-  ComposeNode,
-  Factory,
-  float,
-  Parameter,
   ShaderMaterialMasterNode,
   ShaderNode,
-  TimeNode,
-  UVNode,
-  ValueType,
-  vec2,
   vec3
 } from "shadenfreude"
 import { Color, ShaderMaterial, Vector3 } from "three"
 import { useShader } from "./useShader"
-
-const WobbleNode = Factory(() => ({
-  name: "Wobble",
-
-  inputs: {
-    /** The time offset */
-    time: float(),
-
-    /** How fast to wobble */
-    frequency: float(1),
-
-    /** How hard to wobble */
-    amplitude: float(1)
-  },
-
-  outputs: {
-    /** The wobble value */
-    value: float("sin(inputs.time * inputs.frequency) * inputs.amplitude")
-  }
-}))
-
-type WobbleProps = {
-  frequency?: number
-  amplitude?: number
-  time?: Parameter<"float">
-}
-
-const WobbleAnimation = ({
-  frequency = 1,
-  amplitude = 1,
-  time = TimeNode()
-}: WobbleProps) =>
-  ComposeNode({
-    x: WobbleNode({ time, frequency: 8 * frequency, amplitude }),
-    y: WobbleNode({ time, frequency: 5 * frequency, amplitude }),
-    z: WobbleNode({ time, frequency: 3 * frequency, amplitude })
-  }).outputs.vector3
-
-const Squeezed = Factory(() => ({
-  inputs: {
-    position: vec3(),
-    time: float(),
-    uv: vec2(UVNode())
-  },
-  outputs: {
-    value: vec3(
-      `vec3(
-        inputs.position.x * (1.0 + sin(inputs.time * 2.0 + inputs.uv.y * 13.0) * 0.3),
-        inputs.position.y * (1.0 + cos(inputs.time * 2.3 + inputs.uv.y * 11.0) * 0.2),
-        inputs.position.z * (1.0 + sin(inputs.time * 2.0 + inputs.uv.y * 13.0) * 0.1))`
-    )
-  }
-}))
 
 export default function Playground() {
   const shaderProps = useShader(() => {
