26dec14d7150bec010659642af79898fe754fbdb
Hendrik Mans <hendrik@mans.de>
2022-07-05 13:24:56 +0200

    Eh

--- apps/examples/src/examples/RaymarchingExample.tsx
+++ apps/examples/src/examples/RaymarchingExample.tsx
@@ -1,37 +1,29 @@
 import { useRef } from "react"
-import {
-  AddNode,
-  ColorNode,
-  ShaderMaterialMasterNode,
-  ShaderNode,
-  vec3
-} from "shadenfreude"
-import { Color, ShaderMaterial, Vector3 } from "three"
+import { Factory, ShaderMaterialMasterNode, vec3 } from "shadenfreude"
+import { ShaderMaterial } from "three"
 import { useShader } from "./useShader"
+
+const RaymarchingNode = Factory(() => ({
+  name: "Raymarching Example",
+
+  outputs: {
+    value: vec3()
+  },
+
+  fragment: {
+    body: /*glsl*/ `
+
+    // vec2 screenPos = (2.0 * gl_FragCoord.xy - resolution) / resolution.y;
+
+    outputs.value = vec3(1.0, 0.5, 0.2);
+    `
+  }
+}))
 
 export default function RaymarchingExample() {
   const shaderProps = useShader(() => {
-    const colorA = ColorNode({ a: new Color("#f33") })
-    const colorB = ColorNode({ a: new Color(0, 1, 0) })
-
-    const l = AddNode({
-      a: new Vector3(),
-      b: colorB
-    })
-
-    const colorStack = ShaderNode({
-      name: "Color Stack",
-      inputs: {
-        color: vec3(colorA)
-      },
-      outputs: {
-        value: vec3("inputs.color")
-      },
-      filters: [l]
-    })
-
     return ShaderMaterialMasterNode({
-      color: colorStack
+      color: RaymarchingNode()
     })
   })
   const material = useRef<ShaderMaterial>(null!)
@@ -42,15 +34,8 @@ export default function RaymarchingExample() {
   return (
     <group position-y={15}>
       <mesh>
-        <sphereGeometry args={[8, 32, 32]} />
-
+        <planeGeometry args={[30, 20]} />
         <shaderMaterial ref={material} {...shaderProps} />
-
-        {/* <CustomShaderMaterial
-          baseMaterial={MeshStandardMaterial}
-          {...shaderProps}
-          ref={material}
-        /> */}
       </mesh>
     </group>
   )
