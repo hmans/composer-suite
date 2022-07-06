4e3359a288808d74934e02e7582bfa215174a863
Hendrik Mans <hendrik@mans.de>
2022-07-06 21:12:17 +0200

    More vectors

--- packages/shadenfreude/src/glslRepresentation.ts
+++ packages/shadenfreude/src/glslRepresentation.ts
@@ -1,4 +1,4 @@
-import { Color, Vector3 } from "three"
+import { Color, Vector2, Vector3, Vector4 } from "three"
 import { isVariable, Value } from "./variables"
 
 export const glslRepresentation = (value: Value): string => {
@@ -11,14 +11,36 @@ export const glslRepresentation = (value: Value): string => {
   if (typeof value === "number") return value.toFixed(5)
 
   if (value instanceof Color)
-    return `vec3(${glslRepresentation(value.r)}, ${glslRepresentation(
-      value.g
-    )}, ${glslRepresentation(value.b)})`
+    return `
+      vec3(
+        ${glslRepresentation(value.r)},
+        ${glslRepresentation(value.g)},
+        ${glslRepresentation(value.b)}
+      )`
+
+  if (value instanceof Vector2)
+    return `
+      vec3(
+        ${glslRepresentation(value.x)},
+        ${glslRepresentation(value.y)},
+      )`
 
   if (value instanceof Vector3)
-    return `vec3(${glslRepresentation(value.x)}, ${glslRepresentation(
-      value.y
-    )}, ${glslRepresentation(value.z)})`
+    return `
+      vec3(
+        ${glslRepresentation(value.x)},
+        ${glslRepresentation(value.y)},
+        ${glslRepresentation(value.z)}
+      )`
+
+  if (value instanceof Vector4)
+    return `
+      vec4(
+        ${glslRepresentation(value.x)},
+        ${glslRepresentation(value.y)},
+        ${glslRepresentation(value.z)}
+        ${glslRepresentation(value.w)}
+      )`
 
   /* Fail */
   throw new Error(`Could not render value to GLSL: ${value}`)
