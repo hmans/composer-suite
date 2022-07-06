781228dbc4c2b8eb27fc4190e37f105d3ebb4ced
Hendrik Mans <hendrik@mans.de>
2022-07-06 21:17:31 +0200

    Cleanup math module

--- packages/shadenfreude/src/nodes/math.ts
+++ packages/shadenfreude/src/nodes/math.ts
@@ -1,37 +1,29 @@
 import { type } from "../glslType"
 import { float, GLSLType, Value, variable } from "../variables"
 
-export const Addition = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
+export const Add = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
   variable(type(a), "a + b", {
     title: `Addition (${type(a)} + ${type(b)}`,
     inputs: { a, b }
   })
 
-export const Subtraction = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
+export const Subtract = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
   variable(type(a), "a - b", {
     title: `Subtraction (${type(a)} - ${type(b)}`,
     inputs: { a, b }
   })
 
-export const Division = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
+export const Divide = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
   variable(type(a), "a / b", {
     title: `Division (${type(a)} / ${type(b)}`,
     inputs: { a, b }
   })
 
-export const Multiplication = <T extends GLSLType>(
-  a: Value<T>,
-  b: Value<any>
-) =>
+export const Multiply = <T extends GLSLType>(a: Value<T>, b: Value<any>) =>
   variable(type(a), "a * b", {
     title: `Multiplication (${type(a)} * ${type(b)}`,
     inputs: { a, b }
   })
-
-export const Add = Addition
-export const Subtract = Subtraction
-export const Divide = Division
-export const Multiply = Multiplication
 
 export const Sin = (x: Value<"float">) => float("sin(x)", { inputs: { x } })
 export const Cos = (x: Value<"float">) => float("cos(x)", { inputs: { x } })
