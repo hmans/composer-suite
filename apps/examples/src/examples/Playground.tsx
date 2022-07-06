f7932a6fa6fe8928db2e7b9e7200a88ac0613d11
Hendrik Mans <hendrik@mans.de>
2022-07-06 20:47:26 +0200

    Almost

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -2,27 +2,36 @@ import { useFrame } from "@react-three/fiber"
 import { useMemo } from "react"
 import {
   Add,
+  assignment,
   bool,
   compileShader,
   Join,
   Multiply,
   Sin,
   Time,
+  Value,
   Variable,
+  Varying,
   vec3
 } from "shadenfreude"
-import { Color, MeshStandardMaterial } from "three"
+import { Color, MeshStandardMaterial, Vector3 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 type CSMMasterProps = {
-  diffuseColor: Variable<"vec3">
+  position?: Value<"vec3">
+  diffuseColor?: Value<"vec3">
 }
 
-const CSMMaster = ({ diffuseColor }: CSMMasterProps) =>
+const VertexPosition = Varying("vec3", vec3("position"))
+
+console.log(VertexPosition)
+
+const CSMMaster = (inputs: CSMMasterProps) =>
   bool(true, {
     title: "Master",
-    inputs: { diffuseColor },
-    fragmentBody: `csm_DiffuseColor = vec4(diffuseColor, 1.0);`
+    inputs,
+    vertexBody: assignment("csm_Position", "position"),
+    fragmentBody: assignment("csm_DiffuseColor", "vec4(diffuseColor, 1.0)")
   })
 
 export default function Playground() {
@@ -34,7 +43,8 @@ export default function Playground() {
     const v4 = Join(1, 1, 1, 1)
 
     const root = CSMMaster({
-      diffuseColor: Multiply(baseColor, Add(1, Multiply(Sin(Time), 0.5)))
+      position: VertexPosition
+      // diffuseColor: Multiply(baseColor, Add(1, Multiply(Sin(Time), 0.5)))
     })
 
     return compileShader(root)
--- packages/shadenfreude/src/nodes/inputs.ts
+++ packages/shadenfreude/src/nodes/inputs.ts
@@ -1,4 +1,5 @@
-import { GLSLType, float } from "../variables"
+import idGenerator from "../lib/idGenerator"
+import { GLSLType, float, variable, Value } from "../variables"
 
 export const Uniform = (type: GLSLType, name: string) =>
   float(name, {
@@ -8,3 +9,21 @@ export const Uniform = (type: GLSLType, name: string) =>
   })
 
 export const Time = Uniform("float", "u_time")
+
+const nextVaryingId = idGenerator()
+
+export const Varying = <T extends GLSLType>(type: T, source: Value<T>) => {
+  const varyingName = `v_${nextVaryingId()}`
+
+  return variable(type, varyingName, {
+    title: `Varying: ${varyingName}`,
+
+    inputs: {
+      source
+    },
+
+    vertexHeader: `varying ${type} ${varyingName};`,
+    vertexBody: `${varyingName} = source;`,
+    fragmentHeader: `varying ${type} ${varyingName};`
+  })
+}
