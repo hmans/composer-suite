56b105e53e114e098ceb72a189a83a8a659b9647
Hendrik Mans <hendrik@mans.de>
2022-07-07 10:10:51 +0200

    Unbreak glslType

--- packages/shadenfreude/src/glslType.ts
+++ packages/shadenfreude/src/glslType.ts
@@ -1,16 +1,16 @@
 import { Color, Matrix3, Matrix4, Vector2, Vector3, Vector4 } from "three"
-import { GLSLType, isVariable, Value } from "./variables"
+import { GLSLType, Value, isVariable } from "./variables"
 
-export const glslType = (value: Value): GLSLType => {
+export const glslType = <T extends GLSLType>(value: Value<T>): T => {
   if (isVariable(value)) return value.type
 
-  if (typeof value === "number") return "float"
-  if (value instanceof Vector4) return "vec4"
-  if (value instanceof Vector3) return "vec3"
-  if (value instanceof Vector2) return "vec2"
-  if (value instanceof Matrix3) return "mat3"
-  if (value instanceof Matrix4) return "mat4"
-  if (value instanceof Color) return "vec3"
+  if (typeof value === "number") return "float" as T
+  if (value instanceof Vector4) return "vec4" as T
+  if (value instanceof Vector3) return "vec3" as T
+  if (value instanceof Vector2) return "vec2" as T
+  if (value instanceof Matrix3) return "mat3" as T
+  if (value instanceof Matrix4) return "mat4" as T
+  if (value instanceof Color) return "vec3" as T
 
   /* Fail */
   throw new Error(`Could not render GLSL type for: ${value}`)
