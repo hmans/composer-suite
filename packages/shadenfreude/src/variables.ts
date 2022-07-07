81184da4beba9937ebb36c4eab9fcf8f356fa5b4
Hendrik Mans <hendrik@mans.de>
2022-07-07 13:14:57 +0200

    Remove built-in operator shortcuts

--- packages/shadenfreude/src/variables.ts
+++ packages/shadenfreude/src/variables.ts
@@ -38,21 +38,9 @@ export type Variable<T extends GLSLType = any> = {
   vertexBody?: string
   fragmentHeader?: string
   fragmentBody?: string
-
-  /*
-  Some convenient helpers to directly apply math operations to variables.
-  Do these increase complexity too much for some cute glue? Maybe, maybe not.
-  */
-  Add: (...operands: Value[]) => Variable<T>
-  Subtract: (...operands: Value[]) => Variable<T>
-  Multiply: (...operands: Value[]) => Variable<T>
-  Divide: (...operands: Value[]) => Variable<T>
 }
 
 const nextAnonymousId = idGenerator()
-
-const buildMultiInputs = (values: Value[]) =>
-  values.reduce((acc, v, i) => ({ ...acc, [`m_${i}`]: v }), {})
 
 export const Variable = <T extends GLSLType>(
   type: T,
@@ -69,13 +57,7 @@ export const Variable = <T extends GLSLType>(
     ...extras,
     _: "Variable",
     type,
-    value,
-
-    /* Math helpers! */
-    Add: (...operands: Value[]) => Add(v, ...operands),
-    Subtract: (...operands: Value[]) => Subtract(v, ...operands),
-    Multiply: (...operands: Value[]) => Multiply(v, ...operands),
-    Divide: (...operands: Value[]) => Divide(v, ...operands)
+    value
   }
 
   return v
@@ -103,33 +85,3 @@ export const Vec3 = makeVariableHelper("vec3")
 export const Vec4 = makeVariableHelper("vec4")
 export const Mat3 = makeVariableHelper("mat3")
 export const Mat4 = makeVariableHelper("mat4")
-
-/*** OPERATORS ***
-
-TODO: Move this somewhere else!
-
-It used to be in maths.ts, which caused a circular dependency. Pretty much everything
-was fine with this except for ts-jest, which objected strongly, and crashed.
-*/
-
-export const Operator = (title: string, operator: "+" | "-" | "*" | "/") => <
-  T extends GLSLType
->(
-  a: Value<T>,
-  ...rest: Value[]
-) => {
-  const inputs = buildMultiInputs([a, ...rest])
-
-  /* a + b + c + ... */
-  const expression = Object.keys(inputs).join(operator)
-
-  return Variable(type(a), expression, {
-    title: `${title} (${type(a)})`,
-    inputs
-  })
-}
-
-export const Add = Operator("Add", "+")
-export const Subtract = Operator("Subtract", "-")
-export const Multiply = Operator("Multiply", "*")
-export const Divide = Operator("Divide", "/")
