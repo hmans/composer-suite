0573d4ba6f866cd8e8273cf935368a527fe8a9e9
Hendrik Mans <hendrik@mans.de>
2022-07-07 12:47:32 +0200

    Add alpha to CSM Master

--- packages/shadenfreude/src/nodes/masters.ts
+++ packages/shadenfreude/src/nodes/masters.ts
@@ -6,23 +6,26 @@ import { VertexPosition } from "./geometry"
 export type CustomShaderMaterialMasterprops = {
   position?: Value<"vec3">
   diffuseColor?: Value<"vec3">
+  alpha?: Value<"float">
 }
 
 export const CustomShaderMaterialMaster = ({
   diffuseColor = new Color(1, 1, 1),
-  position = VertexPosition
+  position = VertexPosition,
+  alpha = 1
 }: CustomShaderMaterialMasterprops) =>
   Bool(true, {
     title: "CustomShaderMaterial Master",
 
     inputs: {
       position,
-      diffuseColor
+      diffuseColor,
+      alpha
     },
 
     vertexBody: concatenate(assignment("csm_Position", "position")),
 
     fragmentBody: concatenate(
-      assignment("csm_DiffuseColor", "vec4(diffuseColor, 1.0)")
+      assignment("csm_DiffuseColor", "vec4(diffuseColor, alpha)")
     )
   })
