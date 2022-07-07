aaa8d05571a93d462f7cc9d718170de0bac22498
Hendrik Mans <hendrik@mans.de>
2022-07-07 10:58:17 +0200

    Tweak CSM Master

--- packages/shadenfreude/src/nodes/masters.ts
+++ packages/shadenfreude/src/nodes/masters.ts
@@ -1,24 +1,28 @@
+import { Color } from "three"
 import { assignment, concatenate } from "../lib/concatenator3000"
-import { Value, Bool } from "../variables"
+import { Value, Bool, Vec3 } from "../variables"
+import { VertexPosition } from "./geometry"
 
 export type CustomShaderMaterialMasterprops = {
   position?: Value<"vec3">
   diffuseColor?: Value<"vec3">
 }
 
-export const CustomShaderMaterialMaster = (
-  inputs: CustomShaderMaterialMasterprops
-) =>
+export const CustomShaderMaterialMaster = ({
+  diffuseColor = new Color(1, 1, 1),
+  position = VertexPosition
+}: CustomShaderMaterialMasterprops) =>
   Bool(true, {
     title: "CustomShaderMaterial Master",
-    inputs,
 
-    vertexBody: concatenate(
-      inputs.position && assignment("csm_Position", "position")
-    ),
+    inputs: {
+      position,
+      diffuseColor
+    },
+
+    vertexBody: concatenate(assignment("csm_Position", "position")),
 
     fragmentBody: concatenate(
-      inputs.diffuseColor &&
-        assignment("csm_DiffuseColor", "vec4(diffuseColor, 1.0)")
+      assignment("csm_DiffuseColor", "vec4(diffuseColor, 1.0)")
     )
   })
