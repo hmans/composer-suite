7523417024e4250d34d51b85e38f480dc1ba30cd
Hendrik Mans <hendrik@mans.de>
2022-07-07 09:47:59 +0200

    Split (typings WIP)

--- packages/shadenfreude/src/nodes/vectors.test.ts
+++ packages/shadenfreude/src/nodes/vectors.test.ts
@@ -1,4 +1,6 @@
-import { Join } from "./vectors"
+import { Vector2, Vector3, Vector4 } from "three"
+import { vec3, vec4 } from "../variables"
+import { Join, Split } from "./vectors"
 
 describe("Join", () => {
   it("creates a vec2 variable from two components", () => {
@@ -23,5 +25,37 @@ describe("Join", () => {
     expect(v.inputs.y).toBe(2)
     expect(v.inputs.z).toBe(3)
     expect(v.inputs.w).toBe(4)
+  })
+})
+
+describe("Split", () => {
+  it("splits a vec2 into two components", () => {
+    const split = Split(new Vector2(1, 2))
+
+    expect(split).toMatchObject({
+      x: { type: "float", value: "vector.x" },
+      y: { type: "float", value: "vector.y" }
+    })
+  })
+
+  it("splits a vec3 into three components", () => {
+    const split = Split(new Vector3(1, 2, 3))
+
+    expect(split).toMatchObject({
+      x: { type: "float", value: "vector.x" },
+      y: { type: "float", value: "vector.y" },
+      z: { type: "float", value: "vector.z" }
+    })
+  })
+
+  it("splits a vec4 into four components", () => {
+    const split = Split(new Vector4(1, 2, 3))
+
+    expect(split).toMatchObject({
+      x: { type: "float", value: "vector.x" },
+      y: { type: "float", value: "vector.y" },
+      z: { type: "float", value: "vector.z" },
+      w: { type: "float", value: "vector.w" }
+    })
   })
 })
--- packages/shadenfreude/src/nodes/vectors.ts
+++ packages/shadenfreude/src/nodes/vectors.ts
@@ -1,10 +1,14 @@
-import { Value, float, vec3, Variable, vec4, vec2 } from "../variables"
-
-export const SplitVector3 = (vector: Value<"vec3">) => ({
-  x: float("vector.x", { inputs: { vector } }),
-  y: float("vector.y", { inputs: { vector } }),
-  z: float("vector.z", { inputs: { vector } })
-})
+import { type } from "../glslType"
+import {
+  Value,
+  float,
+  vec3,
+  Variable,
+  vec4,
+  vec2,
+  GLSLType,
+  isType
+} from "../variables"
 
 export type Vector2Components = [Value<"float">, Value<"float">]
 export type Vector3Components = [Value<"float">, Value<"float">, Value<"float">]
@@ -43,4 +47,60 @@ export const Join = <
   }
 
   return vec2("vec2(x, y)", { inputs: { x, y } }) as JoinReturnType<Args>
+}
+
+export const SplitVector2 = (vector: Value<"vec2">): SplitVector<"vec2"> => ({
+  x: float("vector.x", { inputs: { vector } }),
+  y: float("vector.y", { inputs: { vector } })
+})
+
+export const SplitVector3 = (vector: Value<"vec3">): SplitVector<"vec3"> => ({
+  x: float("vector.x", { inputs: { vector } }),
+  y: float("vector.y", { inputs: { vector } }),
+  z: float("vector.z", { inputs: { vector } })
+})
+
+export const SplitVector4 = (vector: Value<"vec4">): SplitVector<"vec4"> => ({
+  x: float("vector.x", { inputs: { vector } }),
+  y: float("vector.y", { inputs: { vector } }),
+  z: float("vector.z", { inputs: { vector } }),
+  w: float("vector.w", { inputs: { vector } })
+})
+
+type VectorTypes = "vec2" | "vec3" | "vec4"
+
+type SplitVector<T extends VectorTypes> = T extends "vec4"
+  ? {
+      x: Value<"float">
+      y: Value<"float">
+      z: Value<"float">
+      w: Value<"float">
+    }
+  : T extends "vec3"
+  ? { x: Value<"float">; y: Value<"float">; z: Value<"float"> }
+  : T extends "vec2"
+  ? { x: Value<"float">; y: Value<"float"> }
+  : never
+
+export const Split = <T extends "vec2" | "vec3" | "vec4">(vector: Value<T>) => {
+  if (isType(vector, "vec4"))
+    return SplitVector4(vector) as {
+      x: Value<"float">
+      y: Value<"float">
+      z: Value<"float">
+      w: Value<"float">
+    }
+
+  if (isType(vector, "vec3"))
+    return SplitVector3(vector) as {
+      x: Value<"float">
+      y: Value<"float">
+      z: Value<"float">
+    }
+  if (isType(vector, "vec2"))
+    return SplitVector2(vector) as {
+      x: Value<"float">
+      y: Value<"float">
+    }
+  throw new Error("Could not split value: " + vector)
 }
