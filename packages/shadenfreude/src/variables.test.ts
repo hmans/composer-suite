64af4a1d22328a3d425620a9eafa040f1292b88d
Hendrik Mans <hendrik@mans.de>
2022-07-08 12:08:22 +0200

    More variable tests

--- packages/shadenfreude/src/variables.test.ts
+++ packages/shadenfreude/src/variables.test.ts
@@ -1,16 +1,27 @@
 import { compileShader } from "./compilers"
-import { Variable } from "./variables"
+import { glslRepresentation } from "./glslRepresentation"
+import { Float, Variable } from "./variables"
+
+const glsl = glslRepresentation
 
 describe("variable", () => {
   it("creates a variable of the specified type and value", () => {
     const v = Variable("float", 1)
     expect(v.type).toBe("float")
     expect(v.value).toBe(1)
+    expect(glsl(v.value)).toBe("1.0")
   })
 
   it("supports string values (which will be used as verbatim expressions)", () => {
     const v = Variable("vec3", "vec3(1.0, 1.0, 1.0)")
-    expect(v.value).toBe("vec3(1.0, 1.0, 1.0)")
+    expect(glsl(v.value)).toBe("vec3(1.0, 1.0, 1.0)")
+  })
+
+  it("allows variables to directly reference other variables", () => {
+    const source = Float(1)
+    const v = Float(source)
+    expect(v.value).toBe(source)
+    expect(glsl(v.value)).toBe(source.name)
   })
 
   it("supports a 'varying' flag that will automatically make it pass its data as a varying", () => {
