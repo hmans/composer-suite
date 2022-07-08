f896e97352f6db1287cda1d5a2016c83d70bd3b9
Hendrik Mans <hendrik@mans.de>
2022-07-08 21:00:51 +0200

    Eh

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,3 +1,4 @@
+import { useTexture } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { useControls } from "leva"
 import { useMemo } from "react"
@@ -14,13 +15,40 @@ import {
   Subtract,
   Time,
   Uniform,
+  Value,
+  Vec2,
   Vec3,
+  Vec4,
   VertexPosition
 } from "shadenfreude"
 import { Color, DoubleSide, MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
+import textureUrl from "./textures/hexgrid.jpeg"
+
+/* Put this into an NPM package, I dare you: */
+const Dissolve = (
+  threshold: Float = 0.5,
+  speed: Float = 1,
+  edgeColor: Vec3 = new Color(0, 10, 8)
+) => {
+  const noise = Simplex3DNoise(Multiply(VertexPosition, speed))
+
+  return {
+    color: Multiply(
+      edgeColor,
+      Smoothstep(Subtract(threshold, 0.2), threshold, noise)
+    ),
+
+    alpha: Step(noise, threshold)
+  }
+}
+
+const SampleTexture = (t: Value<"sampler2D">, xy: Vec2) =>
+  Vec4("texture2D(t, xy)", { inputs: { t, xy } })
 
 export default function Playground() {
+  const texture = useTexture(textureUrl)
+
   const { u_threshold } = useControls("Uniforms", {
     u_threshold: { value: 0, min: -1, max: 1 }
   })
@@ -29,25 +57,8 @@ export default function Playground() {
     console.log("compiling shader")
 
     const parameters = {
-      threshold: Uniform("float", "u_threshold")
-    }
-
-    /* Put this into an NPM package, I dare you: */
-    const Dissolve = (
-      threshold: Float = 0.5,
-      speed: Float = 1,
-      edgeColor: Vec3 = new Color(0, 10, 8)
-    ) => {
-      const noise = Simplex3DNoise(Multiply(VertexPosition, speed))
-
-      return {
-        color: Multiply(
-          edgeColor,
-          Smoothstep(Subtract(threshold, 0.2), threshold, noise)
-        ),
-
-        alpha: Step(noise, threshold)
-      }
+      threshold: Uniform("float", "u_threshold"),
+      texture: Uniform("sampler2D", "u_texture")
     }
 
     /* Use the thing: */
@@ -75,6 +86,7 @@ export default function Playground() {
           baseMaterial={MeshStandardMaterial}
           uniforms={{
             u_threshold: { value: u_threshold },
+            u_texture: { value: texture },
             ...uniforms
           }}
           {...shader}
