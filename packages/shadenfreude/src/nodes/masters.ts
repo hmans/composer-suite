a5a69e7e0864feee51515dcf434cc59ab2c084c1
Hendrik Mans <hendrik@mans.de>
2022-07-11 18:33:32 +0200

    Teach CSMMaster to set normals

--- packages/shadenfreude/src/nodes/masters.ts
+++ packages/shadenfreude/src/nodes/masters.ts
@@ -1,10 +1,11 @@
 import { Color } from "three"
 import { code } from "../expressions"
 import { Bool, Value } from "../tree"
-import { VertexPosition } from "./geometry"
+import { VertexNormal, VertexPosition } from "./geometry"
 
 export type CustomShaderMaterialMasterprops = {
   position?: Value<"vec3">
+  normal?: Value<"vec3">
   diffuseColor?: Value<"vec3">
   alpha?: Value<"float">
 }
@@ -12,10 +13,16 @@ export type CustomShaderMaterialMasterprops = {
 export const CustomShaderMaterialMaster = ({
   diffuseColor = new Color(1, 1, 1),
   position = VertexPosition,
+  normal = VertexNormal,
   alpha = 1
 }: CustomShaderMaterialMasterprops = {}) =>
   Bool(true, {
     name: "CustomShaderMaterial Master",
-    vertexBody: code`csm_Position = ${position};`,
-    fragmentBody: code`csm_DiffuseColor = vec4(${diffuseColor}, ${alpha});`
+    vertexBody: code`
+      csm_Position = ${position};
+      csm_Normal = ${normal};
+    `,
+    fragmentBody: code`
+      csm_DiffuseColor = vec4(${diffuseColor}, ${alpha});
+    `
   })
