aa24b472f25646fbcf2cc99612c2309845b810c0
Hendrik Mans <hendrik@mans.de>
2022-07-11 18:42:04 +0200

    Nice

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -45,19 +45,21 @@ export default function Playground() {
 
     const ModifiedVertex = (v: Value<"vec3">) =>
       Pipe(
-        VertexPosition,
+        Vec3(v),
         ($) => Add($, Multiply(bigwaves, 8)),
         ($) => Add($, Multiply(waves, 4)),
         ($) => Add($, Multiply(ripples, 0.2))
       )
 
-    const tangent = Tangent(VertexNormal)
-    const bitangent = Bitangent(VertexNormal, tangent)
-
+    /* Calculate noises */
     const bigwaves = ScaledNoise(0.008, 0.1)
     const waves = ScaledNoise(0.025, 0.1)
     const ripples = ScaledNoise(5, 0.8)
     const foam = Step(0.5, ScaledNoise(0.1, 0.1))
+
+    /* Adjust vertex normal based on new position */
+    const tangent = Tangent(VertexNormal)
+    const bitangent = Bitangent(VertexNormal, tangent)
 
     const offset = 0.001
     const neighbor1 = Add(VertexPosition, Mul(tangent, offset))
@@ -74,15 +76,15 @@ export default function Playground() {
       normal: Normalize(Cross(displacedTangent, displacedBitangent)),
 
       diffuseColor: Pipe(Vec3(new Color("#99b")), ($) =>
-        Add($, Multiply(foam, 0.03))
+        Add($, Mul(foam, 0.03))
       ),
 
       alpha: 0.9
     })
   }, [])
 
-  // console.log(shader.vertexShader)
-  console.log(shader.fragmentShader)
+  console.log(shader.vertexShader)
+  // console.log(shader.fragmentShader)
 
   return (
     <group position-y={-8}>
