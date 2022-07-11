a36dc9081d510747c0f1db50e99aa8d23adc60ca
Hendrik Mans <hendrik@mans.de>
2022-07-11 21:16:09 +0200

    Cleanup

--- apps/examples/src/examples/ShadenfreudeTextures.tsx
+++ apps/examples/src/examples/ShadenfreudeTextures.tsx
@@ -1,18 +1,40 @@
 import { useTexture } from "@react-three/drei"
-import { CustomShaderMaterialMaster } from "shadenfreude"
-import { MeshStandardMaterial, RepeatWrapping } from "three"
+import {
+  Add,
+  code,
+  CustomShaderMaterialMaster,
+  Fresnel,
+  Mul,
+  Sampler2D,
+  Texture2D,
+  TilingUV,
+  UV,
+  Vec3
+} from "shadenfreude"
+import { Color, MeshStandardMaterial, RepeatWrapping, Vector2 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 import { DustExample } from "./DustExample"
 import hexgrid from "./textures/hexgrid.jpeg"
 import { useShader } from "./useShader"
 
-export default function Playground() {
+export default function ShadenfreudeTextures() {
   const texture = useTexture(hexgrid)
   texture.wrapT = RepeatWrapping
   texture.wrapS = RepeatWrapping
 
   const shader = useShader(() => {
-    return CustomShaderMaterialMaster({})
+    const texture = Sampler2D("u_texture")
+    const sampled = Texture2D(texture, TilingUV(UV, new Vector2(1.5, 1)))
+    const textureColor = Vec3(code`${sampled}.rgb`)
+
+    return CustomShaderMaterialMaster({
+      diffuseColor: Add(
+        Mul(textureColor, new Color("#89c")),
+        Mul(new Color("white"), Fresnel({ intensity: 0.75 }))
+      ),
+
+      alpha: code`${sampled}.a`
+    })
   }, [])
 
   // console.log(shader.vertexShader)
