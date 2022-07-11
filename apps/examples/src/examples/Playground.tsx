5035c0aa268f220f60ba61c5a84acdec45888343
Hendrik Mans <hendrik@mans.de>
2022-07-11 17:50:24 +0200

    Water!

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -6,6 +6,7 @@ import {
   Float,
   Mix,
   Mul,
+  Multiply,
   Pipe,
   Pow,
   Remap,
@@ -23,49 +24,29 @@ import CustomShaderMaterial from "three-custom-shader-material"
 import { DustExample } from "./DustExample"
 import { useShader } from "./useShader"
 
+const ScaledNoise = (scale = 1, timeScale = 1) =>
+  Simplex3DNoise(
+    Add(Multiply(VertexPosition, scale), Multiply(Time, timeScale))
+  )
+
 export default function Playground() {
   const shader = useShader(() => {
-    const scaledPos = Vec3(Mul(VertexPosition, 0.11))
-
-    const noise = Pow(
-      Remap(Simplex3DNoise(code`${scaledPos}`), -1, 1, 0, 1),
-      1.5
-    )
-
-    const steppedNoise = Smoothstep(-0, 1, noise)
-
-    const waterHeight = Float(
-      code`0.3 + sin(${Time} + ${VertexPosition}.y) * 0.02`
-    )
-
-    const waterNoise = Step(
-      0,
-      Simplex3DNoise(Add(Vec3(Mul(VertexPosition, 0.3)), Mul(Time, 0.05)))
-    )
-
-    const dissolve = Dissolve(Smoothstep(-0.5, 0.5, Sin(Time)), 0.1)
+    const bigwaves = ScaledNoise(0.008, 0.1)
+    const waves = ScaledNoise(0.025, 0.1)
+    const ripples = ScaledNoise(5, 0.8)
+    const foam = Step(0, ScaledNoise(0.1, 0.1))
 
     return CustomShaderMaterialMaster({
-      position: Mul(VertexPosition, Float(code`1.0 + ${steppedNoise} * 0.3`)),
-
-      diffuseColor: Pipe(
-        Vec3(new Color("#66c")),
-        /* Water noise yooooo */
-        ($) => Mix($, new Color("#67d"), waterNoise),
-        /* Foam */
-        ($) => Mix($, new Color("#ddf"), Step(Sub(waterHeight, 0.02), noise)),
-        /* Sand */
-        ($) => Mix($, new Color("#ec5"), Step(waterHeight, noise)),
-        /* Green */
-        ($) => Mix($, new Color("#494"), Step(0.34, noise)),
-        /* Mountains */
-        ($) => Mix($, new Color("#ccc"), Step(0.5, noise)),
-        /* Skyrim */
-        ($) => Mix($, new Color("#fff"), Step(0.7, noise)),
-        ($) => Add($, dissolve.color)
+      position: Pipe(
+        VertexPosition,
+        ($) => Add($, Add(Multiply(bigwaves, 4), 2)),
+        ($) => Add($, Add(Multiply(waves, 2), 1)),
+        ($) => Add($, Add(Multiply(ripples, 0.4), 0.2))
       ),
-
-      alpha: dissolve.alpha
+      diffuseColor: Pipe(Vec3(new Color("#99b")), ($) =>
+        Add($, Multiply(foam, 0.03))
+      ),
+      alpha: 0.9
     })
   }, [])
 
@@ -73,17 +54,18 @@ export default function Playground() {
   console.log(shader.fragmentShader)
 
   return (
-    <group position-y={18}>
+    <group position-y={-5}>
       {/* <Fog /> */}
       <DustExample />
       <mesh>
-        <icosahedronGeometry args={[12, 4]} />
+        <boxGeometry args={[500, 5, 500, 100, 1, 100]} />
 
         <CustomShaderMaterial
           baseMaterial={MeshStandardMaterial}
           {...shader}
           transparent
           side={DoubleSide}
+          // wireframe
         />
       </mesh>
     </group>
