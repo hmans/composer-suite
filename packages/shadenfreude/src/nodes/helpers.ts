76916695b156bca406ffac474f9ccbb2c290b0fc
Hendrik Mans <hendrik@mans.de>
2022-07-11 20:32:39 +0200

    Fix Pipe

--- packages/shadenfreude/src/nodes/helpers.ts
+++ packages/shadenfreude/src/nodes/helpers.ts
@@ -1,6 +1,6 @@
-import { GLSLType, Node, Value } from "../tree"
+import { GLSLType, INode, Node, Value } from "../tree"
 
 export const Pipe = <T extends GLSLType>(
   v: Value<T>,
-  ...ops: ((v: Value<T>) => Value<T>)[]
-) => ops.reduce((acc, op) => op(acc), v)
+  ...ops: ((v: Value<T>) => INode<T>)[]
+) => ops.reduce((acc, op) => op(acc), v) as Node<T>
