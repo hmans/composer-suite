c70c95d5ed61f527fefc2fe782ee1bd21e05cc9d
Hendrik Mans <hendrik@mans.de>
2022-07-27 20:39:18 +0200

    Spawn!

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,18 +1,56 @@
-import { MutableRefObject, useEffect, useRef } from "react"
+import { MutableRefObject, useEffect, useLayoutEffect, useRef } from "react"
 import {
   CustomShaderMaterialMaster,
   Vec3,
   VertexPosition
 } from "shader-composer"
 import { useShader } from "shader-composer-r3f"
-import { Color, InstancedMesh, Matrix4, MeshStandardMaterial } from "three"
+import {
+  Color,
+  InstancedBufferAttribute,
+  InstancedMesh,
+  Matrix4,
+  MeshStandardMaterial,
+  Quaternion,
+  Vector3
+} from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
+const makeAttribute = (count: number, itemSize: number) =>
+  new InstancedBufferAttribute(new Float32Array(count * itemSize), itemSize)
+
+/**
+ * Prepares the given instanced mesh and returns an API for interacting with it.
+ */
 const useParticles = (imesh: MutableRefObject<InstancedMesh>) => {
+  useLayoutEffect(() => {
+    /* Prepare geometry */
+    const { geometry, count } = imesh.current
+
+    geometry.setAttribute("lifetime", makeAttribute(count, 2))
+    geometry.setAttribute("velocity", makeAttribute(count, 3))
+  })
+
+  let cursor = 0
+
   const spawn = () => {
     if (!imesh.current) return
-    imesh.current.setMatrixAt(0, new Matrix4())
-    imesh.current.count = 1
+
+    console.log("cursor:", cursor)
+
+    /* Set the matrix at cursor */
+    imesh.current.setMatrixAt(
+      cursor,
+      new Matrix4().compose(
+        new Vector3().randomDirection(),
+        new Quaternion().random(),
+        new Vector3(1, 1, 1)
+      )
+    )
+
+    /* Advance cursor */
+    cursor = (cursor + 1) % imesh.current.count
+
     imesh.current.instanceMatrix.needsUpdate = true
   }
 
@@ -37,6 +75,12 @@ export default function Playground() {
 
   useEffect(() => {
     spawn()
+
+    const id = setInterval(() => {
+      spawn()
+    }, 500)
+
+    return () => clearInterval(id)
   }, [])
 
   return (
