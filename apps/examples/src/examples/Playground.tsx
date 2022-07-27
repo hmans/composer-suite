bf4d8fc886e1b9daf6fd4aa2d352ab5d39182b04
Hendrik Mans <hendrik@mans.de>
2022-07-27 18:14:04 +0200

    Cleanup

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,6 +1,3 @@
-import { InstancedMeshProps } from "@react-three/fiber"
-import { forwardRef, useImperativeHandle, useRef } from "react"
-import { InstancedMesh } from "three"
 import { Particles } from "vfx-composer"
 
 export default function Playground() {
