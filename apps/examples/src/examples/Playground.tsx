c5e03f2650243b6dc2bdc4901f9707608981f305
Hendrik Mans <hendrik@mans.de>
2022-07-27 20:21:09 +0200

    Let's go

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,29 +1,56 @@
-import { useEffect } from "react"
-import { MeshStandardMaterial } from "three"
-import CustomShaderMaterial from "three-custom-shader-material"
-import { makeParticles } from "vfx-composer"
+import { MutableRefObject, useEffect, useRef } from "react"
+import {
+  CustomShaderMaterialMaster,
+  Vec3,
+  VertexPosition
+} from "shader-composer"
 import { useShader } from "shader-composer-r3f"
-import { CustomShaderMaterialMaster } from "shader-composer"
+import { Color, InstancedMesh, Matrix4, MeshStandardMaterial } from "three"
+import CustomShaderMaterial from "three-custom-shader-material"
 
-const Effect = makeParticles()
+const useParticles = (imesh: MutableRefObject<InstancedMesh>) => {
+  const spawn = () => {
+    if (!imesh.current) return
+    imesh.current.setMatrixAt(0, new Matrix4())
+    imesh.current.count = 1
+    imesh.current.instanceMatrix.needsUpdate = true
+  }
+
+  const color = Vec3(new Color("hotpink"))
+
+  const position = VertexPosition
+
+  return { spawn, color, position }
+}
 
 export default function Playground() {
+  const imesh = useRef<InstancedMesh>(null!)
+
+  const { spawn, color, position } = useParticles(imesh)
+
   const shader = useShader(() => {
-    return CustomShaderMaterialMaster({})
+    return CustomShaderMaterialMaster({
+      diffuseColor: color,
+      position
+    })
   })
 
   useEffect(() => {
-    Effect.spawn()
+    spawn()
   }, [])
 
   return (
-    <Effect.Root position-y={2}>
+    <instancedMesh
+      ref={imesh}
+      args={[undefined, undefined, 1000]}
+      position-y={2}
+    >
       <boxGeometry />
       <CustomShaderMaterial
         baseMaterial={MeshStandardMaterial}
         color="hotpink"
         {...shader}
       />
-    </Effect.Root>
+    </instancedMesh>
   )
 }
