b8ea62fdfef599f38770e618952d1e666ce849f7
Hendrik Mans <hendrik@mans.de>
2022-07-27 17:34:14 +0200

    Remove more shadenfreude

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["three-vfx", "randomish", "shadenfreude", "r3f-stage"],
+    exclude: ["three-vfx", "randomish", "shader-composer", "r3f-stage"],
     include: ["react/jsx-runtime"]
   }
 })
