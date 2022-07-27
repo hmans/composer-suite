22bbe9f4ad48a146979f9ebff74eca316bef532f
Hendrik Mans <hendrik@mans.de>
2022-07-27 18:10:32 +0200

    Set up Particles

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,3 +1,28 @@
+import { InstancedMeshProps } from "@react-three/fiber"
+import { forwardRef, useImperativeHandle, useRef } from "react"
+import { InstancedMesh } from "three"
+
+type ParticlesProps = InstancedMeshProps
+
+type Particles = {
+  mesh: InstancedMesh
+}
+
+const Particles = forwardRef<Particles, ParticlesProps>((props, ref) => {
+  const mesh = useRef<InstancedMesh>(null!)
+
+  useImperativeHandle(ref, () => ({
+    mesh: mesh.current
+  }))
+
+  return <instancedMesh {...props} ref={mesh} />
+})
+
 export default function Playground() {
-  return <></>
+  return (
+    <Particles>
+      <boxGeometry />
+      <meshStandardMaterial color="hotpink" />
+    </Particles>
+  )
 }
