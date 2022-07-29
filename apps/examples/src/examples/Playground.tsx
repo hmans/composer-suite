f4d9e8fd7bd9f277d9ad110e15eae69158b6053b
Hendrik Mans <hendrik@mans.de>
2022-07-29 12:06:45 +0200

    Firestorm

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -23,37 +23,28 @@ import {
   VelocityModule
 } from "vfx-composer"
 
-const FirestormModule = (): Module => (input) => ({
-  ...input,
+const FirestormModule = (): Module => (input) => {
+  const spawnOffset = ParticleAttribute(
+    "vec3",
+    () => new Vector3(plusMinus(6), 0, 0)
+  )
+  const speed = ParticleAttribute("float", () => between(2, 5))
+  const darkness = ParticleAttribute("float", () => upTo(0.2))
 
-  color: Mix(
-    input.color,
-    new Color("black"),
+  return {
+    ...input,
 
-    /* Color darkness amount */
-    ParticleAttribute("float", () => upTo(0.2))
-  ),
+    color: Mix(input.color, new Color("black"), darkness),
 
-  position: Add(
-    input.position,
-    Mul(
-      /* Spawn position */
+    position: Add(
+      input.position,
       Mul(
-        ParticleAttribute("vec3", () => new Vector3(plusMinus(6), 0, 0)),
-        NormalizePlusMinusOne(Cos(Mul(ParticleAge, 2)))
-      ),
-
-      Rotation3DY(
-        Mul(
-          ParticleAge,
-
-          /* Speed */
-          ParticleAttribute("float", () => between(2, 5))
-        )
+        Mul(spawnOffset, NormalizePlusMinusOne(Cos(Mul(ParticleAge, 2)))),
+        Rotation3DY(Mul(ParticleAge, speed))
       )
     )
-  )
-})
+  }
+}
 
 export default function Playground() {
   const particles = useRef<Particles>(null!)
