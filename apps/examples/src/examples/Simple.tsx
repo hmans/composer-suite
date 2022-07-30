0e491f066b22710809719b0d9fb9c7ad1b43d79f
Hendrik Mans <hendrik@mans.de>
2022-07-30 17:31:17 +0200

    Clear example

--- apps/examples/src/examples/Simple.tsx
+++ apps/examples/src/examples/Simple.tsx
@@ -1,54 +1,11 @@
-import { useTexture } from "@react-three/drei"
-import { between, chance, plusMinus } from "randomish"
-import { OneMinus } from "shader-composer"
 import { Color, Vector3 } from "three"
-import { Repeat } from "three-vfx"
-import { Emitter, Particles } from "vfx-composer/fiber"
-import { DefaultModules } from "vfx-composer/modules"
-import { ParticleAttribute, ParticleProgress } from "vfx-composer/units"
-import textureUrl from "./textures/particle.png"
+import { ParticleAttribute } from "vfx-composer/units"
 
 export const Simple = () => {
-  const texture = useTexture(textureUrl)
-
   const variables = {
     velocity: ParticleAttribute("vec3", () => new Vector3()),
     color: ParticleAttribute("vec3", () => new Color())
   }
 
-  return (
-    <Particles
-      maxParticles={1000}
-      modules={DefaultModules({
-        velocity: variables.velocity,
-        color: variables.color,
-        scale: OneMinus(ParticleProgress),
-        billboard: true
-      })}
-    >
-      <planeGeometry />
-      <meshStandardMaterial
-        color="white"
-        map={texture}
-        alphaMap={texture}
-        transparent
-        depthWrite={false}
-      />
-
-      <Repeat interval={0.1}>
-        <Emitter
-          count={5}
-          setup={() => {
-            variables.velocity.value.set(
-              plusMinus(5),
-              between(5, 18),
-              plusMinus(5)
-            )
-
-            variables.color.value.set(chance(0.5) ? 0xffffff : 0x000000)
-          }}
-        />
-      </Repeat>
-    </Particles>
-  )
+  return null
 }
