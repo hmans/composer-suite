c9ec47c7fd7b48a58ae5cb866beedf93e15ce2c5
Hendrik Mans <hendrik@mans.de>
2022-07-30 07:06:27 +0200

    Have emitters mutate configurators

--- apps/examples/src/examples/Simple.tsx
+++ apps/examples/src/examples/Simple.tsx
@@ -1,7 +1,7 @@
 import { useTexture } from "@react-three/drei"
 import { between, chance, plusMinus } from "randomish"
 import { OneMinus } from "shader-composer"
-import { Color, Vector3 } from "three"
+import { AdditiveBlending, Color, NormalBlending, Vector3 } from "three"
 import { Repeat } from "three-vfx"
 import { Emitter, Particles } from "vfx-composer/fiber"
 import {
@@ -24,34 +24,16 @@ export const Simple = () => {
 
   return (
     <Particles
-      /* How many particles in this effect? If we spawn more, old particles
-      will be recycled. */
       maxParticles={100}
-      /* Particle effects will accept an array of animation modules defining
-      the behavior of each particle. */
       modules={[
-        /* We're using a plane geometry with a texture, so let's billboard it. */
         Billboard(),
-
-        /* Let's animate scale linearly according to the particle's progress (0-1). */
         Scale(OneMinus(ParticleProgress)),
-
-        /* Let's simulate velocity! We want each particle to have its own velocity,
-        so we'll create one randomly. */
         Velocity(variables.velocity),
-
-        /* Let's simulate gravity. */
         Gravity(),
-
-        /* Finally, let's do something with colors. We want each particle to have its
-        own color, so let's pick between two of them randomly. */
         SetColor(variables.color)
       ]}
     >
-      {/* Particle effects can use any geometry... */}
       <planeGeometry />
-
-      {/* ...and any material. */}
       <meshStandardMaterial
         color="white"
         map={texture}
@@ -60,16 +42,15 @@ export const Simple = () => {
         depthWrite={false}
       />
 
-      {/* Finally, emit some particles! */}
       <Repeat interval={0.1}>
         <Emitter
           count={5}
-          setup={(imesh, index) => {
-            variables.velocity.setupParticle(imesh, index, (v) =>
+          setup={(mesh, index) => {
+            variables.velocity.setupParticle(mesh, index, (v) =>
               v.set(plusMinus(5), between(5, 18), plusMinus(5))
             )
 
-            variables.color.setupParticle(imesh, index, (c) =>
+            variables.color.setupParticle(mesh, index, (c) =>
               c.set(chance(0.5) ? 0xffffff : 0x000000)
             )
           }}
