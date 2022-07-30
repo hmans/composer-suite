698f3fe348831a4acea29b5859a677767f58c817
Hendrik Mans <hendrik@mans.de>
2022-07-30 18:26:34 +0200

    Reset example

--- apps/examples/src/examples/Simple.tsx
+++ apps/examples/src/examples/Simple.tsx
@@ -1,29 +1,3 @@
-import { upTo } from "randomish"
-import { Color, Vector3 } from "three"
-import { makeParticles } from "vfx-composer/fiber"
-import { ParticleAttribute } from "vfx-composer/units"
-
 export const Simple = () => {
-  const Effect = makeParticles()
-
-  const variables = {
-    velocity: ParticleAttribute("vec3", () => new Vector3()),
-    color: ParticleAttribute("vec3", () => new Color())
-  }
-
-  return (
-    <group>
-      <Effect.Root>
-        <boxGeometry />
-        <meshStandardMaterial color="hotpink" />
-      </Effect.Root>
-
-      <Effect.Emitter
-        continuous
-        setup={(p) => {
-          p.randomDirection().multiplyScalar(upTo(10))
-        }}
-      />
-    </group>
-  )
+  return <group></group>
 }
