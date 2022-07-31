5179295922935f952c8ec2fddf80717161c55f30
Hendrik Mans <hendrik@mans.de>
2022-07-31 09:04:47 +0200

    New dynamic lifetime module

--- apps/examples/src/examples/Vanilla.tsx
+++ apps/examples/src/examples/Vanilla.tsx
@@ -1,19 +1,52 @@
 import { upTo } from "randomish"
 import { useEffect, useRef } from "react"
-import { Add, Time } from "shader-composer"
+import {
+  $,
+  Add,
+  Div,
+  Input,
+  SplitVector2,
+  Sub,
+  Time,
+  vec2,
+  Vec3
+} from "shader-composer"
 import { BoxGeometry, Group, MeshStandardMaterial, Object3D } from "three"
 import { Particles, ParticlesMaterial } from "vfx-composer"
+import { Module } from "vfx-composer/modules"
 import { loop } from "./lib/loop"
+
+const Lifetime = (lifetime: Input<"vec2">, time: Input<"float">) => {
+  const [ParticleStartTime, ParticleEndTime] = SplitVector2(lifetime)
+
+  const ParticleMaxAge = Sub(ParticleEndTime, ParticleStartTime)
+  const ParticleAge = Sub(time, ParticleStartTime)
+  const ParticleProgress = Div(ParticleAge, ParticleMaxAge)
+
+  const module: Module = (state) => ({
+    ...state,
+    color: Vec3(state.color, {
+      fragment: {
+        body: $`if (${ParticleProgress} < 0.0 || ${ParticleProgress} > 1.0) discard;`
+      }
+    })
+  })
+
+  return { module, ParticleAge, ParticleProgress }
+}
 
 const vanillaCode = (parent: Object3D) => {
   const geometry = new BoxGeometry()
 
+  const lifetime = Lifetime(vec2(0, 100), Time())
+
   const material = new ParticlesMaterial({
     baseMaterial: MeshStandardMaterial,
     modules: [
+      lifetime.module,
       (state) => ({
         ...state,
-        position: Add(state.position, Time())
+        position: Add(state.position, lifetime.ParticleAge)
       })
     ]
   })
