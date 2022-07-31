39525897bf848f5771162ccc91cd02a8b3778e61
Hendrik Mans <hendrik@mans.de>
2022-07-31 07:44:50 +0200

    Do vanilla things

--- apps/examples/src/examples/Vanilla.tsx
+++ apps/examples/src/examples/Vanilla.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useRef } from "react"
+import { BoxGeometry, Group, MeshStandardMaterial, Object3D } from "three"
+import { Particles } from "vfx-composer"
+
+const vanillaCode = (parent: Object3D) => {
+  const geometry = new BoxGeometry()
+  const material = new MeshStandardMaterial()
+  const particles = new Particles(geometry, material, 10000)
+
+  parent.add(particles)
+
+  particles.spawn()
+}
+
 export const Vanilla = () => {
-  return <group></group>
+  const group = useRef<Group>(null!)
+  useEffect(() => vanillaCode(group.current), [])
+  return <group ref={group}></group>
 }
