a643ee3b0e4b6d97f8a357b3f14523c3bd99bf3d
Hendrik Mans <hendrik@mans.de>
2022-07-31 17:13:04 +0200

    Tweak

--- packages/vfx-composer/src/fiber/Particles.tsx
+++ packages/vfx-composer/src/fiber/Particles.tsx
@@ -1,27 +1,8 @@
-import {
-  extend,
-  InstancedMeshProps,
-  Node,
-  Object3DNode
-} from "@react-three/fiber"
+import { extend, InstancedMeshProps } from "@react-three/fiber"
 import React, { forwardRef, useImperativeHandle, useRef } from "react"
 import { Particles as ParticlesImpl } from "../Particles"
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      particles: Object3DNode<ParticlesImpl, typeof ParticlesImpl>
-    }
-  }
-}
-
-declare module "@react-three/fiber" {
-  interface ThreeElements {
-    particles: Object3DNode<ParticlesImpl, typeof ParticlesImpl>
-  }
-}
-
-extend({ Particles: ParticlesImpl })
+extend({ VfxComposerParticles_: ParticlesImpl })
 
 export type ParticlesProps = InstancedMeshProps
 
@@ -32,6 +13,6 @@ export const Particles = forwardRef<ParticlesImpl, ParticlesProps>(
     useImperativeHandle(ref, () => particles.current)
 
     // @ts-ignore
-    return <particles {...props} ref={particles} />
+    return <vfxComposerParticles_ {...props} ref={particles} />
   }
 )
