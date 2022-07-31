72ecbe5d0f91bf791ca545aeba82467bcf01fe4d
Hendrik Mans <hendrik@mans.de>
2022-07-31 07:56:12 +0200

    loop

--- apps/examples/src/examples/Vanilla.tsx
+++ apps/examples/src/examples/Vanilla.tsx
@@ -1,15 +1,43 @@
+import { between, upTo } from "randomish"
 import { useEffect, useRef } from "react"
 import { BoxGeometry, Group, MeshStandardMaterial, Object3D } from "three"
 import { Particles } from "vfx-composer"
 
+const loop = (callback: () => void) => {
+  let running = true
+
+  const tick = () => {
+    if (running) {
+      requestAnimationFrame(tick)
+      callback()
+    }
+  }
+
+  tick()
+
+  return () => {
+    running = false
+  }
+}
+
 const vanillaCode = (parent: Object3D) => {
   const geometry = new BoxGeometry()
-  const material = new MeshStandardMaterial()
+  const material = new MeshStandardMaterial({ color: "red" })
   const particles = new Particles(geometry, material, 10000)
 
   parent.add(particles)
 
-  particles.spawn()
+  const stopLoop = loop(() => {
+    particles.spawn(1, (p) => p.randomDirection().multiplyScalar(upTo(10)))
+  })
+
+  return () => {
+    stopLoop()
+    parent.remove(particles)
+    particles.dispose()
+    geometry.dispose()
+    material.dispose()
+  }
 }
 
 export const Vanilla = () => {
