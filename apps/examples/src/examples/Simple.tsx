f817eab58aabcfafb14cfcc6bbb4bb75c54a1f10
Hendrik Mans <hendrik@mans.de>
2022-08-01 15:33:01 +0200

    Don't use Timeline Composer for a moment

--- apps/examples/src/examples/Simple.tsx
+++ apps/examples/src/examples/Simple.tsx
@@ -36,25 +36,26 @@ export const Simple = () => {
         </VFXMaterial>
       </Effect.Root>
 
-      <Repeat seconds={1}>
-        <Effect.Emitter
-          count={1000}
-          setup={({ position, rotation }) => {
-            const t = variables.time.uniform.value
-            const { lifetime, velocity, color } = variables
+      {/* <Repeat seconds={1}> */}
+      <Effect.Emitter
+        count={10}
+        continuous
+        setup={({ position, rotation }) => {
+          const t = variables.time.uniform.value
+          const { lifetime, velocity, color } = variables
 
-            /* Randomize the instance transform */
-            position.randomDirection().multiplyScalar(upTo(6))
-            rotation.random()
+          /* Randomize the instance transform */
+          position.randomDirection().multiplyScalar(upTo(6))
+          rotation.random()
 
-            /* Write values into the instanced attributes */
-            const start = t
-            lifetime.value.set(start, start + between(1, 3))
-            velocity.value.set(plusMinus(5), between(5, 18), plusMinus(5))
-            color.value.setRGB(Math.random(), Math.random(), Math.random())
-          }}
-        />
-      </Repeat>
+          /* Write values into the instanced attributes */
+          const start = t
+          lifetime.value.set(start, start + between(1, 3))
+          velocity.value.set(plusMinus(5), between(5, 18), plusMinus(5))
+          color.value.setRGB(Math.random(), Math.random(), Math.random())
+        }}
+      />
+      {/* </Repeat> */}
     </group>
   )
 }
