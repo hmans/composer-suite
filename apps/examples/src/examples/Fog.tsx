6154f009b66965f7e1eb8c51d28411ae89512f7c
Hendrik Mans <hendrik@mans.de>
2022-08-02 15:45:22 +0200

    Betta softies

--- apps/examples/src/examples/Fog.tsx
+++ apps/examples/src/examples/Fog.tsx
@@ -1,66 +1,24 @@
 import { useTexture } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { between, plusMinus, upTo } from "randomish"
-import {
-  $,
-  Float,
-  Input,
-  Mul,
-  Resolution,
-  Rotation3DZ,
-  Snippet,
-  Time,
-  Vec2,
-  Vec3,
-  Vec4,
-  Uniform,
-  VertexPosition
-} from "shader-composer"
+import { Mul, Resolution, Rotation3DZ, Time, Uniform } from "shader-composer"
 import { MeshStandardMaterial, Vector3 } from "three"
-import { depthTexture } from "three-vfx/src/layers"
 import { InstanceSetupCallback } from "vfx-composer"
 import { Emitter, Particles, VFX, VFXMaterial } from "vfx-composer/fiber"
 import { ParticleAttribute } from "vfx-composer/units"
+import { CameraFar, CameraNear, SoftParticle } from "./lib/softies"
 import { useDepthBuffer } from "./lib/useDepthBuffer"
 import { smokeUrl } from "./textures"
 
-const ViewPosition = (position: Input<"vec3">) =>
-  Vec4($`viewMatrix * instanceMatrix * modelMatrix * vec4(${position}, 1.0);`, {
-    varying: true
-  })
-
-const FragmentCoordinate = Vec2($`gl_FragCoord.xy`)
-
-const ScreenUV = Vec2($`${FragmentCoordinate} / ${Resolution}`)
-
-export const CameraNear = Uniform<"float", number>("float", 0)
-export const CameraFar = Uniform<"float", number>("float", 1)
-
-const SceneDepth = (xy: Input<"vec2">, texture: Input<"sampler2D">) =>
-  Float(
-    $`perspectiveDepthToViewZ(texture2D(${texture}, ${xy}).x, ${CameraNear}, ${CameraFar})`,
-    { name: "Scene Depth" }
-  )
-
-const DistanceToSceneDepth = (depthTexture: Input<"sampler2D">) =>
-  Float($`(${ViewPosition}.z - ${SceneDepth(ScreenUV, depthTexture)})`)
-
-export const SoftParticle = (
-  position: Input<"vec3">,
-  softness: Input<"float">,
-  depthTexture: Input<"sampler2D">
-) =>
-  Float($`clamp(${DistanceToSceneDepth(depthTexture)} / ${softness}, 0.0, 1.0)`)
-
 export const Fog = () => {
-  const depthTexture = useDepthBuffer().depthTexture
   const texture = useTexture(smokeUrl)
 
-  const depthSampler2D = Uniform("sampler2D", depthTexture)
+  const depthSampler2D = Uniform("sampler2D", useDepthBuffer().depthTexture)
 
   const time = Time()
   const velocity = ParticleAttribute(new Vector3())
   const rotation = ParticleAttribute(0 as number)
+  const scale = ParticleAttribute(1 as number)
 
   useFrame(({ camera }) => {
     Resolution.value.set(window.innerWidth, window.innerHeight)
@@ -68,11 +26,11 @@ export const Fog = () => {
     CameraFar.value = camera.far
   })
 
-  const setup: InstanceSetupCallback = ({ position, scale }) => {
+  const setup: InstanceSetupCallback = ({ position }) => {
     position.set(plusMinus(10), between(0, 15), plusMinus(10))
-    scale.setScalar(between(5, 10))
     velocity.value.randomDirection().multiplyScalar(upTo(0.002))
     rotation.value = plusMinus(0.1)
+    scale.value = between(10, 50)
   }
 
   return (
@@ -84,16 +42,20 @@ export const Fog = () => {
         transparent
         depthWrite={false}
       >
-        <VFX.SetAlpha alpha={0.5} />
+        <VFX.SetAlpha alpha={0.15} />
         <VFX.Velocity velocity={velocity} time={time} />
         <VFX.Rotate rotation={Rotation3DZ(Mul(time, rotation))} />
+        <VFX.Scale scale={scale} />
 
         <VFX.Billboard />
 
         <VFX.Module
           module={(state) => ({
             ...state,
-            alpha: Mul(state.alpha, SoftParticle(3, depthSampler2D))
+            alpha: Mul(
+              state.alpha,
+              SoftParticle(10, depthSampler2D, state.position)
+            )
           })}
         />
       </VFXMaterial>
--- apps/examples/src/examples/SoftParticlesExample.tsx
+++ apps/examples/src/examples/SoftParticlesExample.tsx
@@ -1,77 +1,12 @@
 import { useFrame } from "@react-three/fiber"
-import {
-  $,
-  Float,
-  Input,
-  InstanceMatrix,
-  ModelMatrix,
-  Mul,
-  Resolution,
-  Uniform,
-  Vec2,
-  Vec4,
-  ViewMatrix
-} from "shader-composer"
-import { MeshStandardMaterial, Vector4 } from "three"
+import { Mul, Resolution, Uniform } from "shader-composer"
+import { MeshStandardMaterial } from "three"
 import { Emitter, Particles, VFX, VFXMaterial } from "vfx-composer/fiber"
+import { CameraFar, CameraNear, SoftParticle } from "./lib/softies"
 import { useDepthBuffer } from "./lib/useDepthBuffer"
 
-const FragmentCoordinate = Vec2($`gl_FragCoord.xy`)
-
-const ScreenUV = Vec2($`${FragmentCoordinate} / ${Resolution}`)
-
-const ToViewSpace = (position: Input<"vec3">) =>
-  Vec4(
-    $`${ViewMatrix} * ${InstanceMatrix} * ${ModelMatrix} * vec4(${position}, 1.0)`
-  )
-
-const SoftParticle = (
-  softness: Input<"float">,
-  depthTexture: Input<"sampler2D">,
-  position: Input<"vec3">
-) =>
-  Float(1, {
-    name: "Soft Particle",
-    fragment: {
-      header: $`
-        float readDepth(vec2 coord) {
-          float depthZ = texture2D(${depthTexture}, coord).x;
-          float viewZ = perspectiveDepthToViewZ(depthZ, ${CameraNear}, ${CameraFar});
-          return viewZ;
-        }
-      `,
-      body: $`
-        /* Get the existing depth at the fragment position */
-        float depth = readDepth(${ScreenUV});
-
-        float v_viewZ = ${Float(0, {
-          varying: true,
-          vertex: {
-            body: $`value = (${ToViewSpace(position)}).z;`
-          }
-        })};
-
-        /* Prepare some convenient local variables */
-        float d = depth;
-        float z = v_viewZ;
-        float softness = ${softness};
-
-        /* Calculate the distance to the fragment */
-        float distance = z - d;
-
-        /* Apply the distance to the fragment alpha */
-        value = clamp(distance / softness, 0.0, 1.0);
-      `
-    }
-  })
-
-export const CameraNear = Uniform<"float", number>("float", 0)
-export const CameraFar = Uniform<"float", number>("float", 1)
-
 export const SoftParticlesExample = () => {
-  const { depthTexture } = useDepthBuffer()
-
-  const depthSampler2D = Uniform("sampler2D", depthTexture)
+  const depthSampler2D = Uniform("sampler2D", useDepthBuffer().depthTexture)
 
   useFrame(({ camera }) => {
     Resolution.value.set(window.innerWidth, window.innerHeight)
@@ -95,7 +30,7 @@ export const SoftParticlesExample = () => {
             ...state,
             alpha: Mul(
               state.alpha,
-              SoftParticle(1, depthSampler2D, state.position)
+              SoftParticle(3, depthSampler2D, state.position)
             )
           })}
         />
