ad8ac5358ac85acc197c76237106998fd67c3b14
Hendrik Mans <hendrik@mans.de>
2022-08-02 15:21:45 +0200

    Eh

--- apps/examples/src/examples/SoftParticlesExample.tsx
+++ apps/examples/src/examples/SoftParticlesExample.tsx
@@ -19,7 +19,36 @@ const ScreenUV = Vec2($`${FragmentCoordinate} / ${Resolution}`)
 const SoftParticle = (
   softness: Input<"float">,
   depthTexture: Input<"sampler2D">
-) => Float(1)
+) =>
+  Float(1, {
+    name: "Soft Particle",
+    fragment: {
+      header: $`
+        float readDepth(vec2 coord) {
+          float depthZ = texture2D(${depthTexture}, coord).x;
+          float viewZ = perspectiveDepthToViewZ(depthZ, ${CameraNear}, ${CameraFar});
+          return viewZ;
+        }
+      `,
+      body: $`
+        /* Get the existing depth at the fragment position */
+        float depth = readDepth(${ScreenUV});
+
+        float v_viewZ = 1.0;
+
+        /* Prepare some convenient local variables */
+        float d = depth;
+        float z = v_viewZ;
+        float softness = ${softness};
+
+        /* Calculate the distance to the fragment */
+        float distance = z - d;
+
+        /* Apply the distance to the fragment alpha */
+        value = clamp(distance / softness, 0.0, 1.0);
+      `
+    }
+  })
 
 export const CameraNear = Uniform<"float", number>("float", 0)
 export const CameraFar = Uniform<"float", number>("float", 1)
