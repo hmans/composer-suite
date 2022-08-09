3bc9600853e80317fa8978f7e91b9b426974bba6
Hendrik Mans <hendrik@mans.de>
2022-08-09 08:50:26 +0200

    nearfar

--- apps/examples/src/examples/lib/softies.ts
+++ apps/examples/src/examples/lib/softies.ts
@@ -9,7 +9,6 @@ import {
   pipe,
   Resolution,
   Saturate,
-  Snippet,
   Sub,
   Texture2D,
   Uniform,
@@ -18,27 +17,28 @@ import {
   Vec4,
   ViewMatrix
 } from "shader-composer"
+import { Camera, Float16BufferAttribute, Scene } from "three"
 import { ModuleFactory } from "vfx-composer/modules"
 
-export const SoftParticles: ModuleFactory<{
-  softness: Input<"float">
-  depthSampler2D: Unit<"sampler2D">
-}> = ({ softness, depthSampler2D }) => (state) => ({
-  ...state,
-  alpha: Mul(
-    state.alpha,
-    SoftParticle(softness, depthSampler2D, state.position)
-  )
+/**
+ * Returns the current fragment's on-screen coordinate.
+ */
+export const FragmentCoordinate = Vec2($`gl_FragCoord.xy`, {
+  name: "Fragment Coordinate",
+  only: "fragment"
 })
 
-export const FragmentCoordinate = Vec2($`gl_FragCoord.xy`)
-
+/**
+ * Returns the on-screen UV coordinate, which equals the current fragment's
+ * absolute on-screen coordinate divided by the screen resolution.
+ */
 export const ScreenUV = Vec2($`${FragmentCoordinate} / ${Resolution}`, {
   name: "Screen UV"
 })
 
 /**
- * Converts the given position (which is assumed to be in local space) to view space.
+ * Converts the given position vector (which is assumed to be in local space)
+ * to view space.
  */
 export const ToViewSpace = (position: Input<"vec3">) =>
   Vec4(
@@ -52,8 +52,8 @@ export const CameraFar = Uniform<"float", number>("float", 1)
 const ReadDepth = (
   xy: Input<"vec2">,
   depthTexture: Unit<"sampler2D">,
-  cameraNear: Input<"float"> = CameraNear,
-  cameraFar: Input<"float"> = CameraFar
+  cameraNear: Input<"float">,
+  cameraFar: Input<"float">
 ) =>
   Float(
     pipe(
@@ -67,7 +67,9 @@ const ReadDepth = (
 const SceneDepth = (xy: Input<"vec2">, depthTexture: Unit<"sampler2D">) => {
   // TODO: make your own damn depth texture
 
-  return Float(ReadDepth(xy, depthTexture), { name: "Scene Depth" })
+  return Float(ReadDepth(xy, depthTexture, CameraNear, CameraFar), {
+    name: "Scene Depth"
+  })
 }
 
 export const SoftParticle = (
@@ -90,3 +92,14 @@ export const SoftParticle = (
 
     { name: "Soft Particle" }
   )
+
+export const SoftParticles: ModuleFactory<{
+  softness: Input<"float">
+  depthSampler2D: Unit<"sampler2D">
+}> = ({ softness, depthSampler2D }) => (state) => ({
+  ...state,
+  alpha: Mul(
+    state.alpha,
+    SoftParticle(softness, depthSampler2D, state.position)
+  )
+})
