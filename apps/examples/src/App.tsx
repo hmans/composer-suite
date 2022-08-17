e6ded5e96929ba031cb50e27b67f821de376d94c
Hendrik Mans <hendrik@mans.de>
2022-08-17 19:40:41 +0200

    Step by step

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,9 +1,12 @@
-import { Application, Example } from "r3f-stage"
+import { Application, Description, Example } from "r3f-stage"
 import "r3f-stage/styles.css"
 import Simple from "./examples/Simple"
+import { Vanilla } from "./examples/Vanilla"
 
 export default () => (
   <Application>
-    <Example path="simple">{/* <Simple /> */}</Example>
+    <Example path="simple" makeDefault>
+      <Description>Poop</Description>
+    </Example>
   </Application>
 )
