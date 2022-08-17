12479d21e103a89799e4fb4e4a2ef5db1cf6c975
Hendrik Mans <hendrik@mans.de>
2022-08-17 16:45:07 +0200

    What in the f is going on

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,4 +1,14 @@
-import { Application } from "r3f-stage"
-// import "r3f-stage/styles.css"
+import { Application, Example } from "r3f-stage"
+import "r3f-stage/styles.css"
+import Simple from "./examples/Simple"
 
-export default () => <Application />
+export default () => (
+  <Application>
+    <Example path="simple">
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    </Example>
+  </Application>
+)
