c8715e5f0d4f21d7adff211b2bd4bc0364e461a3
Hendrik Mans <hendrik@mans.de>
2022-08-18 13:30:40 +0200

    Eh.

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,7 +1,7 @@
+import { PerspectiveCamera } from "@react-three/drei"
 import { Application } from "r3f-stage"
 import "r3f-stage/styles.css"
 import { Simple } from "./examples/Simple"
-import { PerspectiveCamera } from "@react-three/drei"
 
 export default () => (
   <Application>
