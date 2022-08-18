0701c4c74918b500a3f1b38f814dbc9ace935f34
Hendrik Mans <hendrik@mans.de>
2022-08-18 14:06:52 +0200

    Eh.

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,11 +1,16 @@
 import { PerspectiveCamera } from "@react-three/drei"
-import { Application } from "r3f-stage"
+import { Application, Example, FlatStage } from "r3f-stage"
 import "r3f-stage/styles.css"
 import { Simple } from "./examples/Simple"
 
 export default () => (
   <Application>
     <PerspectiveCamera position={[0, 5, 30]} makeDefault />
-    <Simple />
+
+    <FlatStage>
+      <Example path="simple" title="Hello World" makeDefault>
+        <Simple />
+      </Example>
+    </FlatStage>
   </Application>
 )
