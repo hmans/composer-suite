3cb73540d296c5335479222dcdafd7093d0e8d9c
Hendrik Mans <hendrik@mans.de>
2022-08-18 13:23:28 +0200

    Add custom camera

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,4 +1,11 @@
 import { Application } from "r3f-stage"
 import "r3f-stage/styles.css"
+import { Simple } from "./examples/Simple"
+import { PerspectiveCamera } from "@react-three/drei"
 
-export default () => <Application></Application>
+export default () => (
+  <Application>
+    <PerspectiveCamera position={[0, 5, 30]} makeDefault />
+    <Simple />
+  </Application>
+)
