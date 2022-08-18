83a296441d815afacd15727e319f87eebb135676
Hendrik Mans <hendrik@mans.de>
2022-08-18 14:14:47 +0200

    Remove old 3VFX package

--- packages/vfx/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Emitter"
-export * as layers from "./layers"
-export * from "./MeshParticles"
-export * from "./Particles"
-export { useParticles, type SpawnSetup } from "./ParticlesContext"
-export * from "./shaders"
-export * from "./timeline"
--- packages/vfx/src/layers/provideDepthTexture.ts
+++
@@ -1,8 +0,0 @@
-import { DepthTexture } from "three"
-import { createShader } from "../shaders"
-
-export default function(depthTexture: DepthTexture) {
-  return createShader({
-    uniforms: { u_depth: { type: "sampler2D", value: depthTexture } }
-  })
-}
