dcae54752860194cfb25989600aaa0b5d1def5cb
Hendrik Mans <hendrik@mans.de>
2022-08-18 19:02:37 +0200

    Remove obsolete useConst

--- packages/vfx-composer-r3f/src/util/useConst.tsx
+++
@@ -1,8 +0,0 @@
-import { useRef } from "react"
-
-export const useConst = <T extends any>(ctor: () => T) => {
-  const ref = useRef<T>()
-  if (!ref.current) ref.current = ctor()
-
-  return ref.current
-}
