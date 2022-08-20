421c15604c938dc78ff259ba01bd4b0a7205726a
Hendrik Mans <hendrik@mans.de>
2022-08-20 17:59:32 +0200

    Set up the playground

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,3 +1,10 @@
 export default function Playground() {
-  return <group></group>
+  return (
+    <group position-y={1.5}>
+      <mesh>
+        <icosahedronGeometry />
+        <meshPhysicalMaterial color="#dda15e" metalness={0.8} roughness={0.5} />
+      </mesh>
+    </group>
+  )
 }
