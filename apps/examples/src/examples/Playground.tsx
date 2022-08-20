311e0bc421fac28516dcec88fd3ca7be9bd524bc
Hendrik Mans <hendrik@mans.de>
2022-08-20 22:29:42 +0200

    tomorrow

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,60 +1,32 @@
 import { MeshProps } from "@react-three/fiber"
-import { makeStore, State, useStore } from "statery"
-import { BufferGeometry, MeshPhysicalMaterial } from "three"
-import { VFXMaterial } from "vfx-composer"
-
-/* The library */
-
-type CaptureProxy<S extends State> = {
-  [K in keyof S]: (value: S[K]) => void
-}
-
-const makeResourceStore = <S extends State>(initialState?: S) => {
-  const store = makeStore<S>(initialState || ({} as S))
-
-  const capture = new Proxy<CaptureProxy<S>>(store.state, {
-    get: (_, name: string) => {
-      return (value: any) => {
-        store.set({ [name]: value } as S)
-      }
-    }
-  })
-
-  const retrieve = new Proxy<S>(store.state, {
-    get: (_, name: string) => {
-      return useStore(store)[name]
-    }
-  })
-
-  return { capture, retrieve }
-}
-
-const { capture, retrieve } = makeResourceStore<{
-  expensiveMaterial: MeshPhysicalMaterial
-  geometry: BufferGeometry
-  reallyExpensiveMaterial: VFXMaterial
-}>()
 
 export default function Playground() {
   return (
     <group position-y={1.5}>
-      <Materials />
-      <Thingy />
+      {/* Render two thingies! */}
+      <Thingy position-x={-1} />
+      <Thingy position-x={1} />
     </group>
   )
 }
 
-const Materials = () => (
-  <meshPhysicalMaterial
-    ref={capture.expensiveMaterial}
-    color="#dda15e"
-    metalness={0.8}
-    roughness={0.5}
-  />
-)
+const ReallyHeavyMaterial = () => {
+  /* INSERT MEMOIZATION HERE, but not of the component,
+  but the actual material, aaaaaahahahahaha help */
+
+  return (
+    <meshPhysicalMaterial
+      color="#dda15e"
+      metalness={0.8}
+      roughness={0.5}
+      transmission={1}
+    />
+  )
+}
 
 const Thingy = (props: MeshProps) => (
-  <mesh material={retrieve.expensiveMaterial} {...props}>
+  <mesh {...props}>
     <icosahedronGeometry />
+    <ReallyHeavyMaterial />
   </mesh>
 )
