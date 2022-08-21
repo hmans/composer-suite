ee0ea6fc5a5adad2c2f97c959c536f20d27017db
Hendrik Mans <hendrik@mans.de>
2022-08-21 08:52:20 +0200

    Hm!

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,10 +1,59 @@
+import { useRerender } from "@hmans/use-rerender"
 import { applyProps, MeshProps, Node } from "@react-three/fiber"
-import { useEffect, useRef } from "react"
+import {
+  cloneElement,
+  createRef,
+  ReactNode,
+  useEffect,
+  useLayoutEffect,
+  useRef
+} from "react"
 import { Mesh, MeshPhysicalMaterial } from "three"
+
+type Constructor = { new (...args: any[]): any }
+
+type Props<C extends Constructor> = Node<InstanceType<C>, C>
+
+const createSharedResource = () => {
+  const instance = {
+    current: undefined
+  }
+
+  const Root = ({ children }: { children: ReactNode }) => {
+    const modified = cloneElement(children, {
+      ref: (c) => {
+        console.log("setting instance", c)
+        instance.current = c
+      }
+    })
+
+    return <>{modified}</>
+  }
+
+  const Instance = () => {
+    const rerender = useRerender()
+
+    useLayoutEffect(() => {
+      rerender()
+    }, [])
+
+    return instance.current ? (
+      <primitive object={instance.current} attach="material" />
+    ) : null
+  }
+
+  return { Root, Instance }
+}
+
+const ThingyMaterial = createSharedResource()
 
 export default function Playground() {
   return (
     <group position-y={1.5}>
+      <ThingyMaterial.Root>
+        <meshPhysicalMaterial name="ThingyMaterial" color="hotpink" />
+      </ThingyMaterial.Root>
+
       {/* Render two thingies! */}
       <Thingy position-x={-1} />
       <Thingy position-x={1} />
@@ -12,40 +61,13 @@ export default function Playground() {
   )
 }
 
-type Constructor = { new (...args: any[]): any }
-
-type Props<C extends Constructor> = Node<InstanceType<C>, C>
-
-const createSharedResource = <C extends Constructor>(constructor: C) => {
-  return (props: Props<C>) => {
-    return null
-  }
-}
-
-const SharedThingyMaterial = createSharedResource(MeshPhysicalMaterial)
-
-const ThingyMaterial = () => (
-  <SharedThingyMaterial
-    color="#dda15e"
-    metalness={0.8}
-    roughness={0.5}
-    transmission={1}
-    attach="material"
-  />
-)
-
 const Thingy = (props: MeshProps) => {
   const mesh = useRef<Mesh>(null!)
-
-  useEffect(() => {
-    // @ts-ignore
-    console.log(mesh.current.material.uuid)
-  })
 
   return (
     <mesh {...props} ref={mesh}>
       <icosahedronGeometry />
-      <ThingyMaterial />
+      <ThingyMaterial.Instance />
     </mesh>
   )
 }
