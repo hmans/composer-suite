0dcc88fac4c1bed2f2865a8fc86de0b964e80a56
Hendrik Mans <hendrik@mans.de>
2022-08-21 12:01:03 +0200

    Break the Simple example

--- apps/examples/src/examples/Simple.tsx
+++ apps/examples/src/examples/Simple.tsx
@@ -6,19 +6,14 @@ import { AdditiveBlending, MeshStandardMaterial, Vector2, Vector3 } from "three"
 import { Emitter, Particles, VFX, VFXMaterial } from "vfx-composer-r3f"
 import { Lifetime } from "vfx-composer/modules"
 import { ParticleAttribute } from "vfx-composer/units"
+import { sharedResource } from "./lib/sharedResource"
 import { particleUrl } from "./textures"
 
 const useParticleLifetime = (lifetime: Input<"vec2">, time: Input<"float">) =>
   useMemo(() => Lifetime(lifetime, time), [lifetime, time])
 
-export const Simple = () => {
+const SimpleMaterial = sharedResource(({ variables }: { variables: any }) => {
   const texture = useTexture(particleUrl)
-
-  const [variables] = useState(() => ({
-    time: Time(),
-    lifetime: ParticleAttribute(new Vector2()),
-    velocity: ParticleAttribute(new Vector3())
-  }))
 
   const {
     ParticleProgress,
@@ -27,23 +22,36 @@ export const Simple = () => {
   } = useParticleLifetime(variables.lifetime, variables.time)
 
   return (
+    <VFXMaterial
+      baseMaterial={MeshStandardMaterial}
+      map={texture}
+      transparent
+      depthWrite={false}
+      blending={AdditiveBlending}
+    >
+      <VFX.Billboard />
+      <VFX.Scale scale={OneMinus(ParticleProgress)} />
+      <VFX.Velocity velocity={variables.velocity} time={ParticleAge} />
+      <VFX.Acceleration force={new Vector3(0, -2, 0)} time={ParticleAge} />
+      <VFX.Module module={lifetimeModule} />
+    </VFXMaterial>
+  )
+})
+
+export const Simple = () => {
+  const [variables] = useState(() => ({
+    time: Time(),
+    lifetime: ParticleAttribute(new Vector2()),
+    velocity: ParticleAttribute(new Vector3())
+  }))
+
+  return (
     <group>
+      <SimpleMaterial.Resource variables={variables} />
+
       <Particles maxParticles={1000} safetyBuffer={1_000}>
         <planeGeometry args={[0.2, 0.2]} />
-
-        <VFXMaterial
-          baseMaterial={MeshStandardMaterial}
-          map={texture}
-          transparent
-          depthWrite={false}
-          blending={AdditiveBlending}
-        >
-          <VFX.Billboard />
-          <VFX.Scale scale={OneMinus(ParticleProgress)} />
-          <VFX.Velocity velocity={variables.velocity} time={ParticleAge} />
-          <VFX.Acceleration force={new Vector3(0, -2, 0)} time={ParticleAge} />
-          <VFX.Module module={lifetimeModule} />
-        </VFXMaterial>
+        <SimpleMaterial />
 
         <Emitter
           continuous
