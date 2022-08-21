fd98e9990525c614433d06588f51ee7cf779fc61
Hendrik Mans <hendrik@mans.de>
2022-08-21 19:39:54 +0200

    Get started with r3f package

---
+++ packages/material-composer-r3f/src/index.ts
@@ -0,0 +1,2 @@
+export { VFXReactor as Module } from "./reactor"
+export * from "./ComposableMaterial"
---
+++ packages/material-composer-r3f/src/reactor.tsx
@@ -0,0 +1,45 @@
+import { FC, useEffect, useMemo } from "react"
+import * as VFXModules from "material-composer/modules"
+import { Module, ModuleFactory, ModuleFactoryProps } from "vfx-composer/modules"
+import { useVFXMaterialContext } from "./ComposableMaterial"
+
+type VFXModules = typeof VFXModules
+
+const cache = new Map<string, VFXComponent<any>>()
+
+type VFXComponentProps<K extends keyof VFXModules> = Parameters<
+  VFXModules[K]
+>[0]
+
+type VFXComponent<K extends keyof VFXModules> = FC<VFXComponentProps<K>>
+
+type VFXProxy = {
+  [K in keyof VFXModules]: VFXModules[K] extends (...args: any[]) => Module
+    ? VFXComponent<K>
+    : never
+}
+
+const makeModuleComponent = <P extends ModuleFactoryProps>(
+  fac: ModuleFactory<P>
+) => (props: P) => {
+  const module = useMemo(() => fac(props), [props])
+
+  const { addModule, removeModule } = useVFXMaterialContext()
+
+  useEffect(() => {
+    addModule(module)
+    return () => removeModule(module)
+  }, [module])
+
+  return null
+}
+
+export const VFXReactor = new Proxy<VFXProxy>({} as VFXProxy, {
+  get<N extends keyof VFXModules>(target: any, name: N) {
+    if (!cache.has(name)) {
+      // @ts-ignore
+      cache.set(name, makeModuleComponent(VFXModules[name]))
+    }
+    return cache.get(name)
+  }
+})
