62e2b30a3bbb73a76290aa4be9b54495f804274c
Hendrik Mans <hendrik@mans.de>
2022-08-21 11:45:10 +0200

    CooL!

--- apps/examples/src/examples/Fireball.tsx
+++ apps/examples/src/examples/Fireball.tsx
@@ -1,27 +1,38 @@
-import { Mul, Time, vec3 } from "shader-composer"
+import { Input, Mul, Time, vec3 } from "shader-composer"
 import { MeshStandardMaterial } from "three"
 import { VFX, VFXMaterial } from "vfx-composer-r3f"
+import { sharedResource } from "./lib/sharedResource"
+
+const LavaMaterial = sharedResource(
+  ({ time = Time() }: { time?: Input<"float"> }) => {
+    return (
+      <VFXMaterial baseMaterial={MeshStandardMaterial}>
+        <VFX.DistortSurface offset={Mul(time, 0.4)} amplitude={0.1} />
+
+        <VFX.Lava
+          offset={Mul(vec3(0.1, 0.2, 0.5), time)}
+          scale={0.3}
+          octaves={5}
+          power={1}
+        />
+      </VFXMaterial>
+    )
+  }
+)
 
 export default function FireballExample() {
-  const time = Time()
-
   return (
     <group position-y={1.5}>
-      {/* <directionalLight intensity={0.8} position={[20, 10, 10]} /> */}
+      <LavaMaterial.Resource time={Mul(Time(), 0.1)} />
 
-      <mesh>
+      <mesh position-x={-1.5}>
         <icosahedronGeometry args={[1, 8]} />
+        <LavaMaterial />
+      </mesh>
 
-        <VFXMaterial baseMaterial={MeshStandardMaterial}>
-          <VFX.DistortSurface offset={Mul(time, 0.4)} amplitude={0.1} />
-
-          <VFX.Lava
-            offset={Mul(vec3(0.1, 0.2, 0.5), time)}
-            scale={0.3}
-            octaves={5}
-            power={1}
-          />
-        </VFXMaterial>
+      <mesh position-x={+1.5}>
+        <icosahedronGeometry args={[1, 8]} />
+        <LavaMaterial />
       </mesh>
     </group>
   )
