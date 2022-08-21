ac95dde7c820a42155da1828054557bac33fac90
Hendrik Mans <hendrik@mans.de>
2022-08-21 08:31:19 +0200

    Eh

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -16,33 +16,16 @@ type Constructor = { new (...args: any[]): any }
 
 type Props<C extends Constructor> = Node<InstanceType<C>, C>
 
-const resource = {
-  instance: null
+const createSharedResource = <C extends Constructor>(constructor: C) => {
+  return (props: Props<C>) => {
+    return null
+  }
 }
 
-const useSharedResource = <C extends Constructor>(
-  constructor: C,
-  args: ConstructorParameters<C>,
-  props: Props<C>
-) => {
-  if (!resource.instance)
-    resource.instance = applyProps(new constructor(args), props as any)
-
-  return resource.instance
-}
-
-const SharedResource = <C extends Constructor>({
-  constructor,
-  args,
-  ...props
-}: { constructor: C } & Props<C>) => {
-  const resource = useSharedResource(constructor, args, props)
-  return <primitive object={resource} attach="material" />
-}
+const SharedThingyMaterial = createSharedResource(MeshPhysicalMaterial)
 
 const ThingyMaterial = () => (
-  <SharedResource
-    constructor={MeshPhysicalMaterial}
+  <SharedThingyMaterial
     color="#dda15e"
     metalness={0.8}
     roughness={0.5}
