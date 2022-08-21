142d5aaf38ccd758a11486624c6cde2934913740
Hendrik Mans <hendrik@mans.de>
2022-08-21 15:54:18 +0200

    More cleanup

--- packages/vfx-composer/src/units/index.ts
+++ packages/vfx-composer/src/units/index.ts
@@ -1,26 +1,7 @@
-import {
-  $,
-  Add,
-  Clamp01,
-  Div,
-  Float,
-  Input,
-  LocalToViewSpace,
-  Mul,
-  PerspectiveDepth,
-  pipe,
-  Pow,
-  Saturate,
-  ScreenUV,
-  Sub,
-  Unit,
-  varying,
-  vec3
-} from "shader-composer"
-import { Turbulence3D } from "shader-composer-toybox"
 import { Color, Vector2, Vector3, Vector4 } from "three"
 
 export * from "./billboard"
+export * from "./experiments"
 export * from "./particles"
 
 /* TODO: promote this into Shader Composer */
@@ -35,49 +16,3 @@ export type GLSLTypeFor<J> = J extends number
   : J extends Color
   ? "vec3"
   : never
-
-export const Random = (n: Input<"float">) =>
-  Float($`fract(sin(${n}) * 1e4)`, { name: "Random1" })
-
-export const SoftParticle = (
-  softness: Input<"float">,
-  position: Input<"vec3">,
-  depthTexture: Unit<"sampler2D">
-) => {
-  return Float(
-    pipe(
-      position,
-      /* Convert position to view space and grab depth */
-      (v) => varying(LocalToViewSpace(v).z),
-      /* Subtract from the existing scene depth at the fragment coordinate */
-      (v) => Sub(v, PerspectiveDepth(ScreenUV, depthTexture)),
-      /* Divide by softness factor */
-      (v) => Div(v, softness),
-      /* Clamp between 0 and 1 */
-      (v) => Saturate(v)
-    ),
-
-    { name: "Soft Particle" }
-  )
-}
-
-export type HeatOptions = {
-  offset?: Input<"vec3" | "float">
-  scale?: Input<"float">
-  octaves?: number
-  power?: Input<"float">
-}
-
-export const Heat = (
-  v: Input<"vec3">,
-  { offset = vec3(0, 0, 0), scale = 1, octaves = 5, power = 1 }: HeatOptions
-) =>
-  pipe(
-    v,
-    (v) => Add(v, offset),
-    (v) => Mul(v, scale),
-    (v) => Turbulence3D(v, octaves),
-    (v) => Add(v, 0.5),
-    (v) => Clamp01(v),
-    (v) => Pow(v, power)
-  )
--- packages/vfx-composer/src/units/particles.ts
+++ packages/vfx-composer/src/units/particles.ts
@@ -1,10 +1,18 @@
 import {
   Attribute,
   Div,
+  Float,
   glslType,
   Input,
+  LocalToViewSpace,
+  PerspectiveDepth,
+  pipe,
+  Saturate,
+  ScreenUV,
   SplitVector2,
-  Sub
+  Sub,
+  Unit,
+  varying
 } from "shader-composer"
 import { Color, InstancedMesh, Vector2, Vector3, Vector4 } from "three"
 import { GLSLTypeFor } from "."
@@ -91,4 +99,26 @@ export const ParticleAttribute = <
       }
     }
   }
+}
+
+export const SoftParticle = (
+  softness: Input<"float">,
+  position: Input<"vec3">,
+  depthTexture: Unit<"sampler2D">
+) => {
+  return Float(
+    pipe(
+      position,
+      /* Convert position to view space and grab depth */
+      (v) => varying(LocalToViewSpace(v).z),
+      /* Subtract from the existing scene depth at the fragment coordinate */
+      (v) => Sub(v, PerspectiveDepth(ScreenUV, depthTexture)),
+      /* Divide by softness factor */
+      (v) => Div(v, softness),
+      /* Clamp between 0 and 1 */
+      (v) => Saturate(v)
+    ),
+
+    { name: "Soft Particle" }
+  )
 }
