cf099a26e0af97e78492ca08c8a1e390587307bd
Hendrik Mans <hendrik@mans.de>
2022-08-21 11:30:47 +0200

    Yes

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,67 +1,22 @@
-import { cloneElement, FC } from "react"
-import { mergeRefs } from "react-merge-refs"
-import { makeStore, useStore } from "statery"
-import { BufferGeometry, ColorRepresentation, Material } from "three"
+import { sharedResource } from "./lib/sharedResource"
 
-const sharedResource = <P extends any>(component: FC<P>) => {
-  const store = makeStore<{ instance?: any }>({})
-
-  const Create = (props: P) => {
-    const element = component(props)
-    if (!element) return null
-
-    return cloneElement(element, {
-      ref: mergeRefs([
-        (element as any).ref,
-        (instance: any) => store.set({ instance })
-      ])
-    })
-  }
-
-  const Use = () => {
-    const { instance } = useStore(store)
-
-    /*
-    In a future R3F version, this will not be necessary.
-    https://github.com/pmndrs/react-three-fiber/pull/2449
-    */
-    const attach =
-      instance instanceof Material
-        ? "material"
-        : instance instanceof BufferGeometry
-        ? "geometry"
-        : undefined
-
-    return instance ? <primitive object={instance} attach={attach} /> : null
-  }
-
-  return { Create, Use }
-}
-
-/*
-Write a component that creates a resource you want to reuse. Wrap
-it in the sharedResource decorator.
-*/
-
-const ThingyMaterial = sharedResource(
-  ({ color = "hotpink" }: { color: ColorRepresentation }) => {
-    return <meshPhysicalMaterial name="ThingyMaterial" color={color} />
-  }
-)
+const SharedMaterial = sharedResource(() => (
+  <meshPhysicalMaterial name="ThingyMaterial" color="hotpink" />
+))
 
 export default function Playground() {
   return (
     <group position-y={1.5}>
-      <ThingyMaterial.Create color="green" />
+      <SharedMaterial.Resource />
 
       <mesh position-x={-1.5}>
         <sphereGeometry />
-        <ThingyMaterial.Use />
+        <SharedMaterial />
       </mesh>
 
       <mesh position-x={+1.5}>
         <dodecahedronGeometry />
-        <ThingyMaterial.Use />
+        <SharedMaterial />
       </mesh>
     </group>
   )
---
+++ apps/examples/src/examples/lib/sharedResource.tsx
@@ -0,0 +1,41 @@
+import { cloneElement, FC } from "react"
+import { mergeRefs } from "react-merge-refs"
+import { makeStore, useStore } from "statery"
+import { BufferGeometry, Material } from "three"
+
+export const sharedResource = <P extends any>(component: FC<P>) => {
+  const store = makeStore<{ instance?: any }>({})
+
+  const Resource = (props: P) => {
+    const element = component(props)
+    if (!element) return null
+
+    return cloneElement(element, {
+      ref: mergeRefs([
+        (element as any).ref,
+        (instance: any) => store.set({ instance })
+      ])
+    })
+  }
+
+  const Use = () => {
+    const { instance } = useStore(store)
+
+    /*
+    In a future R3F version, the following will no longer be necessary.
+    https://github.com/pmndrs/react-three-fiber/pull/2449
+    */
+    const attach =
+      instance instanceof Material
+        ? "material"
+        : instance instanceof BufferGeometry
+        ? "geometry"
+        : undefined
+
+    return instance ? <primitive object={instance} attach={attach} /> : null
+  }
+
+  Use.Resource = Resource
+
+  return Use
+}
