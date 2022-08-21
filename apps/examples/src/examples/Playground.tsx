3b2dbabcc0f8da64aa617e495a2a35867f4cd4ee
Hendrik Mans <hendrik@mans.de>
2022-08-21 11:19:38 +0200

    Cleanup

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,17 +1,20 @@
-import { useFrame } from "@react-three/fiber"
-import { cloneElement, FC, forwardRef, useRef } from "react"
-import { makeStore, useStore } from "statery"
-import { BufferGeometry, Material, Mesh, MeshPhysicalMaterial } from "three"
+import { cloneElement, FC } from "react"
 import { mergeRefs } from "react-merge-refs"
+import { makeStore, useStore } from "statery"
+import { BufferGeometry, ColorRepresentation, Material } from "three"
 
 const sharedResource = <P extends any>(component: FC<P>) => {
   const store = makeStore<{ instance?: any }>({})
 
   const Create = (props: P) => {
-    const element = component(props) as any
+    const element = component(props)
+    if (!element) return null
 
     return cloneElement(element, {
-      ref: mergeRefs([element.ref, (instance: any) => store.set({ instance })])
+      ref: mergeRefs([
+        (element as any).ref,
+        (instance: any) => store.set({ instance })
+      ])
     })
   }
 
@@ -35,52 +38,29 @@ const sharedResource = <P extends any>(component: FC<P>) => {
   return { Create, Use }
 }
 
-/* Write a component that creates a resource you want to reuse. Wrap
-        it in the sharedResource decorator. */
-const ThingyMaterial = sharedResource(() => {
-  const ref = useRef<MeshPhysicalMaterial>(null!)
+/*
+Write a component that creates a resource you want to reuse. Wrap
+it in the sharedResource decorator.
+*/
 
-  useFrame(() => {
-    console.log(ref.current)
-  })
-
-  return (
-    <meshPhysicalMaterial name="ThingyMaterial" color="hotpink" ref={ref} />
-  )
-})
+const ThingyMaterial = sharedResource(
+  ({ color = "hotpink" }: { color: ColorRepresentation }) => {
+    return <meshPhysicalMaterial name="ThingyMaterial" color={color} />
+  }
+)
 
 export default function Playground() {
-  const ref = useRef<Mesh<any, MeshPhysicalMaterial>>(null!)
-
-  /*
-          For testing, we'll mutate one of the mesh's material's
-          color. If the resource sharing works, the colors of both
-          meshes will update.
-          */
-  useFrame(({ clock }) => {
-    ref.current!.material.color.setScalar(
-      (Math.sin(clock.elapsedTime * 3) + 1) / 2
-    )
-  })
-
   return (
     <group position-y={1.5}>
-      {/* Explicitly create the shared resource. This gives you control
-                  over when and where the resource is created, and how long it'll
-                  stick around. */}
-      <ThingyMaterial.Create />
+      <ThingyMaterial.Create color="green" />
 
       <mesh position-x={-1.5}>
         <sphereGeometry />
-
-        {/* Use the shared resource! */}
         <ThingyMaterial.Use />
       </mesh>
 
-      <mesh position-x={+1.5} ref={ref}>
+      <mesh position-x={+1.5}>
         <dodecahedronGeometry />
-
-        {/* Use it again! */}
         <ThingyMaterial.Use />
       </mesh>
     </group>
