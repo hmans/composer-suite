875b6f35373a64703845e9edf7abfef6d2c4263e
Hendrik Mans <hendrik@mans.de>
2022-08-26 20:20:41 +0200

    Cleanup

--- apps/examples/src/examples/FogExample.tsx
+++ apps/examples/src/examples/FogExample.tsx
@@ -1,6 +1,6 @@
 import { useConst } from "@hmans/use-const"
 import { useTexture } from "@react-three/drei"
-import { ComposableMaterial, Layer, Modules } from "material-composer-r3f"
+import { ComposableMaterial, Modules } from "material-composer-r3f"
 import { Layers, useRenderPipeline } from "r3f-stage"
 import { between, plusMinus, upTo } from "randomish"
 import { Mul, Rotation3DZ, Time } from "shader-composer"
