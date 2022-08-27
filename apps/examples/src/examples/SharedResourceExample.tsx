8ea0a219962251fea0f43123f90aea218de8faee
Hendrik Mans <hendrik@mans.de>
2022-08-27 16:39:50 +0200

    Let's go

--- apps/examples/src/examples/SharedResourceExample.tsx
+++ apps/examples/src/examples/SharedResourceExample.tsx
@@ -1,13 +1,31 @@
 import { ComposableMaterial, Modules } from "material-composer-r3f"
 import { between, insideSphere } from "randomish"
-import { Add, Mul, Sin, Time } from "shader-composer"
+import { useMemo } from "react"
+import { $, Add, Float, Int, Mul, pipe, Sin, Time } from "shader-composer"
 import { Vector3 } from "three"
 import { Emitter, Particles, useParticles } from "vfx-composer-r3f"
 
 const tmpVec3 = new Vector3()
 
+const InstanceID = Int($`gl_InstanceID`, { only: "vertex" })
+
 export default function SharedResourceExample() {
   const particles = useParticles()
+
+  const time = useMemo(() => Time(), [])
+
+  const scale = useMemo(
+    () =>
+      pipe(
+        InstanceID,
+        (v) => Float($`float(${v})`),
+        (v) => Add(Time(), v),
+        (v) => Sin(v),
+        (v) => Mul(v, 0.2),
+        (v) => Add(v, 1)
+      ),
+    [time]
+  )
 
   return (
     <group>
@@ -17,16 +35,16 @@ export default function SharedResourceExample() {
         castShadow
         receiveShadow
       >
-        <icosahedronGeometry args={[0.08, 3]} />
+        <sphereGeometry args={[0.08, 16, 16]} />
 
-        <ComposableMaterial color="#e63946" metalness={0.3} roughness={0.5}>
-          <Modules.Scale scale={Add(1, Mul(Sin(Time()), 0.2))} />
+        <ComposableMaterial color="#e63946" metalness={0.5} roughness={0.6}>
+          <Modules.Scale scale={scale} />
         </ComposableMaterial>
 
         <Emitter
           position-y={1.5}
           rate={Infinity}
-          limit={500}
+          limit={1000}
           setup={({ position, scale }) => {
             const pos = insideSphere()
             tmpVec3.copy(pos as Vector3)
