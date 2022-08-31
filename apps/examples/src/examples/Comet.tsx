fb099d9d066eb426f88b8724cc96b9c2536874ea
Hendrik Mans <hendrik@mans.de>
2022-08-31 02:31:54 +0200

    It cometh

--- apps/examples/src/examples/Comet.tsx
+++ apps/examples/src/examples/Comet.tsx
@@ -32,26 +32,22 @@ import { PSRDNoise3D } from "shader-composer-toybox"
 import { Color, DoubleSide, RepeatWrapping } from "three"
 import streamTextureUrl from "./textures/stream.png"
 
-const time = Time()
-
 const Inverted = <T extends GLSLType>(v: Input<T>) =>
   Unit(type(v), Mul(v, -1), { name: "Inverted Value" })
 
 export default function CometExample() {
   return (
     <group>
-      <group position-y={1.5}>
-        <TestAura />
-      </group>
-      {/* <CameraShake /> */}
-      {/* <Comet position={[-1, 1, 0]} /> */}
+      <group position-y={1.5}>{/* <TestAura /> */}</group>
+      <CameraShake />
+      <Comet position={[-1, 1.5, 0]} />
     </group>
   )
 }
 
 const TestAura = () => (
   <Aura
-    scale-y={1.5}
+    scale-y={1}
     castShadow
     gradient={[
       [new Color("#d62828").multiplyScalar(1.5), 0],
@@ -59,8 +55,8 @@ const TestAura = () => (
       [new Color("#fb8b24").multiplyScalar(2), 0.9],
       [new Color("#f8f9fa").multiplyScalar(8), 1]
     ]}
-    tiling={vec2(3, 1)}
-    offset={vec2(0, Inverted(Add(time, UV.x)))}
+    tiling={vec2(3, 0.5)}
+    offset={vec2(0, Inverted(Add(Time(), UV.x)))}
   />
 )
 
@@ -88,7 +84,11 @@ const Aura = ({
     <mesh {...props}>
       <sphereGeometry args={[1, 32, 16]} />
 
-      <composable.MeshBasicMaterial transparent side={DoubleSide}>
+      <composable.MeshBasicMaterial
+        transparent
+        side={DoubleSide}
+        depthWrite={false}
+      >
         <modules.Gradient
           stops={gradient}
           start={0}
@@ -106,9 +106,32 @@ const Comet = (props: GroupProps) => (
     <group rotation-z={Math.PI / 5}>
       <Float>
         <Rock />
-        <FireShield />
-        {/* <OuterShield /> */}
-        {/* <OutestShield /> */}
+
+        <Aura
+          scale={[1.2, 2, 1.2]}
+          position-y={0.8}
+          gradient={[
+            [new Color("#d62828").multiplyScalar(1.5), 0],
+            [new Color("#fb8b24").multiplyScalar(2), 0.5],
+            [new Color("#fb8b24").multiplyScalar(2), 0.9],
+            [new Color("#f8f9fa").multiplyScalar(8), 1]
+          ]}
+          tiling={vec2(3, 0.5)}
+          offset={vec2(0, Inverted(Add(Time(), UV.x)))}
+        />
+
+        <Aura
+          scale={[1.6, 1.8, 1.6]}
+          position-y={0.5}
+          gradient={[
+            [new Color("#7209b7").multiplyScalar(1.5), 0],
+            [new Color("#b5179e").multiplyScalar(2), 0.5],
+            [new Color("#b5179e").multiplyScalar(2), 0.9],
+            [new Color("#f72585").multiplyScalar(8), 1]
+          ]}
+          tiling={vec2(3, 0.5)}
+          offset={vec2(0, Inverted(Add(Time(), UV.x)))}
+        />
       </Float>
     </group>
   </group>
@@ -118,70 +141,7 @@ const Rock = () => (
   <Animate fun={(g, dt) => (g.rotation.x = g.rotation.y += 2 * dt)}>
     <mesh>
       <icosahedronGeometry args={[1, 0]} />
-      <meshStandardMaterial color="#222" />
+      <composable.MeshStandardMaterial color="#222" />
     </mesh>
   </Animate>
-)
-
-const FireShield = memo(() => {
-  const time = Time()
-
-  const mask = pipe(
-    VertexPosition,
-    (v) => vec3(Add(v.x, Mul(time, 1)), v.y, v.z),
-    PSRDNoise3D,
-    NormalizePlusMinusOne,
-    (v) => Smoothstep(VertexPosition.x, VertexPosition.x, v)
-  )
-
-  const heat = pipe(
-    VertexPosition,
-    (v) => vec3(Sub(Mul(v.x, 0.1), Mul(time, 4)), v.y, v.z),
-    PSRDNoise3D,
-    NormalizePlusMinusOne,
-    (v) => Smoothstep(0.5, 0.7, v)
-  )
-
-  return (
-    <mesh scale={[2, 1.3, 1.3]} position-x={0.7}>
-      <sphereGeometry />
-      <composable.MeshStandardMaterial transparent depthWrite={false}>
-        {/* <modules.Color color={vec3(heat, heat, heat)} /> */}
-        <modules.Gradient
-          stops={[
-            [new Color("yellow"), 0],
-            [new Color("#e63946"), 0.7]
-          ]}
-          position={heat}
-        />
-        <modules.Alpha alpha={Mul(heat, mask)} />
-      </composable.MeshStandardMaterial>
-    </mesh>
-  )
-})
-
-const OutestShield = () => (
-  <mesh scale={[2, 1.5, 1.5]} position-x={0.6}>
-    <sphereGeometry />
-    <meshStandardMaterial
-      color="blue"
-      opacity={0.1}
-      side={DoubleSide}
-      transparent
-      depthWrite={false}
-    />
-  </mesh>
-)
-
-const OuterShield = () => (
-  <mesh scale={[2, 1.3, 1.3]} position-x={0.7}>
-    <sphereGeometry />
-    <meshStandardMaterial
-      color="red"
-      opacity={0.1}
-      side={DoubleSide}
-      transparent
-      depthWrite={false}
-    />
-  </mesh>
 )
