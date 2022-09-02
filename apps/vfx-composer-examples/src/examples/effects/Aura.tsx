03afbd00e6a6b3f73986a289ca9a0749eff596ce
Hendrik Mans <hendrik@mans.de>
2022-09-02 09:24:44 +0200

    Extract NoiseMask to its own module

--- apps/vfx-composer-examples/src/examples/effects/Aura.tsx
+++ apps/vfx-composer-examples/src/examples/effects/Aura.tsx
@@ -4,19 +4,10 @@ import { Layer, LayerArgs } from "material-composer"
 import { composable, moduleComponent } from "material-composer-r3f"
 import { Alpha, Gradient, SurfaceWobble } from "material-composer/modules"
 import {
-  Add,
-  Div,
   GlobalTime,
   GradientStops,
   Input,
   Mul,
-  Negate,
-  NormalizePlusMinusOne,
-  OneMinus,
-  pipe,
-  Saturate,
-  Smoothstep,
-  Sub,
   Texture2D,
   TilingUV,
   Unit,
@@ -24,9 +15,9 @@ import {
   vec2
 } from "shader-composer"
 import { useUniformUnit } from "shader-composer-r3f"
-import { PSRDNoise2D } from "shader-composer-toybox"
 import { DoubleSide, RepeatWrapping } from "three"
 import streamTextureUrl from "../textures/stream.png"
+import { NoiseMask } from "../units/NoiseMask"
 
 export type AuraArgs = {
   gradient: GradientStops<"vec3">
@@ -91,25 +82,5 @@ export const SphericalAura = ({
         <AuraLayer {...props} texture={textureUniform} />
       </composable.meshBasicMaterial>
     </mesh>
-  )
-}
-
-export const NoiseMask = (
-  threshold: Input<"float"> = 0.5,
-  fringe: Input<"float"> = 0.5,
-  time: Input<"float"> = GlobalTime
-) => {
-  const noise = NormalizePlusMinusOne(
-    PSRDNoise2D(TilingUV(UV, vec2(8, 8), vec2(0, Negate(time))))
-  )
-
-  return pipe(
-    Smoothstep(
-      Sub(threshold, Div(fringe, 2)),
-      Add(threshold, Div(fringe, 2)),
-      OneMinus(UV.y)
-    ),
-    (v) => Sub(v, Mul(noise, threshold)),
-    Saturate
   )
 }
