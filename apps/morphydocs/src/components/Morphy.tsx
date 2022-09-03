d3599c4bb81ef7af70b6cee0fdbb334e45d33198
Hendrik Mans <hendrik@mans.de>
2022-09-03 07:36:33 +0200

    Soon

--- apps/morphydocs/src/components/Morphy.tsx
+++ apps/morphydocs/src/components/Morphy.tsx
@@ -1,4 +1,17 @@
-import { Project, VariableDeclaration } from "ts-morph"
+import { Project, VariableDeclaration, VariableStatement } from "ts-morph"
+
+type SymbolEntry = {
+  description?: string
+}
+
+const processVariableDeclaration = (
+  statement: VariableDeclaration
+): SymbolEntry => ({
+  description: statement
+    .getVariableStatement()
+    ?.getJsDocs()[0]
+    ?.getCommentText()
+})
 
 async function ingestModule(tsconfig: string, mainFile: string) {
   const project = new Project({
@@ -9,27 +22,45 @@ async function ingestModule(tsconfig: string, mainFile: string) {
 
   const exportedDeclarations = main.getExportedDeclarations()
 
+  const symbols = new Map<string, SymbolEntry>()
+
   for (const [name, declarations] of exportedDeclarations) {
-    for (const declaration of declarations) {
-      if (VariableDeclaration.isVariableDeclaration(declaration)) {
-        console.log(
-          (declaration as VariableDeclaration)
-            .getVariableStatement()
-            ?.getJsDocs()[0]
-            ?.getFullText()
-        )
-      }
+    const declaration = declarations[0]
+    if (VariableDeclaration.isVariableDeclaration(declaration)) {
+      symbols.set(
+        name,
+        processVariableDeclaration(declaration as VariableDeclaration)
+      )
     }
   }
+
+  return symbols
 }
 
-ingestModule(
+const symbols = await ingestModule(
   "../../tsconfig.json",
   "../../packages/shader-composer/src/index.ts"
 )
 
+console.log(symbols)
+
+const Symbol = (props: SymbolEntry & { name: string }) => {
+  return (
+    <p>
+      <strong>{props.name}:</strong> {props.description}
+    </p>
+  )
+}
+
 const Morphy = () => {
-  return <p>Hiiii</p>
+  return (
+    <div>
+      <h1>Morphy!</h1>
+      {Object.entries(symbols).map(([name, symbol]) => (
+        <p>{name}</p>
+      ))}
+    </div>
+  )
 }
 
 export default Morphy
