816f151a687fca86728c4c4723cbe4e3215ad282
Hendrik Mans <hendrik@mans.de>
2022-09-03 20:59:35 +0200

    Alright

--- apps/ui-composer-examples/src/App.tsx
+++ apps/ui-composer-examples/src/App.tsx
@@ -1,23 +1,39 @@
-import { styled, globalCss } from "@stitches/react"
+import { styled, globalCss, css } from "@stitches/react"
+
+/* Palette: https://coolors.co/palette/22223b-4a4e69-9a8c98-c9ada7-f2e9e4 */
 
 const globalStyles = globalCss({
-  body: { margin: 0, padding: 0 }
+  body: {
+    margin: 0,
+    padding: 0,
+    height: "100%",
+    width: "100%",
+    overflow: "hidden"
+  },
+  "div#root": {
+    width: "100vw",
+    height: "100vh"
+  }
 })
+
+globalStyles()
 
 const Box = styled("div", {
   backgroundColor: "gainsboro",
   borderRadius: "9999px",
   fontSize: "13px",
   padding: "10px 15px",
+
   "&:hover": {
     backgroundColor: "lightgray"
   }
 })
 
-function UIRoot({ children }: { children?: React.ReactNode }) {
-  globalStyles()
-  return <div>{children}</div>
-}
+const UIRoot = styled("div", {
+  backgroundColor: "#22223B",
+  width: "100%",
+  height: "100%"
+})
 
 function App() {
   return (
