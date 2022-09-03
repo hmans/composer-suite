03ecc366d9cff1bc6254913b09e4de4b46ce98a1
Hendrik Mans <hendrik@mans.de>
2022-09-03 21:36:42 +0200

    Text and heading

--- apps/ui-composer-examples/src/App.tsx
+++ apps/ui-composer-examples/src/App.tsx
@@ -9,7 +9,7 @@ const globalStyles = globalCss({
     height: "100%",
     width: "100%",
     overflow: "hidden",
-    font: "15px/1.3 Inter, sans-serif"
+    font: "15px/1.5 Inter, sans-serif"
   },
   "div#root": {
     width: "100vw",
@@ -28,21 +28,37 @@ const UIRoot = styled("div", {
 
 const Panel = styled("div", {
   backgroundColor: "#333",
-  color: "#F2E9E4",
+  color: "#dcc",
   textShadow: "rgba(0, 0, 0, 0.2) 0px 1px 1px",
-  padding: "0.8rem"
+  padding: "0.8rem",
+  "*:first-child": { marginTop: 0 },
+  "*:last-child": { marginBottom: 0 }
 })
 
 const Text = styled("div", {
   margin: "0.5rem 0"
 })
 
+const Heading = styled("h3", {
+  font: "inherit",
+  fontWeight: "bold",
+  color: "#F2E9E4",
+  margin: "1.5rem 0 0.5rem 0"
+})
+
 const App = () => (
   <UIRoot>
     <Panel>
-      <Text>This is a panel. It displays things. Amazing!</Text>
+      <Heading>Welcome!</Heading>
+      <Text>
+        This is a panel. It displays things. Amazing! Many curious, interesting
+        things, that hopefully will make building editor UIs really useful.
+        (This is just a slightly longer paragraph to see how it renders.)
+      </Text>
       <Text>Like text.</Text>
       <Text>Or even more text.</Text>
+      <Heading>Inputs!</Heading>
+      <Text>We should try an input. Inputs are really cool.</Text>
     </Panel>
   </UIRoot>
 )
