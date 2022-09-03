e56e0f4f3753adfa61210cf596c7d30918be52cc
Hendrik Mans <hendrik@mans.de>
2022-09-03 20:52:32 +0200

    Add stitches

--- apps/ui-composer-examples/src/App.tsx
+++ apps/ui-composer-examples/src/App.tsx
@@ -1,9 +1,30 @@
+import { styled, globalCss } from "@stitches/react"
+
+const globalStyles = globalCss({
+  body: { margin: 0, padding: 0 }
+})
+
+const Box = styled("div", {
+  backgroundColor: "gainsboro",
+  borderRadius: "9999px",
+  fontSize: "13px",
+  padding: "10px 15px",
+  "&:hover": {
+    backgroundColor: "lightgray"
+  }
+})
+
 function UIRoot({ children }: { children?: React.ReactNode }) {
+  globalStyles()
   return <div>{children}</div>
 }
 
 function App() {
-  return <UIRoot>Yooo</UIRoot>
+  return (
+    <UIRoot>
+      <Box>Yooo</Box>
+    </UIRoot>
+  )
 }
 
 export default App
