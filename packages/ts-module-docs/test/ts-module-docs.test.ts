72ae81c11a0858d11c9bf05f742581da7895d2ee
Hendrik Mans <hendrik@mans.de>
2022-09-03 16:49:40 +0200

    Add a mock

--- packages/ts-module-docs/test/ts-module-docs.test.ts
+++ packages/ts-module-docs/test/ts-module-docs.test.ts
@@ -3,27 +3,8 @@ import { extractModuleDocumentation } from "../src"
 describe("extractModuleDocumentation", () => {
   it("does a thing", () => {
     const docs = extractModuleDocumentation(
-      "tsconfig.json",
-      "packages/ts-module-docs/src/index.ts"
+      __dirname + "/mock/tsconfig.json",
+      __dirname + "/mock/mock.ts"
     )
-
-    expect(docs).toMatchInlineSnapshot(`
-      Object {
-        "symbols": Array [
-          Object {
-            "doc": undefined,
-            "name": "extractModuleDocumentation",
-          },
-          Object {
-            "doc": undefined,
-            "name": "renderDocNode",
-          },
-          Object {
-            "doc": undefined,
-            "name": "renderDocNodes",
-          },
-        ],
-      }
-    `)
   })
 })
