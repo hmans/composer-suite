31a05d513f8a95cf7225007acc55b25b9e463f14
Hendrik Mans <hendrik@mans.de>
2022-09-03 10:25:41 +0200

    Render

--- apps/morphydocs/src/components/morphy.ts
+++ apps/morphydocs/src/components/morphy.ts
@@ -1,22 +1,24 @@
 import { JSDocTag, Project, VariableDeclaration } from "ts-morph"
 
-export type SymbolEntry = {
+export type SymbolDescription = {
+  name: string
   description?: string
   tags?: JSDocTag[]
 }
 
 export type ModuleDescription = {
-  symbols: string[]
-  symbolDescriptions: Map<string, SymbolEntry>
+  symbols: SymbolDescription[]
 }
 
 const processVariableDeclaration = (
+  name: string,
   declaration: VariableDeclaration
-): SymbolEntry => {
+): SymbolDescription => {
   const statement = declaration.getVariableStatementOrThrow()
   const jsDoc = statement.getJsDocs()[0]
 
   return {
+    name,
     description: jsDoc?.getDescription(),
     tags: jsDoc?.getTags()
   }
@@ -31,21 +33,19 @@ function morphy(tsconfig: string, mainFile: string): ModuleDescription {
 
   const exportedDeclarations = main.getExportedDeclarations()
 
-  const symbols = new Map<string, SymbolEntry>()
+  const symbols = new Array<SymbolDescription>()
 
   for (const [name, declarations] of exportedDeclarations) {
     const declaration = declarations[0]
     if (VariableDeclaration.isVariableDeclaration(declaration)) {
-      symbols.set(
-        name,
-        processVariableDeclaration(declaration as VariableDeclaration)
+      symbols.push(
+        processVariableDeclaration(name, declaration as VariableDeclaration)
       )
     }
   }
 
   return {
-    symbols: Array.from(symbols.keys()),
-    symbolDescriptions: symbols
+    symbols
   }
 }
 
