552117c9aba26520ca7d4bae2551167acb943fdd
Hendrik Mans <hendrik@mans.de>
2022-09-04 19:19:38 +0200

    Let's go?

--- packages/r3f-stage/src/Application.tsx
+++ packages/r3f-stage/src/Application.tsx
@@ -1,14 +1,25 @@
 import React, { FC } from "react"
+import * as UI from "ui-composer"
 import { ThreeApplication, ThreeApplicationProps } from "./ThreeApplication"
-import { UI } from "./ui/UI"
+import { UI as LegacyUI } from "./ui/UI"
 
 export type ApplicationProps = {} & ThreeApplicationProps
 
 export const Application: FC<ApplicationProps> = ({ ...props }) => {
   return (
-    <>
-      <UI />
-      <ThreeApplication {...props} />
-    </>
+    <UI.Root>
+      <UI.HorizontalGroup>
+        <div style={{ flex: 2 }}>
+          <LegacyUI />
+          <ThreeApplication {...props} />
+        </div>
+        <UI.VerticalGroup css={{ flex: 1 }}>
+          <UI.Panel>
+            <UI.Heading>Hi from UI Composer!</UI.Heading>
+            <p>Hello</p>
+          </UI.Panel>
+        </UI.VerticalGroup>
+      </UI.HorizontalGroup>
+    </UI.Root>
   )
 }
