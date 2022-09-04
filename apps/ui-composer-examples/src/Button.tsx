0bba08b5c07a01b5c36549c5192c829e5813dd4f
Hendrik Mans <hendrik@mans.de>
2022-09-04 16:36:50 +0200

    Cleanup

--- apps/ui-composer-examples/src/Button.tsx
+++ apps/ui-composer-examples/src/Button.tsx
@@ -8,6 +8,7 @@ export const Button = styled("button", {
     "2px 2px 2px 0 rgba(0, 0, 0, 0.5), inset 2px 2px 2px 0 rgba(255, 255, 255, 0.2)",
   border: "1px solid rgba(0, 0, 0, 0.8)",
   borderWidth: "1px 1px 3px 1px",
+
   outline: "none",
 
   font: "inherit",
@@ -15,8 +16,6 @@ export const Button = styled("button", {
   color: "rgba(255, 255, 255, 0.7)",
   cursor: "pointer",
   textShadow: "rgba(0, 0, 0, 0.2) 1px 2px 1px",
-
-  // transition: "background-color 0.1s ease-out",
 
   "&:hover": {
     backgroundColor: "#666"
