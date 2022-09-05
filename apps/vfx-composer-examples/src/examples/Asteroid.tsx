b0124f283463f13b101078bef979175f51ef5e69
Hendrik Mans <hendrik@mans.de>
2022-09-05 15:38:48 +0200

    Fix Asteroid example

--- apps/vfx-composer-examples/src/examples/Asteroid.tsx
+++ apps/vfx-composer-examples/src/examples/Asteroid.tsx
@@ -1,5 +1,9 @@
 import { Animate } from "@hmans/things"
-import { CameraShake, Float, useTexture } from "@react-three/drei"
+import {
+  CameraShake,
+  Float as FloatAnimation,
+  useTexture
+} from "@react-three/drei"
 import { GroupProps, useThree } from "@react-three/fiber"
 import { pipe } from "fp-ts/function"
 import { composable, modules } from "material-composer-r3f"
@@ -16,8 +20,9 @@ import {
   NormalizePlusMinusOne,
   Smoothstep,
   UV,
-  vec2,
-  vec3
+  Vec2,
+  Vec3,
+  Float
 } from "shader-composer"
 import { PSRDNoise2D } from "shader-composer-toybox"
 import { Color, DoubleSide } from "three"
@@ -49,12 +54,12 @@ const Asteroid = (props: GroupProps) => (
       rotation-y={Math.PI / 3}
       position={[-2, -1, 0]}
     >
-      <Float speed={340} rotationIntensity={0} floatIntensity={0.3}>
+      <FloatAnimation speed={340} rotationIntensity={0} floatIntensity={0.3}>
         <Rock />
         <InnerAura />
         <MiddleAura />
         <OuterAura />
-      </Float>
+      </FloatAnimation>
 
       <Sparks />
       <RockSplitters />
@@ -74,7 +79,7 @@ const Rock = () => (
         <modules.SurfaceWobble offset={Mul(time, 0.4)} amplitude={0.1} />
 
         <Lava
-          offset={Mul(vec3(0.4, 0.8, 0.5), time)}
+          offset={Mul(Vec3([0.4, 0.8, 0.5]), time)}
           scale={0.3}
           octaves={5}
           power={1}
@@ -94,8 +99,8 @@ const InnerAura = () => (
       [new Color("#fb8b24").multiplyScalar(2), 0.9],
       [new Color("#f8f9fa").multiplyScalar(8), 1]
     ]}
-    tiling={vec2(3, 0.5)}
-    offset={vec2(0, Negate(Add(time, UV.x)))}
+    tiling={Vec2([3, 0.5])}
+    offset={Vec2([0, Negate(Add(time, UV.x))])}
     wobble={0.04}
   />
 )
@@ -111,8 +116,8 @@ const MiddleAura = () => (
       [new Color("#fb8b24").multiplyScalar(2), 0.9],
       [new Color("#f8f9fa").multiplyScalar(8), 1]
     ]}
-    tiling={vec2(3, 0.5)}
-    offset={vec2(0, Negate(Add(time, UV.x)))}
+    tiling={Vec2([3, 0.5])}
+    offset={Vec2([0, Negate(Add(time, UV.x))])}
     wobble={0.04}
   />
 )
@@ -128,16 +133,16 @@ const OuterAura = () => (
       [new Color("#8338ec").multiplyScalar(2), 0.9],
       [new Color("#ff006e").multiplyScalar(10), 1]
     ]}
-    tiling={vec2(3, 0.5)}
-    offset={vec2(0, Negate(Add(time, UV.x)))}
+    tiling={Vec2([3, 0.5])}
+    offset={Vec2([0, Negate(Add(time, UV.x))])}
     wobble={0.02}
   />
 )
 
 const Sparks = () => {
   const particles = useParticles()
-  const id = float(InstanceID, { varying: true })
-  const getNoise = (offset: Input<"float">) => PSRDNoise2D(vec2(offset, id))
+  const id = Float(InstanceID, { varying: true })
+  const getNoise = (offset: Input<"float">) => PSRDNoise2D(Vec2([offset, id]))
   const clock = useThree((s) => s.clock)
 
   return (
@@ -158,18 +163,18 @@ const Sparks = () => {
 
         <modules.Scale scale={Add(2, getNoise(123))} />
 
-        <modules.SurfaceWobble offset={vec3(time, id, 0)} amplitude={0.5} />
+        <modules.SurfaceWobble offset={Vec3([time, id, 0])} amplitude={0.5} />
 
         <modules.Translate
-          offset={vec3(Mul(getNoise(99), 5), getNoise(67), getNoise(567))}
+          offset={Vec3([Mul(getNoise(99), 5), getNoise(67), getNoise(567)])}
         />
 
         <modules.Velocity
-          direction={vec3(
+          direction={Vec3([
             Mul(getNoise(87843), 2),
             Mul(Add(Abs(getNoise(123)), 1.3), 20),
             Mul(getNoise(278499), 2)
-          )}
+          ])}
           space="local"
           time={particles.age}
         />
@@ -191,8 +196,8 @@ const Sparks = () => {
 
 const RockSplitters = () => {
   const particles = useParticles()
-  const id = float(InstanceID, { varying: true })
-  const getNoise = (offset: Input<"float">) => PSRDNoise2D(vec2(offset, id))
+  const id = Float(InstanceID, { varying: true })
+  const getNoise = (offset: Input<"float">) => PSRDNoise2D(Vec2([offset, id]))
 
   return (
     <Particles capacity={100} safetyCapacity={10}>
@@ -202,21 +207,21 @@ const RockSplitters = () => {
         <modules.Lifetime {...particles} />
 
         <modules.Translate
-          offset={vec3(Mul(getNoise(99), 5), getNoise(67), getNoise(567))}
+          offset={Vec3([Mul(getNoise(99), 5), getNoise(67), getNoise(567)])}
         />
 
         <modules.Velocity
-          direction={vec3(
+          direction={Vec3([
             Mul(getNoise(87843), 2),
             Mul(Add(Abs(getNoise(123)), 1.3), 40),
             Mul(getNoise(278499), 2)
-          )}
+          ])}
           space="local"
           time={particles.age}
         />
 
         <modules.Acceleration
-          direction={vec3(0, -60, 0)}
+          direction={Vec3([0, -60, 0])}
           space="world"
           time={particles.age}
         />
@@ -264,7 +269,7 @@ const SmokeTrail = () => {
           />
 
           <modules.Velocity
-            direction={vec3(0, 10, 0)}
+            direction={Vec3([0, 10, 0])}
             time={particles.age}
             space="local"
           />
@@ -305,7 +310,7 @@ const Clouds = () => {
           <modules.Billboard />
 
           <modules.Velocity
-            direction={vec3(0, 10, 0)}
+            direction={Vec3([0, 10, 0])}
             time={particles.age}
             space="local"
           />
@@ -334,7 +339,7 @@ const WindLines = () => {
 
         <composable.meshBasicMaterial color="#555" side={DoubleSide}>
           <modules.Velocity
-            direction={vec3(0, 100, 0)}
+            direction={Vec3([0, 100, 0])}
             time={particles.age}
             space="local"
           />
