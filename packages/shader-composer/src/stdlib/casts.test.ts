1fb593580ebca673784a748c206d9bcbe081d6ad
Hendrik Mans <hendrik@mans.de>
2022-09-05 11:32:54 +0200

    Add vec3

--- packages/shader-composer/src/stdlib/casts.test.ts
+++ packages/shader-composer/src/stdlib/casts.test.ts
@@ -1,6 +1,7 @@
 import { Int } from "./values"
-import { float } from "./casts"
+import { float, vec3 } from "./casts"
 import { glslRepresentation } from "../glslRepresentation"
+import { Color } from "three"
 
 describe("float", () => {
   it("returns an expression that casts the given value to a float", () => {
@@ -8,7 +9,16 @@ describe("float", () => {
     const cast = float(value)
 
     expect(glslRepresentation(cast)).toEqual(
-      `float(${value._unitConfig.variableName})`
+      `float(${glslRepresentation(value)})`
     )
+  })
+})
+
+describe("vec3", () => {
+  it("returns an expression that casts the given value to a vec3", () => {
+    const value = new Color("red")
+    const cast = vec3(value)
+
+    expect(glslRepresentation(cast)).toEqual(`vec3(vec3(1.0, 0.0, 0.0))`)
   })
 })
--- packages/shader-composer/src/stdlib/casts.ts
+++ packages/shader-composer/src/stdlib/casts.ts
@@ -5,6 +5,8 @@ import { Float, Mat3, Mat4, Vec2, Vec3, Vec4 } from "./values"
 
 export const float = (v: Input<"int" | "float">) => $`float(${v})`
 
+export const vec3 = (v: Input<"vec3">) => $`vec3(${v})`
+
 // export const float = (
 //   v: Input<"float" | "bool" | "int"> = 0,
 //   extras?: Partial<UnitConfig<"float">>
