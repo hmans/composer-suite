e660c428a011921d18841294318c72c88952ef5e
Hendrik Mans <hendrik@mans.de>
2022-09-05 19:59:10 +0200

    BooleanControl

--- packages/ui-composer/src/components/BooleanControl.tsx
+++ packages/ui-composer/src/components/BooleanControl.tsx
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import { styled } from "../styles"
 
 export type BooleanControlProps = {
   description?: string
+  checked?: boolean
+  onChange?: (checked: boolean) => void
 }
 
-export const BooleanControl = ({ description }: BooleanControlProps) => {
+export const BooleanControl = ({
+  description,
+  onChange,
+  checked: initialChecked = false
+}: BooleanControlProps) => {
+  const [checked, setChecked] = useState(initialChecked)
+
   return (
     <>
-      <input type="checkbox" onChange={() => console.log("changed!")} />
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => {
+          onChange?.(e.target.checked)
+          setChecked(e.target.checked)
+        }}
+      />
       {description && <Description>{description}</Description>}
     </>
   )
