c7eeb955a26d9f1082e63e16b76ad62cf3a6992a
Hendrik Mans <hendrik@mans.de>
2022-09-06 11:35:36 +0200

    Add examples

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,5 +1,13 @@
+import "r3f-stage/styles.css"
+import { Application } from "r3f-stage"
+import { Examples } from "./Examples"
+
 function App() {
-  return <p>mooo</p>
+  return (
+    <Application>
+      <Examples />
+    </Application>
+  )
 }
 
 export default App
---
+++ apps/examples/src/Examples.tsx
@@ -0,0 +1,5 @@
+import { Example } from "r3f-stage"
+
+export const Examples = () => (
+  <Example path="input-composer" title="Input Composer"></Example>
+)
