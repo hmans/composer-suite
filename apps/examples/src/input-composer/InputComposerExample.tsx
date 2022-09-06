3040f12d4afa5a787ab7dccec3170b45604c218a
Hendrik Mans <hendrik@mans.de>
2022-09-06 11:47:19 +0200

    Gotta start somewhere

--- apps/examples/src/input-composer/InputComposerExample.tsx
+++ apps/examples/src/input-composer/InputComposerExample.tsx
@@ -1,6 +1,19 @@
 import { FlatStage } from "r3f-stage"
+import { useMemo } from "react"
+import { useFrame } from "@react-three/fiber"
 
 export default function Example() {
+  const moveControl = useMemo(() => {
+    console.log("This is a good place to put your setup code.")
+
+    return () => ({ x: 0, y: 0 })
+  }, [])
+
+  useFrame(() => {
+    const move = moveControl()
+    console.log(move)
+  })
+
   return (
     <FlatStage>
       <Player />
@@ -10,8 +23,8 @@ export default function Example() {
 
 const Player = () => {
   return (
-    <mesh>
-      <dodecahedronGeometry />
+    <mesh position-y={0.5} castShadow>
+      <capsuleGeometry args={[0.25, 0.5, 16, 16]} />
       <meshStandardMaterial color="hotpink" />
     </mesh>
   )
