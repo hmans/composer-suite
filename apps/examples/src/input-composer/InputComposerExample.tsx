78cd4d201d108f449d70f91995d1219271aeddc2
Hendrik Mans <hendrik@mans.de>
2022-09-06 11:49:51 +0200

    fp-ts

--- apps/examples/src/input-composer/InputComposerExample.tsx
+++ apps/examples/src/input-composer/InputComposerExample.tsx
@@ -1,12 +1,24 @@
 import { FlatStage } from "r3f-stage"
 import { useMemo } from "react"
 import { useFrame } from "@react-three/fiber"
+import { pipe } from "fp-ts/lib/function"
+
+type Vector = { x: number; y: number }
+
+const resetVector = (v: Vector) => {
+  v.x = 0
+  v.y = 0
+
+  return v
+}
 
 export default function Example() {
   const moveControl = useMemo(() => {
     console.log("This is a good place to put your setup code.")
 
-    return () => ({ x: 0, y: 0 })
+    const moveVector = { x: 0, y: 0 }
+
+    return () => pipe(moveVector, resetVector)
   }, [])
 
   useFrame(() => {
