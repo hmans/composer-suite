cdc5e619e5e6102c436bfa6a7b63429536a7c9f2
Hendrik Mans <hendrik@mans.de>
2022-09-07 14:48:10 +0200

    Eh

--- apps/examples/src/input-composer/InputComposerExample.tsx
+++ apps/examples/src/input-composer/InputComposerExample.tsx
@@ -1,70 +1,12 @@
 import { GroupProps, useFrame } from "@react-three/fiber"
-import { identity, pipe } from "fp-ts/lib/function"
-import {
-  applyDeadzone,
-  clampVector,
-  getGamepadVector,
-  getKeyboardVector,
-  IDevice,
-  resetVector
-} from "input-composer"
-import gamepadDriver, { GamepadDevice } from "input-composer/drivers/gamepad"
-import keyboardDriver, { KeyboardDevice } from "input-composer/drivers/keyboard"
+import gamepadDriver from "input-composer/drivers/gamepad"
+import keyboardDriver from "input-composer/drivers/keyboard"
 import { Description, FlatStage } from "r3f-stage"
 import { forwardRef, useMemo, useRef } from "react"
 import { Group, Vector3 } from "three"
+import { makeController } from "./makeController"
 
 const tmpVec3 = new Vector3()
-
-const makeController = () => {
-  const state = {
-    keyboard: null as KeyboardDevice | null,
-    gamepad: null as GamepadDevice | null,
-    activeDevice: null as IDevice | null
-  }
-
-  const controls = {
-    move: { x: 0, y: 0 }
-  }
-
-  const setActiveDevice = (device: IDevice) => {
-    if (state.activeDevice === device) return
-    console.log("Switching active device to:", device)
-    state.activeDevice = device
-  }
-
-  gamepadDriver.start()
-  keyboardDriver.start()
-
-  gamepadDriver.onDeviceAppeared.addListener((gamepad) => {
-    state.gamepad = gamepad
-    state.gamepad.onActivity.addListener(() => setActiveDevice(gamepad))
-  })
-
-  keyboardDriver.onDeviceAppeared.addListener((keyboard) => {
-    state.keyboard = keyboard
-    state.keyboard.onActivity.addListener(() => setActiveDevice(keyboard))
-  })
-
-  return {
-    move: () =>
-      pipe(
-        controls.move,
-        resetVector,
-
-        state.gamepad && state.activeDevice === state.gamepad
-          ? getGamepadVector(state.gamepad, 0, 1)
-          : identity,
-
-        state.keyboard && state.activeDevice === state.keyboard
-          ? getKeyboardVector(state.keyboard, "w", "s", "a", "d")
-          : identity,
-
-        applyDeadzone(0.1),
-        clampVector
-      )
-  }
-}
 
 export default function Example({ playerSpeed = 3 }) {
   const player = useRef<Group>(null!)
@@ -76,6 +18,7 @@ export default function Example({ playerSpeed = 3 }) {
     keyboardDriver.update()
 
     const move = controller.move()
+
     player.current.position.add(
       tmpVec3.set(move.x, 0, -move.y).multiplyScalar(playerSpeed * dt)
     )
