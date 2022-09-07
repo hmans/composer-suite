6194b2399946a85cf4ab8e0f6e5c38854338fb3d
Hendrik Mans <hendrik@mans.de>
2022-09-07 11:27:00 +0200

    Comments

--- packages/input-composer/src/types.ts
+++ packages/input-composer/src/types.ts
@@ -5,10 +5,19 @@ export interface IVector {
   y: number
 }
 
-export interface IDriver<D = undefined> {
+export interface IDriver<D extends IDevice> {
+  /** Start the driver. */
   start: () => void
+
+  /** Stop the driver. */
   stop: () => void
+
+  /**
+   * Update the driver. Must be called by the user, every time
+   * driver input is about to be queried (eg. once per frame.)
+   */
   update: () => void
+
   onDeviceAppeared: IEventCallback<D>
   onDeviceDisappeared: IEventCallback<D>
   onDeviceActivity: IEventCallback<D>
