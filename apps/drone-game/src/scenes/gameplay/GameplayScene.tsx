2ab68f4cbdaeb31e683dd41818559fe4e9f636b3
Hendrik Mans <hendrik@mans.de>
2022-09-09 20:27:32 +0200

    Player

--- apps/drone-game/src/scenes/gameplay/GameplayScene.tsx
+++ apps/drone-game/src/scenes/gameplay/GameplayScene.tsx
@@ -9,26 +9,31 @@ import { CuboidCollider, Debug, Physics, RigidBody } from "@react-three/rapier"
 export const GameplayScene = () => {
   return (
     <>
-      <>
-        <ambientLight intensity={0.2} />
-        <directionalLight
-          color="white"
-          intensity={0.7}
-          position={[10, 10, 10]}
-          castShadow
-        />
-        <directionalLight
-          color="white"
-          intensity={0.2}
-          position={[-10, 5, 10]}
-          castShadow
-        />
-      </>
+      <ambientLight intensity={0.2} />
+      <directionalLight
+        color="white"
+        intensity={0.7}
+        position={[10, 10, 10]}
+        castShadow
+      />
+      <directionalLight
+        color="white"
+        intensity={0.2}
+        position={[-10, 5, 10]}
+        castShadow
+      />
 
       <Physics gravity={[0, -5, 0]} colliders={false}>
         <Debug color="red" sleepColor="blue" />
 
-        <RigidBody colliders="hull" restitution={0}>
+        {/* Player */}
+        <RigidBody position={[0, 10, 20]}>
+          <PerspectiveCamera makeDefault />
+          <CuboidCollider args={[2, 2, 2]} />
+        </RigidBody>
+
+        {/* Scenery */}
+        <RigidBody colliders="hull" position-y={3}>
           <mesh castShadow rotation={[0.2, 0.2, 0.2]}>
             <dodecahedronGeometry />
             <meshStandardMaterial
@@ -39,18 +44,15 @@ export const GameplayScene = () => {
           </mesh>
         </RigidBody>
 
+        {/* Ground */}
         <RigidBody type="kinematicPosition">
           <Ground />
-          <CuboidCollider position={[0, -0.5, 0]} args={[10, 0.5, 10]} />
+          <CuboidCollider position={[0, -0.5, 0]} args={[100, 0.5, 100]} />
         </RigidBody>
       </Physics>
 
       <fogExp2 args={["#000", 0.03]} attach="fog" />
       <Environment preset="sunset" />
-
-      <PerspectiveCamera position={[0, 3, 7]} makeDefault />
-
-      <OrbitControls />
     </>
   )
 }
