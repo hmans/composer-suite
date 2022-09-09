a449ba24adeb9fbc468fe15f9dc004d6e7e5ef7d
Hendrik Mans <hendrik@mans.de>
2022-09-09 08:57:59 +0200

    WIP-ish

--- apps/examples/src/input-composer/InputComposerExample.tsx
+++ apps/examples/src/input-composer/InputComposerExample.tsx
@@ -1,8 +1,8 @@
 import { useConst } from "@hmans/things"
 import { GroupProps, useFrame } from "@react-three/fiber"
 import { flow, pipe } from "fp-ts/lib/function"
-import { applyDeadzone, clampVector } from "input-composer"
-import { useKeyboardInput } from "input-composer/react"
+import { applyDeadzone, clampVector, IVector, magnitude } from "input-composer"
+import { useGamepadInput, useKeyboardInput } from "input-composer/react"
 import { Description, FlatStage } from "r3f-stage"
 import { forwardRef, useCallback, useMemo, useRef } from "react"
 import { Group, Vector3 } from "three"
@@ -24,10 +24,20 @@ const onPress = (callback: () => void) => {
   }
 }
 
+const onActivity = (callback: () => void) => (v: IVector) => {
+  if (magnitude(v) > 0) {
+    callback()
+  }
+
+  return v
+}
+
 export default function Example({ playerSpeed = 3 }) {
   const keyboard = useKeyboardInput()
+  const gamepad = useGamepadInput(0)
   const velocity = useConst(() => new Vector3())
   const player = useRef<Group>(null!)
+  const controlScheme = useRef<"keyboard" | "gamepad">("keyboard")
 
   const doubleJump = useCallback(() => {
     let doubleJumped = false
@@ -44,11 +54,31 @@ export default function Example({ playerSpeed = 3 }) {
   }, [velocity, player])
 
   const controller = useMemo(() => {
-    const jumpFlow = onPress(doubleJump())
+    /* Define some generic stick vector handling */
     const stickFlow = flow(applyDeadzone(0.1), clampVector())
+
+    /* Define some specific flows for our two input actions */
+    const jumpFlow = onPress(doubleJump())
     const moveFlow = stickFlow
 
-    const getMoveVector = () => keyboard.getVector("w", "s", "a", "d")
+    /* Define the functions that will actually get the input states */
+    const getKeyboardMoveVector = flow(
+      () => keyboard.getVector("w", "s", "a", "d"),
+      onActivity(() => (controlScheme.current = "keyboard"))
+    )
+
+    const getGamepadMoveVector = flow(
+      () => gamepad.getVector(0, 1),
+      onActivity(() => {
+        console.log("GAMEPAD")
+        controlScheme.current = "gamepad"
+      })
+    )
+
+    const getKeyboardJumpButton = () => !!keyboard.isPressed(" ")
+    const getGamepadJumpButton = () => gamepad.getButton(0)
+
+    const getMoveVector = () => getKeyboardMoveVector()
     const getJumpButton = () => !!keyboard.isPressed(" ")
 
     return () => ({
