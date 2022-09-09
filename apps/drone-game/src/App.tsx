d59c10efacced98015aa380accb18878ee73ad35
Hendrik Mans <hendrik@mans.de>
2022-09-09 17:22:10 +0200

    Set things up

--- apps/drone-game/src/App.tsx
+++ apps/drone-game/src/App.tsx
@@ -1,3 +1,22 @@
+import { RenderCanvas, RenderPipeline } from "render-composer"
+import { Environment, OrbitControls } from "@react-three/drei"
+
 export function App() {
-  return <p>moo</p>
+  return (
+    <RenderCanvas>
+      <RenderPipeline bloom antiAliasing vignette>
+        <Environment preset="sunset" />
+        <mesh>
+          <dodecahedronGeometry />
+          <meshStandardMaterial
+            color="hotpink"
+            metalness={0.3}
+            roughness={0.4}
+          />
+        </mesh>
+
+        <OrbitControls />
+      </RenderPipeline>
+    </RenderCanvas>
+  )
 }
