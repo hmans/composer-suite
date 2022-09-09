94142112b738ccd7514d9d01d13a7bc81e630252
Hendrik Mans <hendrik@mans.de>
2022-09-09 18:03:41 +0200

    Animate

--- apps/drone-game/src/scenes/gameplay/GameplayScene.tsx
+++ apps/drone-game/src/scenes/gameplay/GameplayScene.tsx
@@ -1,15 +1,41 @@
-import { Environment, OrbitControls } from "@react-three/drei"
+import { Animate, rotate } from "@hmans/things"
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera
+} from "@react-three/drei"
+import { GroupProps } from "@react-three/fiber"
 
 export const GameplayScene = () => {
   return (
     <>
       <Environment preset="sunset" />
-      <mesh>
-        <dodecahedronGeometry />
-        <meshStandardMaterial color="hotpink" metalness={0.3} roughness={0.4} />
-      </mesh>
+
+      <Ground />
+
+      <Animate fun={rotate(1, 1.5, -0.4)} position-y={1.5}>
+        <mesh>
+          <dodecahedronGeometry />
+          <meshStandardMaterial
+            color="hotpink"
+            metalness={0.3}
+            roughness={0.4}
+          />
+        </mesh>
+      </Animate>
+
+      <PerspectiveCamera position={[0, 3, 7]} makeDefault />
 
       <OrbitControls />
     </>
   )
 }
+
+const Ground = (props: GroupProps) => (
+  <group {...props}>
+    <mesh rotation-x={-Math.PI / 2}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial color="#111" />
+    </mesh>
+  </group>
+)
