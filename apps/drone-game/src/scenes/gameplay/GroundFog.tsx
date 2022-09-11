dccbc851374991d3114a7138aeac0fd25e0f92f1
Hendrik Mans <hendrik@mans.de>
2022-09-11 15:26:03 +0200

    GPU things up!

--- apps/drone-game/src/scenes/gameplay/GroundFog.tsx
+++ apps/drone-game/src/scenes/gameplay/GroundFog.tsx
@@ -1,18 +1,28 @@
-import { useConst } from "@hmans/things"
 import { useTexture } from "@react-three/drei"
+import { pipe } from "fp-ts/function"
 import { composable, modules } from "material-composer-r3f"
-import { between, plusMinus } from "randomish"
 import { Layers, useRenderPipeline } from "render-composer"
-import { Mul, Rotation3DZ, Time } from "shader-composer"
+import {
+  Input,
+  InstanceID,
+  Mul,
+  NormalizePlusMinusOne,
+  ScaleAndOffset,
+  Vec2,
+  Vec3
+} from "shader-composer"
 import { useUniformUnit } from "shader-composer-r3f"
-import { Emitter, Particles, useParticleAttribute } from "vfx-composer-r3f"
+import { PSRDNoise2D } from "shader-composer-toybox"
+import { Emitter, Particles } from "vfx-composer-r3f"
 import smokeUrl from "../../assets/smoke.png"
 
 export const GroundFog = () => {
   const texture = useTexture(smokeUrl)
 
-  const scale = useParticleAttribute(() => 1 as number)
   const depth = useUniformUnit("sampler2D", useRenderPipeline().depth)
+
+  const InstanceNoise = (offset: Input<"float">) =>
+    PSRDNoise2D(Vec2([InstanceID, offset]))
 
   return (
     <group>
@@ -24,19 +34,25 @@ export const GroundFog = () => {
           transparent
           depthWrite={false}
         >
-          <modules.Scale scale={scale} />
           <modules.Billboard />
+
+          <modules.Scale
+            scale={pipe(InstanceNoise(123), NormalizePlusMinusOne, (v) =>
+              ScaleAndOffset(v, 10, 5)
+            )}
+          />
+
+          <modules.Translate
+            offset={pipe(
+              Vec3([InstanceNoise(1), InstanceNoise(2), InstanceNoise(3)]),
+              (v) => ScaleAndOffset(v, Vec3([30, 1, 30]))
+            )}
+          />
+
           <modules.Softness softness={5} depthTexture={depth} />
         </composable.meshStandardMaterial>
 
-        <Emitter
-          limit={150}
-          rate={Infinity}
-          setup={({ position }) => {
-            position.set(plusMinus(30), between(-2, 4), plusMinus(30))
-            scale.value = between(8, 10)
-          }}
-        />
+        <Emitter limit={150} rate={Infinity} />
       </Particles>
     </group>
   )
