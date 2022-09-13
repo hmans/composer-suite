b22b5a6cade15721867ca406fee9d40ea6e8eca8
Hendrik Mans <hendrik@mans.de>
2022-09-13 16:08:35 +0200

    Fix normal in Rotate

---
+++ .changeset/dry-countries-taste.md
@@ -0,0 +1,5 @@
+---
+"material-composer": patch
+---
+
+The `Rotate` module will now also, by default, rotate the vertex normal. If, for some reason, you don't want this to happen, just set the `normal` option to `false`.
--- packages/material-composer/src/modules/Rotate.ts
+++ packages/material-composer/src/modules/Rotate.ts
@@ -3,11 +3,13 @@ import { Input, Mat3, Mul } from "shader-composer"
 
 type RotateProps = {
   rotation: Input<"mat3" | "mat4">
+  normal?: boolean
 }
 
-export const Rotate: ModuleFactory<RotateProps> = ({ rotation }) => (
+export const Rotate: ModuleFactory<RotateProps> = ({ rotation, normal = true }) => (
   state
 ) => ({
   ...state,
+  normal: normal ? Mul(state.normal, Mat3(rotation)) : state.normal,
   position: Mul(state.position, Mat3(rotation))
 })
