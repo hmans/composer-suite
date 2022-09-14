c2987ee887da01e977ae1b19c0fb21078e233bcb
Hendrik Mans <hendrik@mans.de>
2022-09-14 20:39:50 +0200

    Cleanup

--- packages/render-composer/src/SMAAEffect.tsx
+++ packages/render-composer/src/SMAAEffect.tsx
@@ -1,16 +1,9 @@
-import { useThree } from "@react-three/fiber"
 import * as PP from "postprocessing"
 import { useContext, useMemo } from "react"
 import { EffectPassContext } from "./EffectPass"
 
 export const SMAAEffect = () => {
-  const scene = useThree((s) => s.scene)
-  const camera = useThree((s) => s.camera)
-  const effects = useContext(EffectPassContext)
-
   const effect = useMemo(() => new PP.SMAAEffect(), [])
-
-  effects.useItem(effect)
-
+  useContext(EffectPassContext).useItem(effect)
   return null
 }
