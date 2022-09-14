6dfc2cae33f399eabfacc6eeaaf614d2b185abb8
Hendrik Mans <hendrik@mans.de>
2022-09-14 21:56:21 +0200

    Update effects, too

--- packages/render-composer/src/usePostProcessingEffect.tsx
+++ packages/render-composer/src/usePostProcessingEffect.tsx
@@ -1,3 +1,4 @@
+import { useThree } from "@react-three/fiber"
 import * as PP from "postprocessing"
 import { useContext, useLayoutEffect, useMemo } from "react"
 import { EffectPassContext } from "./EffectPass"
@@ -6,12 +7,21 @@ export const usePostProcessingEffect = <T extends PP.Effect, P>(
   ctor: () => T,
   props: P
 ) => {
+  /* Create effect */
   const effect = useMemo(ctor, [])
 
+  /* Update props on rerender */
   useLayoutEffect(() => {
     Object.assign(effect, props)
   }, [effect, props])
 
+  /* Handle resolution changes */
+  const size = useThree((s) => s.size)
+  useLayoutEffect(() => {
+    effect.setSize(size.width, size.height)
+  }, [effect, size.width, size.height])
+
+  /* Register with the effect pass */
   useContext(EffectPassContext).useItem(effect)
 
   return effect
