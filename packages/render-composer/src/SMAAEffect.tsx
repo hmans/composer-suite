5ce878ff27f118004008a022419d5b08f7ff29d8
Hendrik Mans <hendrik@mans.de>
2022-09-14 21:13:56 +0200

    Props for SMAA

--- packages/render-composer/src/SMAAEffect.tsx
+++ packages/render-composer/src/SMAAEffect.tsx
@@ -1,9 +1,17 @@
 import * as PP from "postprocessing"
-import { useContext, useMemo } from "react"
+import { useContext, useLayoutEffect, useMemo } from "react"
 import { EffectPassContext } from "./EffectPass"
 
-export const SMAAEffect = () => {
-  const effect = useMemo(() => new PP.SMAAEffect(), [])
+export type SMAAEffectProps = ConstructorParameters<typeof PP.SMAAEffect>[0]
+
+export const SMAAEffect = (props: SMAAEffectProps) => {
+  const effect = useMemo(() => new PP.SMAAEffect(props), [])
+
+  useLayoutEffect(() => {
+    Object.assign(effect, props)
+  }, [effect, props])
+
   useContext(EffectPassContext).useItem(effect)
+
   return null
 }
