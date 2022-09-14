454033196ee730e28f74bd9e0c24eda99211ae4b
Hendrik Mans <hendrik@mans.de>
2022-09-14 23:35:25 +0200

    Use new stuff

--- apps/render-composer-examples/src/App.tsx
+++ apps/render-composer-examples/src/App.tsx
@@ -1,16 +1,7 @@
 import { Animate } from "@hmans/r3f-animate"
 import { Environment, Loader } from "@react-three/drei"
 import { Suspense, useLayoutEffect } from "react"
-import {
-  SelectiveBloomEffect,
-  EffectComposer,
-  EffectPass,
-  Canvas,
-  RenderPass,
-  SMAAEffect,
-  useRenderPipeline,
-  VignetteEffect
-} from "render-composer"
+import * as RC from "render-composer"
 import { Object3D } from "three"
 
 const rotate = (o: Object3D, dt: number) => {
@@ -23,45 +14,43 @@ function App() {
     <>
       <Loader />
 
-      <Canvas>
-        <EffectComposer>
-          <RenderPass />
+      <RC.Canvas>
+        <RC.RenderPipeline>
+          <RC.EffectPass>
+            <RC.SelectiveBloomEffect intensity={3} />
+            <RC.SMAAEffect />
+            <RC.VignetteEffect darkness={0.5} />
+          </RC.EffectPass>
 
-          <EffectPass>
-            <SelectiveBloomEffect intensity={3} />
-            <SMAAEffect />
-            <VignetteEffect darkness={0.5} />
-          </EffectPass>
-        </EffectComposer>
+          <Suspense>
+            <color attach="background" args={["#264653"]} />
+            <Environment preset="sunset" />
 
-        <Suspense>
-          <color attach="background" args={["#264653"]} />
-          <Environment preset="sunset" />
+            <directionalLight position={[30, 10, 10]} intensity={1.5} />
 
-          <directionalLight position={[30, 10, 10]} intensity={1.5} />
+            {/* <Sun /> */}
 
-          {/* <Sun /> */}
-
-          <Animate
-            fun={(o, _, { clock }) => {
-              o.position.x = Math.sin(clock.getElapsedTime() * 0.7) * 2
-              o.position.y = Math.sin(clock.getElapsedTime() * 1.1)
-              o.position.z = Math.cos(clock.getElapsedTime() * 0.5)
-            }}
-          >
-            <Animate fun={rotate}>
-              <mesh>
-                <icosahedronGeometry />
-                <meshStandardMaterial
-                  color="#E9C46A"
-                  metalness={0.5}
-                  roughness={0.5}
-                />
-              </mesh>
+            <Animate
+              fun={(o, _, { clock }) => {
+                o.position.x = Math.sin(clock.getElapsedTime() * 0.7) * 2
+                o.position.y = Math.sin(clock.getElapsedTime() * 1.1)
+                o.position.z = Math.cos(clock.getElapsedTime() * 0.5)
+              }}
+            >
+              <Animate fun={rotate}>
+                <mesh>
+                  <icosahedronGeometry />
+                  <meshStandardMaterial
+                    color="#E9C46A"
+                    metalness={0.5}
+                    roughness={0.5}
+                  />
+                </mesh>
+              </Animate>
             </Animate>
-          </Animate>
-        </Suspense>
-      </Canvas>
+          </Suspense>
+        </RC.RenderPipeline>
+      </RC.Canvas>
     </>
   )
 }
