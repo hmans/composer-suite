422f7cc600d14ffdfc23e6c7d21fe70809bc5638
Hendrik Mans <hendrik@mans.de>
2022-09-14 21:47:32 +0200

    Use new bits in r3f-stage

--- packages/r3f-stage/src/ThreeApplication.tsx
+++ packages/r3f-stage/src/ThreeApplication.tsx
@@ -2,7 +2,16 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { useControls } from "leva"
 import { Perf } from "r3f-perf"
 import React, { FC, ReactNode } from "react"
-import { Canvas, RenderPipeline } from "render-composer"
+import {
+  Canvas,
+  EffectComposer,
+  EffectPass,
+  RenderPass,
+  RenderPipeline,
+  SelectiveBloomEffect,
+  SMAAEffect,
+  VignetteEffect
+} from "render-composer"
 import { Layers } from "./Layers"
 
 export type ThreeApplicationProps = {
@@ -42,60 +51,66 @@ export const ThreeApplication: FC<ThreeApplicationProps> = ({
 
   return (
     <Canvas dpr={controls.dpr}>
-      <RenderPipeline
-        bloom={controls.effects}
-        vignette={controls.effects}
-        antiAliasing={controls.effects}
-      >
-        {/* Background color */}
-        <color args={["#222"]} attach="background" />
+      <EffectComposer>
+        <RenderPass />
 
-        {/* Fog */}
-        <fogExp2 args={["#222", 0.03]} attach="fog" />
-
-        {/* Lights */}
-        {lights && (
-          <>
-            <ambientLight intensity={0.2} />
-            <directionalLight
-              color="white"
-              intensity={0.7}
-              position={[10, 10, 10]}
-              castShadow
-            />
-            <directionalLight
-              color="white"
-              intensity={0.2}
-              position={[-10, 5, 10]}
-              castShadow
-            />
-          </>
+        {controls.effects && (
+          <EffectPass>
+            <SMAAEffect />
+            <SelectiveBloomEffect />
+            <VignetteEffect />
+          </EffectPass>
         )}
+      </EffectComposer>
 
-        {/* Camera */}
-        <PerspectiveCamera
-          position={[0, 2, 8]}
-          layers-mask={Layers.Default + Layers.TransparentFX}
-          makeDefault
-        />
+      {/* Background color */}
+      <color args={["#222"]} attach="background" />
 
-        {/* Camera Controls */}
-        <OrbitControls
-          makeDefault
-          maxDistance={30}
-          minDistance={3}
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI * 0.48}
-          autoRotate
-          autoRotateSpeed={controls.autoRotate}
-        />
+      {/* Fog */}
+      <fogExp2 args={["#222", 0.03]} attach="fog" />
 
-        {/* Examples */}
-        {children}
+      {/* Lights */}
+      {lights && (
+        <>
+          <ambientLight intensity={0.2} />
+          <directionalLight
+            color="white"
+            intensity={0.7}
+            position={[10, 10, 10]}
+            castShadow
+          />
+          <directionalLight
+            color="white"
+            intensity={0.2}
+            position={[-10, 5, 10]}
+            castShadow
+          />
+        </>
+      )}
 
-        {/* Performance Monitor */}
-        {controls.performance && <Perf position="bottom-right" />}
-      </RenderPipeline>
+      {/* Camera */}
+      <PerspectiveCamera
+        position={[0, 2, 8]}
+        layers-mask={Layers.Default + Layers.TransparentFX}
+        makeDefault
+      />
+
+      {/* Camera Controls */}
+      <OrbitControls
+        makeDefault
+        maxDistance={30}
+        minDistance={3}
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI * 0.48}
+        autoRotate
+        autoRotateSpeed={controls.autoRotate}
+      />
+
+      {/* Examples */}
+      {children}
+
+      {/* Performance Monitor */}
+      {controls.performance && <Perf position="bottom-right" />}
     </Canvas>
   )
 }
