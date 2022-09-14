54a2cdaebdde11ed058229e4215df0fc53864bcf
Hendrik Mans <hendrik@mans.de>
2022-09-14 19:02:50 +0200

    useItem

--- packages/render-composer/src/RenderPass.tsx
+++ packages/render-composer/src/RenderPass.tsx
@@ -1,24 +1,16 @@
 import { useThree } from "@react-three/fiber"
 import { RenderPass as RenderPassImpl } from "postprocessing"
-import { useContext, useLayoutEffect } from "react"
+import { useContext, useMemo } from "react"
 import { EffectComposerContext } from "./EffectComposer"
 
 export const RenderPass = () => {
-  const passes = useContext(EffectComposerContext)
   const scene = useThree((s) => s.scene)
   const camera = useThree((s) => s.camera)
 
-  useLayoutEffect(() => {
-    const pass = new RenderPassImpl(scene, camera)
+  const pass = useMemo(() => new RenderPassImpl(scene, camera), [scene, camera])
 
-    passes.addItem(pass)
-    passes.bumpVersion()
-
-    return () => {
-      passes.removeItem(pass)
-      passes.bumpVersion()
-    }
-  }, [])
+  const { useItem } = useContext(EffectComposerContext)
+  useItem(pass)
 
   return null
 }
