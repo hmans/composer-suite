b550510f277f0aebea2ae48d491365f72eabcd3c
Hendrik Mans <hendrik@mans.de>
2022-09-15 13:26:42 +0200

    LambdaPass

--- packages/render-composer/src/passes/index.ts
+++ packages/render-composer/src/passes/index.ts
@@ -1,5 +1,6 @@
 export * from "./CopyPass"
 export * from "./DepthCopyPass"
 export * from "./EffectPass"
+export * from "./LambdaPass"
 export * from "./LayerRenderPass"
 export * from "./RenderPass"
