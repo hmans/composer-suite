149207bf7a366b2ceac92e95b6dd9ed65cb78d6b
Hendrik Mans <hendrik@mans.de>
2022-09-19 15:41:09 +0200

    The beginnings of MaterialModules

---
+++ packages/material-composer-r3f/src/MaterialModules.tsx
@@ -0,0 +1,30 @@
+import { useConst } from "@hmans/use-const"
+import { useInstanceHandle } from "@react-three/fiber"
+import { compileModules } from "material-composer"
+import React, { useLayoutEffect, useMemo, useRef } from "react"
+import { useShader } from "shader-composer-r3f"
+import { provideModuleRegistration } from "./moduleRegistration"
+
+export type MaterialModulesProps = {
+  children?: React.ReactNode
+}
+
+export const MaterialModules = ({ children }: MaterialModulesProps) => {
+  const modules = provideModuleRegistration()
+  const root = useMemo(() => compileModules(modules.list), [modules.version])
+  const shader = useShader(() => root, [root])
+
+  const object = useConst(() => ({}))
+  const ref = useRef()
+  const instance = useInstanceHandle(ref)
+
+  useLayoutEffect(() => {
+    console.log(instance.current.parent)
+  }, [])
+
+  return (
+    <primitive object={object} ref={ref}>
+      {children}
+    </primitive>
+  )
+}
