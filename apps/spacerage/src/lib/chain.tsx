a313b692cf676058cc37378f6b9ad6f141c70cdf
Hendrik Mans <hendrik@mans.de>
2022-09-21 13:41:19 +0200

    Remove obsolete code

--- apps/spacerage/src/lib/chain.tsx
+++
@@ -1,7 +0,0 @@
-export const chain =
-  <A extends any[]>(
-    firstFun: (...args: A) => void,
-    ...functions: ((...args: any[]) => void)[]
-  ) =>
-  (...args: A) =>
-    functions.reduce((_, next) => next(...args), firstFun(...args))
