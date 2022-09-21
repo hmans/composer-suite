71242ebe59390cb03808345aa211f29195b8d195
Hendrik Mans <hendrik@mans.de>
2022-09-21 14:51:15 +0200

    A little bit of movement

--- apps/spacerage/src/scenes/gameplay/Player.tsx
+++ apps/spacerage/src/scenes/gameplay/Player.tsx
@@ -1,11 +1,25 @@
 import { useGLTF } from "@react-three/drei"
-import { RigidBody } from "@react-three/rapier"
+import { useFrame } from "@react-three/fiber"
+import { RigidBody, RigidBodyApi } from "@react-three/rapier"
+import { useInput } from "input-composer"
+import { useRef } from "react"
 
 export const Player = () => {
+  const body = useRef<RigidBodyApi>(null!)
   const gltf = useGLTF("/models/spaceship25.gltf")
+  const input = useInput()
+
+  useFrame(() => {
+    const horizontal = input.keyboard.axis("KeyA", "KeyD")
+    const vertical = input.keyboard.axis("KeyS", "KeyW")
+
+    body.current.resetForces()
+
+    body.current.addForce({ x: horizontal * 2, y: vertical * 2, z: 0 })
+  })
 
   return (
-    <RigidBody>
+    <RigidBody ref={body} linearDamping={2} angularDamping={0.5}>
       <primitive object={gltf.scene} scale={0.1} />
     </RigidBody>
   )
