34e054da5e5faa4ba1d62e8e19dccf42939a76d0
Hendrik Mans <hendrik@mans.de>
2022-09-21 19:51:02 +0200

    Eh

--- apps/spacerage/src/scenes/gameplay/Player.tsx
+++ apps/spacerage/src/scenes/gameplay/Player.tsx
@@ -3,9 +3,12 @@ import { useFrame } from "@react-three/fiber"
 import { RigidBody, RigidBodyApi } from "@react-three/rapier"
 import { useInput } from "input-composer"
 import { useRef } from "react"
+import { Quaternion, Vector3 } from "three"
 import { Stage } from "../../configuration"
 import { useCapture } from "../../lib/useCapture"
 import { gameplayStore } from "./state"
+
+const tmpVec3 = new Vector3()
 
 export const Player = () => {
   const body = useRef<RigidBodyApi>(null!)
@@ -17,7 +20,27 @@ export const Player = () => {
     const vertical = input.keyboard.axis("KeyS", "KeyW")
 
     body.current.resetForces()
+    body.current.resetTorques()
+
+    /* Move player */
     body.current.addForce({ x: horizontal * 2, y: vertical * 2, z: 0 })
+
+    /* Rotate player in direction of movement */
+    const velocity = tmpVec3.copy(body.current.linvel()).normalize()
+
+    const forward = new Vector3(-1, 0, 0).applyQuaternion(
+      body.current.rotation()
+    )
+    // console.log(forward)
+    const dot = forward.dot(velocity)
+    console.log(dot)
+
+    // Rotate the player into the direction of his velocity
+    body.current.addTorque({
+      x: 0,
+      y: 0,
+      z: dot * 0.1
+    })
   }, Stage.Early)
 
   return (
