59a56ca80d3fe1ad12a39c612f0eaf7cc706adcc
Hendrik Mans <hendrik@mans.de>
2022-09-21 19:20:59 +0200

    Use Physics stage

--- apps/spacerage/src/scenes/gameplay/GameplayScene.tsx
+++ apps/spacerage/src/scenes/gameplay/GameplayScene.tsx
@@ -2,11 +2,16 @@ import { Skybox } from "../../common/Skybox"
 import { Player } from "./Player"
 import { Physics } from "@react-three/rapier"
 import { FollowCamera } from "./FollowCamera"
+import { Stage } from "../../configuration"
 
 export const GameplayScene = () => {
   return (
     <group>
-      <Physics gravity={[0, 0, 0]} timeStep={1 / 100}>
+      <Physics
+        gravity={[0, 0, 0]}
+        timeStep={1 / 100}
+        updatePriority={Stage.Physics}
+      >
         <Skybox />
         <FollowCamera />
 
