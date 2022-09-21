b8cbcebc0a7f581b951ca37f8306370323c1852a
Hendrik Mans <hendrik@mans.de>
2022-09-21 12:15:28 +0200

    MC

--- packages/material-composer-r3f/src/index.ts
+++ packages/material-composer-r3f/src/index.ts
@@ -2,4 +2,9 @@ export * from "@material-composer/patch-material"
 export * from "@material-composer/patched"
 export * from "./composable"
 export * from "./Layer"
-export { ModuleReactor as modules, moduleComponent } from "./reactor"
+export { moduleComponent, ModuleReactor as Modules } from "./reactor"
+
+/* Legacy */
+/* TODO: remove in future version */
+export { Composable as composable } from "./composable"
+export { ModuleReactor as modules } from "./reactor"
