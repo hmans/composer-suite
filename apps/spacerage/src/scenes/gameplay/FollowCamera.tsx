61edba2a788192a88ddd6c42dc032ffff8f61036
Hendrik Mans <hendrik@mans.de>
2022-09-21 18:50:02 +0200

    Tweak camera

--- apps/spacerage/src/scenes/gameplay/FollowCamera.tsx
+++ apps/spacerage/src/scenes/gameplay/FollowCamera.tsx
@@ -1,4 +1,5 @@
 import { useFrame } from "@react-three/fiber"
+import { useEffect } from "react"
 import { useStore } from "statery"
 import { Vector3 } from "three"
 import { Stage } from "../../configuration"
@@ -6,17 +7,22 @@ import { store } from "../../state"
 import { gameplayStore } from "./state"
 
 const offset = new Vector3(0, 0, 10)
+const playerPos = new Vector3()
 const tmpVec3 = new Vector3()
 
 export const FollowCamera = () => {
   const { player } = useStore(gameplayStore)
   const { camera } = useStore(store)
 
+  useEffect(() => {
+    if (camera) camera.quaternion.set(0, 0, 0, 1)
+  }, [camera])
+
   useFrame(() => {
     if (camera && player) {
-      player.getWorldPosition(tmpVec3)
-      camera.lookAt(tmpVec3)
-      camera.position.lerp(tmpVec3.add(offset), 0.1)
+      player.getWorldPosition(playerPos)
+      const cameraTarget = tmpVec3.copy(playerPos).add(offset)
+      camera.position.lerp(cameraTarget, 0.2)
     }
   }, Stage.Late)
 
