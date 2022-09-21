7137995bc4d1115332a349609749e68be7d5b42d
Hendrik Mans <hendrik@mans.de>
2022-09-21 13:52:49 +0200

    GameplayScene

--- apps/spacerage/src/App.tsx
+++ apps/spacerage/src/App.tsx
@@ -2,18 +2,24 @@ import { Loader } from "@react-three/drei"
 import { Suspense } from "react"
 import * as RC from "render-composer"
 import { PostProcessing } from "./common/PostProcessing"
+import { GameplayScene } from "./scenes/gameplay/GameplayScene"
 import { MenuScene } from "./scenes/menu/MenuScene"
 import { GameState } from "./state"
 
 export const App = () => (
   <>
     <Loader />
+
     <RC.Canvas dpr={1}>
       <RC.RenderPipeline>
         <PostProcessing />
         <Suspense>
           <GameState.Match state="menu">
             <MenuScene />
+          </GameState.Match>
+
+          <GameState.Match state="gameplay">
+            <GameplayScene />
           </GameState.Match>
         </Suspense>
       </RC.RenderPipeline>
--- apps/spacerage/src/state.ts
+++ apps/spacerage/src/state.ts
@@ -1,3 +1,3 @@
 import { createStateMachine } from "state-composer"
 
-export const GameState = createStateMachine<"menu">("menu")
+export const GameState = createStateMachine<"menu" | "gameplay">("menu")
