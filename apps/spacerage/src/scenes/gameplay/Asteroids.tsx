2ff75e4831d344bcfa27395a986641e90ef18fcc
Hendrik Mans <hendrik@mans.de>
2022-09-22 15:00:39 +0200

    More asteroids

--- apps/spacerage/src/scenes/gameplay/Asteroids.tsx
+++ apps/spacerage/src/scenes/gameplay/Asteroids.tsx
@@ -1,10 +1,24 @@
+import { useGLTF } from "@react-three/drei"
+import { between, plusMinus } from "randomish"
+import { Material, Mesh } from "three"
+import { Emitter, Particles } from "vfx-composer-r3f"
 import { Asteroid } from "./Asteroid"
 
 export const Asteroids = () => {
+  const gltf = useGLTF("/models/asteroid03.gltf")
+  const mesh = gltf.scene.children[0] as Mesh
+
   return (
-    <>
-      <Asteroid position={[5, 3, 0]} />
-      <Asteroid position={[-3, -5, 0]} />
-    </>
+    <Particles geometry={mesh.geometry} material={mesh.material as Material}>
+      <Emitter
+        limit={1000}
+        rate={Infinity}
+        setup={({ position, rotation, scale }) => {
+          position.set(plusMinus(100), plusMinus(100), 0)
+          rotation.random()
+          scale.setScalar(between(0.8, 2))
+        }}
+      />
+    </Particles>
   )
 }
