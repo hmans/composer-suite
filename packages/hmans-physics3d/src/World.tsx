4417d0aa527e62a7d5b14020dbafc763f0512416
Hendrik Mans <hendrik@mans.de>
2022-09-22 10:45:05 +0200

    Gravity

--- packages/hmans-physics3d/src/World.tsx
+++ packages/hmans-physics3d/src/World.tsx
@@ -6,14 +6,13 @@ import { importRapier } from "./util/importRapier"
 
 export type WorldProps = {
   children?: ReactNode
+  gravity?: [number, number, number]
 }
 
-export const World = ({ children }: WorldProps) => {
+export const World = ({ children, gravity = [0, -9.81, 0] }: WorldProps) => {
   useAsset(importRapier)
 
-  const world = useConst(
-    () => new RAPIER.World(new RAPIER.Vector3(0, -9.81, 0))
-  )
+  const world = useConst(() => new RAPIER.World(new RAPIER.Vector3(...gravity)))
 
   return <>{children}</>
 }
---
+++ packages/hmans-physics3d/src/types.ts
@@ -0,0 +1,5 @@
+export interface IVector3 {
+  x: number
+  y: number
+  z: number
+}
