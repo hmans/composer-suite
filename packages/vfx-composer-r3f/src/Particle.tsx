10d2cf7d4411c3aa74761609c1f4cb703bdae27c
Hendrik Mans <hendrik@mans.de>
2022-09-22 15:49:08 +0200

    Make things nice

--- packages/vfx-composer-r3f/src/Particle.tsx
+++ packages/vfx-composer-r3f/src/Particle.tsx
@@ -11,6 +11,14 @@ import { useParticlesContext } from "./Particles"
 
 const hideMatrix = new Matrix4().makeScale(0, 0, 0)
 
+/**
+ * Use `<Particle>` to emit a single particle that remains CPU-controlled, meaning
+ * that it will continuously update its instance matrix to match its transform
+ * in the Three.js scene graph.
+ *
+ * **Note:** As soon as you use just a single `<Particle>` in your `<Particles>` effect,
+ * it will disable the partial buffer updates, and result in a full buffer update every frame.
+ */
 export const Particle = forwardRef<Object3D, Object3DProps>((props, ref) => {
   const sceneObject = useRef<Object3D>(null!)
   const particles = useParticlesContext()
@@ -28,6 +36,7 @@ export const Particle = forwardRef<Object3D, Object3DProps>((props, ref) => {
     }
   }, [])
 
+  /* Every frame, update the particle's matrix, and queue a re-upload */
   useFrame(() => {
     particles.setMatrixAt(id, sceneObject.current.matrixWorld)
     particles.instanceMatrix.needsUpdate = true
