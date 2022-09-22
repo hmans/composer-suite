029f8811cc11d4bbfd8dd2f9355e6f70432a1bda
Hendrik Mans <hendrik@mans.de>
2022-09-22 13:37:06 +0200

    Time-based camera follow

--- apps/spacerage/src/scenes/gameplay/FollowCamera.tsx
+++ apps/spacerage/src/scenes/gameplay/FollowCamera.tsx
@@ -18,11 +18,11 @@ export const FollowCamera = () => {
     if (camera) camera.quaternion.set(0, 0, 0, 1)
   }, [camera])
 
-  useFrame(() => {
+  useFrame((_, dt) => {
     if (camera && player) {
       player.getWorldPosition(playerPos)
       const cameraTarget = tmpVec3.copy(playerPos).add(offset)
-      camera.position.lerp(cameraTarget, 0.2)
+      camera.position.lerp(cameraTarget, 2 * dt)
     }
   }, Stage.Late)
 
