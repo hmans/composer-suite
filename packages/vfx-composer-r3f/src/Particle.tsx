fb1a7e227dcc4dd902e86a86852fa19fc2308215
Hendrik Mans <hendrik@mans.de>
2022-09-22 15:30:00 +0200

    Particle!

--- packages/vfx-composer-r3f/src/Particle.tsx
+++ packages/vfx-composer-r3f/src/Particle.tsx
@@ -1,16 +1,24 @@
 import { Object3DProps } from "@react-three/fiber"
-import React, { forwardRef, useImperativeHandle, useRef } from "react"
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  useLayoutEffect,
+  useRef
+} from "react"
 import { Object3D } from "three"
-import { Emitter } from "./Emitter"
+import { useParticlesContext } from "./Particles"
 
 export const Particle = forwardRef<Object3D, Object3DProps>((props, ref) => {
   const sceneObject = useRef<Object3D>(null!)
+  const particles = useParticlesContext()
 
+  /* Spawn a particle on mount */
+  useLayoutEffect(() => {
+    particles.emit(1)
+  }, [particles])
+
+  /* Forward the ref */
   useImperativeHandle(ref, () => sceneObject.current)
 
-  return (
-    <object3D ref={sceneObject}>
-      <Emitter limit={1} rate={Infinity} />
-    </object3D>
-  )
+  return <object3D ref={sceneObject} {...props} />
 })
