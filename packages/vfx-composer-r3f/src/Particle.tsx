d742cd02f69f263b1fb22d51c840b909d22e38e0
Hendrik Mans <hendrik@mans.de>
2022-09-22 15:44:54 +0200

    It works

--- packages/vfx-composer-r3f/src/Particle.tsx
+++ packages/vfx-composer-r3f/src/Particle.tsx
@@ -1,21 +1,42 @@
-import { Object3DProps } from "@react-three/fiber"
+import { useConst } from "@hmans/use-const"
+import { Object3DProps, useFrame } from "@react-three/fiber"
 import React, {
   forwardRef,
   useImperativeHandle,
   useLayoutEffect,
   useRef
 } from "react"
-import { Object3D } from "three"
+import { Matrix4, Object3D, Quaternion, Vector3 } from "three"
 import { useParticlesContext } from "./Particles"
 
 export const Particle = forwardRef<Object3D, Object3DProps>((props, ref) => {
   const sceneObject = useRef<Object3D>(null!)
   const particles = useParticlesContext()
 
-  /* Spawn a particle on mount */
-  useLayoutEffect(() => {
+  const id = useConst(() => {
+    const cursor = particles.cursor
     particles.emit(1)
-  }, [particles])
+    return cursor
+  })
+
+  /* Hide the particle again on unmount */
+  useLayoutEffect(() => {
+    return () => {
+      particles.setMatrixAt(
+        id,
+        new Matrix4().compose(
+          new Vector3(),
+          new Quaternion(),
+          new Vector3(0, 0, 0)
+        )
+      )
+    }
+  }, [])
+
+  useFrame(() => {
+    particles.setMatrixAt(id, sceneObject.current.matrixWorld)
+    particles.instanceMatrix.needsUpdate = true
+  })
 
   /* Forward the ref */
   useImperativeHandle(ref, () => sceneObject.current)
