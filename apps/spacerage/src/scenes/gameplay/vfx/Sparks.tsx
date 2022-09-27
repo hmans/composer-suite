40bcdde3ad092670e9123a8c4bb1091c535ec4f9
Hendrik Mans <hendrik@mans.de>
2022-09-27 12:45:05 +0200

    Memoize SparksMaterial

--- apps/spacerage/src/scenes/gameplay/vfx/Sparks.tsx
+++ apps/spacerage/src/scenes/gameplay/vfx/Sparks.tsx
@@ -1,5 +1,6 @@
 import { Composable, Modules } from "material-composer-r3f"
 import { between, upTo } from "randomish"
+import { memo } from "react"
 import { Mul, OneMinus, Vec3 } from "shader-composer"
 import { Color } from "three"
 import { createParticleLifetime } from "vfx-composer"
@@ -9,29 +10,32 @@ import { ECS } from "../state"
 
 const lifetime = createParticleLifetime()
 
-export const Sparks = () => {
+const SparksMaterial = memo(() => {
   const rng = InstanceRNG()
-
   const direction = Vec3([rng(12), rng(84), rng(1)])
 
   return (
-    <InstancedParticles capacity={200}>
+    <Composable.MeshStandardMaterial>
+      <Modules.Scale scale={OneMinus(lifetime.progress)} />
+      <Modules.Velocity
+        direction={Mul(direction, 5)}
+        time={lifetime.age}
+        space="local"
+      />
+      <Modules.Color color={new Color("yellow").multiplyScalar(2)} />
+      <Modules.Lifetime {...lifetime} />
+    </Composable.MeshStandardMaterial>
+  )
+})
+
+export const Sparks = () => {
+  const emitters = ECS.useArchetype("isSparks", "jsx").entities
+
+  return (
+    <InstancedParticles>
       <planeGeometry args={[0.1, 0.1]} />
-
-      <Composable.meshStandardMaterial>
-        <Modules.Scale scale={OneMinus(lifetime.progress)} />
-        <Modules.Velocity
-          direction={Mul(direction, 5)}
-          time={lifetime.age}
-          space="local"
-        />
-        <Modules.Color color={new Color("yellow").multiplyScalar(2)} />
-        <Modules.Lifetime {...lifetime} />
-      </Composable.meshStandardMaterial>
-
-      <ECS.ManagedEntities tag="isSparks">
-        {(entity) => entity.jsx!}
-      </ECS.ManagedEntities>
+      <SparksMaterial />
+      <ECS.Entities entities={emitters}>{(entity) => entity.jsx!}</ECS.Entities>
     </InstancedParticles>
   )
 }
