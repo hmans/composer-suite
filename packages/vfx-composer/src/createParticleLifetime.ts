c4d7724b98cc97a274d859d64c315bb9d567f012
Hendrik Mans <hendrik@mans.de>
2022-09-27 09:26:18 +0200

    createParticleLifetimeAPI

--- packages/vfx-composer/src/createParticleLifetime.ts
+++ packages/vfx-composer/src/createParticleLifetime.ts
@@ -1,12 +1,29 @@
-import { Div, GlobalTime, Sub, Vec2 } from "shader-composer"
+import { Div, GlobalTime, Input, Sub, Vec2 } from "shader-composer"
 import { Vector2 } from "three"
 import { ParticleAttribute } from "./ParticleAttribute"
 
 export type ParticleUnits = ReturnType<typeof createParticleLifetime>
 
 export const createParticleLifetime = () => {
-  const attribute = ParticleAttribute(new Vector2())
-  const { x: startTime, y: endTime } = Vec2(attribute)
+  const lifetime = ParticleAttribute(new Vector2())
+
+  const api = createParticleLifetimeAPI(lifetime)
+
+  return {
+    ...api,
+
+    /** Sets the current value of the lifetime attribute. The value will be read
+    by the particle system once the particle is spawned. */
+    setLifetime: (duration: number, offset: number = 0) =>
+      lifetime.value.set(
+        api.time.value + offset,
+        api.time.value + offset + duration
+      )
+  }
+}
+
+export const createParticleLifetimeAPI = (lifetime: Input<"vec2">) => {
+  const { x: startTime, y: endTime } = Vec2(lifetime)
   const time = GlobalTime
   const age = Sub(time, startTime)
   const maxAge = Sub(endTime, startTime)
@@ -15,10 +32,8 @@ export const createParticleLifetime = () => {
     /** The time uniform used by the particle system. */
     time,
 
-    /** Sets the current value of the lifetime attribute. The value will be read
-    by the particle system once the particle is spawned. */
-    setLifetime: (duration: number, offset: number = 0) =>
-      attribute.value.set(time.value + offset, time.value + offset + duration),
+    /** The actual lifetime attribute. */
+    lifetime,
 
     /** The absolute age, in seconds, of the particle. */
     age,
