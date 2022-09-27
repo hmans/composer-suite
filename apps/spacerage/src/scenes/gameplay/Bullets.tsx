b363b2bcac161af5c58c364097f2c819c6c70267
Hendrik Mans <hendrik@mans.de>
2022-09-27 15:37:04 +0200

    Tweak Bullets

--- apps/spacerage/src/scenes/gameplay/Bullets.tsx
+++ apps/spacerage/src/scenes/gameplay/Bullets.tsx
@@ -3,16 +3,18 @@ import { InstancedParticles } from "vfx-composer-r3f"
 import { ECS } from "./state"
 
 export const Bullets = () => {
+  const { entities } = ECS.useArchetype("isBullet", "jsx")
+
   return (
     <InstancedParticles>
       <planeGeometry args={[0.1, 0.8]} />
       <meshBasicMaterial color={new Color("yellow").multiplyScalar(2)} />
 
-      <ECS.ManagedEntities tag="isBullet">
+      <ECS.Entities entities={entities}>
         {(entity) => (
           <ECS.Component name="sceneObject">{entity.jsx}</ECS.Component>
         )}
-      </ECS.ManagedEntities>
+      </ECS.Entities>
     </InstancedParticles>
   )
 }
