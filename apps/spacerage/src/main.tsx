fe935405d4a3ef0b5118d386b290fe6272b8a023
Hendrik Mans <hendrik@mans.de>
2022-09-27 12:42:54 +0200

    Enable debugging

--- apps/spacerage/src/main.tsx
+++ apps/spacerage/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import * as SC from "shader-composer"
 import { App } from "./App"
 import "./index.css"
 
@@ -8,3 +9,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
   </React.StrictMode>
 )
+
+SC.enableDebugging()
