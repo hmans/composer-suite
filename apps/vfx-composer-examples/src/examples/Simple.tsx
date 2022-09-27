351f56ade5a4393faf739addc1c2624093406954
Hendrik Mans <hendrik@mans.de>
2022-09-27 06:29:50 +0200

    Simplify example

--- apps/vfx-composer-examples/src/examples/Simple.tsx
+++ apps/vfx-composer-examples/src/examples/Simple.tsx
@@ -36,7 +36,7 @@ export const Simple = () => {
             direction={new Vector3(0, -2, 0)}
             time={particles.age}
           />
-          <modules.Lifetime {...particles} />
+          <modules.Lifetime progress={particles.progress} />
         </composable.meshStandardMaterial>
 
         {/* The other important component here is the emitter, which will, as you
