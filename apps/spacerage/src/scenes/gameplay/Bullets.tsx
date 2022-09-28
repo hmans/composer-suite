a7228a551042cd7c08d56d335514b593113cfbd2
Hendrik Mans <hendrik@mans.de>
2022-09-28 10:26:34 +0200

    Add explainers

--- apps/spacerage/src/scenes/gameplay/Bullets.tsx
+++ apps/spacerage/src/scenes/gameplay/Bullets.tsx
@@ -3,10 +3,12 @@ import { InstancedParticles, Particle, ParticleProps } from "vfx-composer-r3f"
 import { ECS } from "./state"
 
 export const Bullets = () => (
+  /* InstancedParticles is VFX Composer's wrapper around InstancedMesh. */
   <InstancedParticles capacity={200}>
     <planeGeometry args={[0.1, 0.8]} />
     <meshBasicMaterial color={new Color("yellow").multiplyScalar(2)} />
 
+    {/* This will automatically render the JSX of all bullets tagged with `isBullet`. */}
     <ECS.ArchetypeEntities archetype={["isBullet", "jsx"]}>
       {(entity) => entity.jsx}
     </ECS.ArchetypeEntities>
@@ -14,7 +16,11 @@ export const Bullets = () => (
 )
 
 export const Bullet = (props: ParticleProps) => (
+  /* When the bullet renders, it adds a `sceneObject` component to the entity.
+  The systems that simulates velocity needs this in order to move the bullet. */
   <ECS.Component name="sceneObject">
+    {/* The actual scene object. This is a controlled particle, ie. a scene object
+    that will continuously update the matrix of a specific instance. */}
     <Particle {...props} matrixAutoUpdate />
   </ECS.Component>
 )
