469f9a50ee25d756183ce00b4be52fa3471f72e3
Hendrik Mans <hendrik@mans.de>
2022-09-29 15:23:10 +0200

    Enginehum

--- apps/spacerage/src/scenes/gameplay/sfx/EngineHum.tsx
+++ apps/spacerage/src/scenes/gameplay/sfx/EngineHum.tsx
@@ -1,7 +1,10 @@
 import { useFrame } from "@react-three/fiber"
 import { useAudioComposer } from "audio-composer"
 import {
+  createContext,
   forwardRef,
+  ReactNode,
+  useContext,
   useImperativeHandle,
   useLayoutEffect,
   useMemo,
@@ -11,8 +14,10 @@ import { useStore } from "statery"
 import { AudioContext } from "three"
 import { ECS, gameplayStore } from "../state"
 
+const nodeContext = createContext<AudioNode>(null!)
+
 export type OscillatorProps = {
-  children?: JSX.Element
+  children?: ReactNode
   frequency?: number
   type?: OscillatorType
 }
@@ -21,13 +26,15 @@ export const Oscillator = forwardRef<OscillatorNode, OscillatorProps>(
   ({ type = "sine", frequency = 440, children }, ref) => {
     const audioCtx = AudioContext.getContext()
 
+    const parent = useContext(nodeContext)
+
     const oscillator = useMemo(() => {
       const time = audioCtx.currentTime
 
       const oscillator = audioCtx.createOscillator()
       oscillator.type = type
       oscillator.frequency.setValueAtTime(frequency, time)
-      oscillator.connect(audioCtx.destination)
+      oscillator.connect(parent || audioCtx.destination)
 
       return oscillator
     }, [audioCtx])
@@ -42,37 +49,57 @@ export const Oscillator = forwardRef<OscillatorNode, OscillatorProps>(
 
     useImperativeHandle(ref, () => oscillator)
 
-    return children as JSX.Element
+    return <>{children}</>
   }
 )
+
+export type GainNodeProps = {
+  children?: ReactNode
+}
+
+export const Gain = forwardRef<GainNode, GainNodeProps>(({ children }, ref) => {
+  const audioCtx = AudioContext.getContext()
+
+  const gainNode = useMemo(() => {
+    const gainNode = audioCtx.createGain()
+    gainNode.connect(audioCtx.destination)
+    gainNode.gain.setValueAtTime(0.3, audioCtx.currentTime)
+
+    return gainNode
+  }, [audioCtx])
+
+  return (
+    <nodeContext.Provider value={gainNode}>{children}</nodeContext.Provider>
+  )
+})
 
 export const EngineHum = () => {
   const osc1 = useRef<OscillatorNode>(null!)
   const osc2 = useRef<OscillatorNode>(null!)
   const osc3 = useRef<OscillatorNode>(null!)
 
-  const baseFrequency = 44
+  const baseFrequency = 22
 
   const [player] = ECS.useArchetype("player", "rigidBody")
 
   useFrame(() => {
-    if (!player || !osc1.current) return
+    if (!player) return
 
     osc1.current.frequency.value =
-      baseFrequency + player.rigidBody.linvel().length() * 2
+      baseFrequency + player.rigidBody.linvel().length() * 1
 
     osc2.current.frequency.value =
-      baseFrequency * 2 + player.rigidBody.linvel().length() * 3
+      baseFrequency * 2 + player.rigidBody.linvel().length() * 1.3
 
     osc3.current.frequency.value =
-      baseFrequency * 1.1 + player.rigidBody.linvel().length() * 4
+      baseFrequency * 1.1 + player.rigidBody.linvel().length() * 2
   })
 
   return (
-    <>
+    <Gain>
       <Oscillator type="triangle" frequency={baseFrequency} ref={osc1} />
       <Oscillator type="triangle" frequency={88} ref={osc2} />
       <Oscillator type="triangle" frequency={40} ref={osc3} />
-    </>
+    </Gain>
   )
 }
