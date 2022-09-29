89f7e81468ac0b6fc2325e54b0a43b09a553a141
Hendrik Mans <hendrik@mans.de>
2022-09-29 14:41:17 +0200

    Oscillate!

--- apps/spacerage/src/scenes/gameplay/sfx/EngineHum.tsx
+++ apps/spacerage/src/scenes/gameplay/sfx/EngineHum.tsx
@@ -1,38 +1,77 @@
 import { useAudioComposer } from "audio-composer"
 import { useLayoutEffect } from "react"
 
-export const EngineHum = () => {
+export type OscillatorProps = {
+  children?: JSX.Element
+  frequency?: number
+  type?: OscillatorType
+}
+
+export const Oscillator = ({
+  type = "sine",
+  frequency = 440,
+  children
+}: OscillatorProps) => {
   const { listener } = useAudioComposer()
 
   useLayoutEffect(() => {
     if (!listener) return
-
     const audioCtx = listener.context
     const time = audioCtx.currentTime
 
     const oscillator = audioCtx.createOscillator()
-    oscillator.type = "sawtooth"
-    oscillator.frequency.setValueAtTime(110, time)
-    // oscillator.connect(audioCtx.destination)
-    const sweepEnv = audioCtx.createGain()
-    const min = 0.1
-    const max = 0.5
+    oscillator.type = type
+    oscillator.frequency.setValueAtTime(frequency, time)
 
-    sweepEnv.gain.cancelScheduledValues(time)
-    sweepEnv.gain.setValueAtTime(min, time)
-    const rate = 0.2
-    for (let i = 0; i < 100000; i++) {
-      sweepEnv.gain.linearRampToValueAtTime(max, time + i * rate)
-      sweepEnv.gain.linearRampToValueAtTime(min, time + i * rate + rate / 2)
-    }
-
-    oscillator.connect(sweepEnv).connect(audioCtx.destination)
-    oscillator.start()
+    oscillator.connect(audioCtx.destination)
+    oscillator.start(time)
 
     return () => {
       oscillator.stop()
     }
   }, [listener])
 
-  return null
+  return children as JSX.Element
+}
+
+export const EngineHum = () => {
+  const { listener } = useAudioComposer()
+
+  // useLayoutEffect(() => {
+  //   if (!listener) return
+
+  //   const audioCtx = listener.context
+  //   const time = audioCtx.currentTime
+
+  //   const oscillator = audioCtx.createOscillator()
+  //   oscillator.type = "sawtooth"
+  //   oscillator.frequency.setValueAtTime(110, time)
+  //   // oscillator.connect(audioCtx.destination)
+  //   const sweepEnv = audioCtx.createGain()
+  //   const min = 0.1
+  //   const max = 0.5
+
+  //   sweepEnv.gain.cancelScheduledValues(time)
+  //   sweepEnv.gain.setValueAtTime(min, time)
+  //   const rate = 0.2
+  //   for (let i = 0; i < 100000; i++) {
+  //     sweepEnv.gain.linearRampToValueAtTime(max, time + i * rate)
+  //     sweepEnv.gain.linearRampToValueAtTime(min, time + i * rate + rate / 2)
+  //   }
+
+  //   oscillator.connect(sweepEnv).connect(audioCtx.destination)
+  //   oscillator.start()
+
+  //   return () => {
+  //     oscillator.stop()
+  //   }
+  // }, [listener])
+
+  return (
+    <>
+      <Oscillator type="sine" frequency={44} />
+      <Oscillator type="sine" frequency={88} />
+      <Oscillator type="sine" frequency={40} />
+    </>
+  )
 }
