cb3e3f7e74d01f3f4185ad5bb0de2912c4afda0b
Hendrik Mans <hendrik@mans.de>
2022-09-29 19:48:53 +0200

    Tweak

--- packages/audio-composer/src/AudioListener.tsx
+++ packages/audio-composer/src/AudioListener.tsx
@@ -9,22 +9,13 @@ export const AudioListener = () => {
 
   useLayoutEffect(() => {
     store.set({ listener: listener.current })
+
     return () => {
       store.set({ listener: null })
     }
   }, [listener])
 
   useLayoutEffect(() => {
-    if (!context) return
-
-    // const compressor = context.createDynamicsCompressor()
-    // compressor.threshold.setValueAtTime(-10, context.currentTime)
-    // compressor.knee.setValueAtTime(10, context.currentTime)
-    // compressor.ratio.setValueAtTime(12, context.currentTime)
-    // compressor.attack.setValueAtTime(0, context.currentTime)
-    // compressor.release.setValueAtTime(0.25, context.currentTime)
-    // listener.current.setFilter(compressor)
-
     // listener.current.setFilter(context)
     listener.current.gain.connect(context)
 
