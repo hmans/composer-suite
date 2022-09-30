52f86d193a1e809d5998972d8be4468b562a2be1
Hendrik Mans <hendrik@mans.de>
2022-09-30 11:09:45 +0200

    Add a simple start screen

--- apps/spacerage/src/App.tsx
+++ apps/spacerage/src/App.tsx
@@ -5,6 +5,7 @@ import { lazy, Suspense } from "react"
 import * as RC from "render-composer"
 import { PostProcessing } from "./common/PostProcessing"
 import { Stage } from "./configuration"
+import { StartScreen } from "./lib/StartScreen"
 import { useCapture } from "./lib/useCapture"
 import { GameState, store } from "./state"
 
@@ -12,11 +13,12 @@ const MenuScene = lazy(() => import("./scenes/menu/MenuScene"))
 const GameplayScene = lazy(() => import("./scenes/gameplay/GameplayScene"))
 
 export const App = () => (
-  <RC.Canvas dpr={1}>
-    <RC.RenderPipeline updatePriority={Stage.Render}>
-      <AC.AudioContext>
-        <AC.Compressor>
-          <AC.Reverb seconds={2} decay={5}>
+  <StartScreen>
+    <RC.Canvas dpr={1}>
+      <RC.RenderPipeline updatePriority={Stage.Render}>
+        <AC.AudioContext>
+          <AC.Compressor>
+            {/* <AC.Reverb seconds={2} decay={5}> */}
             <PostProcessing />
             <Suspense>
               <PerspectiveCamera
@@ -36,9 +38,10 @@ export const App = () => (
 
               <Perf matrixUpdate />
             </Suspense>
-          </AC.Reverb>
-        </AC.Compressor>
-      </AC.AudioContext>
-    </RC.RenderPipeline>
-  </RC.Canvas>
+            {/* </AC.Reverb> */}
+          </AC.Compressor>
+        </AC.AudioContext>
+      </RC.RenderPipeline>
+    </RC.Canvas>
+  </StartScreen>
 )
