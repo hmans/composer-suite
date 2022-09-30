3005f0b9eacb3c5a68922bc72056f9d2e477e9a8
Hendrik Mans <hendrik@mans.de>
2022-09-30 13:29:53 +0200

    Menu hum

--- apps/spacerage/src/scenes/menu/sfx/MenuDroneSFX.tsx
+++ apps/spacerage/src/scenes/menu/sfx/MenuDroneSFX.tsx
@@ -1,39 +1,14 @@
-import { Filter, Gain, LinearRamp, Oscillator } from "audio-composer"
+import { Filter, Gain, LinearRamp, Oscillator, Reverb } from "audio-composer"
 import { Delay, Repeat } from "timeline-composer"
 
 export const MenuDroneSFX = () => (
-  <Gain volume={0.2}>
-    <Filter type="bandpass" frequency={50} Q={5}>
-      {/* Oscillate the filter's Q property... a lot */}
-      <Gain target="Q" volume={5}>
-        <Oscillator frequency={3} type="sine">
-          {/* Oscillate the the oscillator's oscillation. IT'S OSCILLEPTION */}
-          <Gain target="frequency" volume={5}>
-            <Oscillator frequency={1} type="sawtooth" />
-          </Gain>
-        </Oscillator>
-      </Gain>
-
-      {/* The actual sound. */}
-      <Oscillator frequency={55} type="sawtooth" />
-
-      <Oscillator frequency={440} type="sawtooth" detune={50}>
-        <Repeat seconds={20}>
-          <LinearRamp property="frequency" duration={10} to={110} />
-          <Delay seconds={10}>
-            <LinearRamp property="frequency" duration={10} to={440} />
-          </Delay>
-        </Repeat>
-      </Oscillator>
-
-      <Oscillator frequency={110} type="sawtooth" detune={-50}>
-        <Repeat seconds={20}>
-          <LinearRamp property="frequency" duration={10} to={440} />
-          <Delay seconds={10}>
-            <LinearRamp property="frequency" duration={10} to={110} />
-          </Delay>
-        </Repeat>
-      </Oscillator>
-    </Filter>
+  <Gain volume={0.1}>
+    <Reverb>
+      <Filter type="lowpass" frequency={100}>
+        <Oscillator type="sawtooth" frequency={55} />
+        <Oscillator type="sawtooth" frequency={55} detune={10} />
+        <Oscillator type="sawtooth" frequency={55} detune={-10} />
+      </Filter>
+    </Reverb>
   </Gain>
 )
