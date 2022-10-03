05fbff60197bb0f178c1a019ecefc25528fdba7f
Hendrik Mans <hendrik@mans.de>
2022-10-03 19:54:38 +0200

    Cleanup

--- apps/spacerage/src/scenes/gameplay/vfx/AsteroidExplosions.tsx
+++ apps/spacerage/src/scenes/gameplay/vfx/AsteroidExplosions.tsx
@@ -1,22 +1,9 @@
 import { GroupProps, useLoader } from "@react-three/fiber"
 import { PositionalAudio } from "audio-composer"
-import { flow } from "fp-ts/lib/function"
 import { Composable, Modules } from "material-composer-r3f"
-import { ModuleRegistrationContext } from "material-composer-r3f/src/moduleRegistration"
-import { Module } from "module"
-import { between, upTo } from "randomish"
-import { useLayoutEffect } from "react"
-import {
-  Add,
-  Mul,
-  Normalize,
-  Remap,
-  ScaleAndOffset,
-  Smoothstep,
-  Vec3
-} from "shader-composer"
-import { rand } from "shader-composer-toybox/src/noise/random"
-import { AudioLoader, Color } from "three"
+import { between } from "randomish"
+import { Mul, ScaleAndOffset, Smoothstep, Vec3 } from "shader-composer"
+import { AudioLoader } from "three"
 import { createParticleLifetime } from "vfx-composer"
 import { Emitter, InstancedParticles } from "vfx-composer-r3f"
 import { InstanceRNG } from "../../../lib/InstanceRNG"
