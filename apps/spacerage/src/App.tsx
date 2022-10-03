30b225d7614f19cd6464737919feca55f251aac7
Hendrik Mans <hendrik@mans.de>
2022-10-03 20:30:18 +0200

    Add UI Composer to Spacerage

--- apps/spacerage/src/App.tsx
+++ apps/spacerage/src/App.tsx
@@ -11,6 +11,7 @@ import { input } from "./input"
 import { StartScreen } from "./lib/StartScreen"
 import { useCapture } from "./lib/useCapture"
 import { GameState, store } from "./state"
+import * as UI from "ui-composer"
 
 const MenuScene = lazy(() => import("./scenes/menu/MenuScene"))
 const GameplayScene = lazy(() => import("./scenes/gameplay/GameplayScene"))
@@ -21,41 +22,110 @@ export const App = () => {
 
   return (
     <StartScreen>
-      <RC.Canvas dpr={1}>
-        <RC.RenderPipeline updatePriority={Stage.Render}>
-          <AC.AudioContext>
-            <AC.Compressor>
-              {/* <AC.Reverb seconds={2} decay={5}> */}
-              <PostProcessing />
-              <Suspense>
-                <PerspectiveCamera
-                  position={[0, 0, 20]}
-                  rotation-y={-0.8}
-                  makeDefault
-                  ref={useCapture(store, "camera")}
-                >
-                  <AudioListener />
-                </PerspectiveCamera>
+      <UI.Root>
+        <UI.HorizontalGroup>
+          <div style={{ flex: 2 }}>
+            <RC.Canvas dpr={1}>
+              <RC.RenderPipeline updatePriority={Stage.Render}>
+                <AC.AudioContext>
+                  <AC.Compressor>
+                    {/* <AC.Reverb seconds={2} decay={5}> */}
+                    <PostProcessing />
+                    <Suspense>
+                      <PerspectiveCamera
+                        position={[0, 0, 20]}
+                        rotation-y={-0.8}
+                        makeDefault
+                        ref={useCapture(store, "camera")}
+                      >
+                        <AudioListener />
+                      </PerspectiveCamera>
 
-                <GameState.Match state="menu">
-                  <Suspense>
-                    <MenuScene />
-                  </Suspense>
-                </GameState.Match>
+                      <GameState.Match state="menu">
+                        <Suspense>
+                          <MenuScene />
+                        </Suspense>
+                      </GameState.Match>
 
-                <GameState.Match state="gameplay">
-                  <Suspense>
-                    <GameplayScene />
-                  </Suspense>
-                </GameState.Match>
+                      <GameState.Match state="gameplay">
+                        <Suspense>
+                          <GameplayScene />
+                        </Suspense>
+                      </GameState.Match>
 
-                <Perf matrixUpdate />
-              </Suspense>
-              {/* </AC.Reverb> */}
-            </AC.Compressor>
-          </AC.AudioContext>
-        </RC.RenderPipeline>
-      </RC.Canvas>
+                      {/* <Perf matrixUpdate /> */}
+                    </Suspense>
+                    {/* </AC.Reverb> */}
+                  </AC.Compressor>
+                </AC.AudioContext>
+              </RC.RenderPipeline>
+            </RC.Canvas>
+          </div>
+
+          <UI.HorizontalResizer />
+
+          <UI.VerticalGroup css={{ flex: 1 }}>
+            <UI.Panel>
+              <UI.Heading>Welcome!</UI.Heading>
+              <p>
+                This is a panel. It displays things. Amazing! Many curious,
+                interesting things, that hopefully will make building editor UIs
+                really useful. (This is just a slightly longer paragraph to see
+                how it renders.)
+              </p>
+              <p>Like text.</p>
+              <p>Or even more text.</p>
+            </UI.Panel>
+
+            <UI.Panel>
+              <UI.Heading>Buttons</UI.Heading>
+              <p>Buttonnnnssss, they're amazing!</p>
+
+              <UI.VerticalGroup>
+                <UI.Button>Click me</UI.Button>
+                <UI.Button>Or click this one</UI.Button>
+                <UI.HorizontalGroup gap>
+                  <UI.Button>Left</UI.Button>
+                  <UI.Button>Right</UI.Button>
+                </UI.HorizontalGroup>
+              </UI.VerticalGroup>
+            </UI.Panel>
+
+            <UI.Panel>
+              <UI.Heading>Inputs</UI.Heading>
+              <p>We should try some inputs. Inputs are really cool.</p>
+
+              <UI.Control>
+                <UI.Label>Text</UI.Label>
+                <UI.Input type="text" spellCheck="false" />
+              </UI.Control>
+
+              <UI.Control>
+                <UI.Label>Number</UI.Label>
+                <UI.Input type="number" />
+              </UI.Control>
+
+              <UI.Control>
+                <UI.Label>Range</UI.Label>
+                <UI.Input type="range" />
+              </UI.Control>
+
+              <UI.Control>
+                <UI.Label>Vector</UI.Label>
+
+                <UI.HorizontalGroup align={"center"} gap>
+                  X
+                  <UI.Input type="number" />
+                  Y
+                  <UI.Input type="number" />
+                  Z
+                  <UI.Input type="number" />
+                </UI.HorizontalGroup>
+              </UI.Control>
+            </UI.Panel>
+          </UI.VerticalGroup>
+        </UI.HorizontalGroup>
+      </UI.Root>
     </StartScreen>
   )
 }
