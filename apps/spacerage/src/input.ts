794c12cfa61b6d0457dac513d1eb636d5250b165
Hendrik Mans <hendrik@mans.de>
2022-10-04 19:20:54 +0200

    Deadzone

--- apps/spacerage/src/input.ts
+++ apps/spacerage/src/input.ts
@@ -132,6 +132,10 @@ class Stick extends AbstractControl implements IVector {
   x = 0
   y = 0
 
+  length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y)
+  }
+
   apply(vector: IVector) {
     this.x = vector.x
     this.y = vector.y
@@ -139,7 +143,7 @@ class Stick extends AbstractControl implements IVector {
   }
 
   normalize() {
-    const length = Math.sqrt(this.x * this.x + this.y * this.y)
+    const length = this.length()
     if (length > 0) {
       this.x /= length
       this.y /= length
@@ -148,11 +152,26 @@ class Stick extends AbstractControl implements IVector {
   }
 
   clampLength(limit = 1) {
-    const length = Math.sqrt(this.x * this.x + this.y * this.y)
+    const length = this.length()
     if (length > limit) {
       this.x /= length
       this.y /= length
     }
+    return this
+  }
+
+  deadzone(threshold = 0.15) {
+    const length = this.length()
+
+    if (length < threshold) {
+      this.x = 0
+      this.y = 0
+    } else {
+      this.normalize()
+      this.x = (this.x * (length - threshold)) / (1 - threshold)
+      this.y = (this.y * (length - threshold)) / (1 - threshold)
+    }
+
     return this
   }
 
@@ -251,8 +270,8 @@ class Controller extends AbstractController {
     }
 
     /* Do additional processing */
-    move.clampLength()
-    aim.clampLength()
+    move.clampLength().deadzone(0.1)
+    aim.clampLength().deadzone(0.1)
   }
 }
 
