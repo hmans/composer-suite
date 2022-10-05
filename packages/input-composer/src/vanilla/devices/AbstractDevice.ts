a41a5379187cba336995a450a9e2029ed3336cea
Hendrik Mans <hendrik@mans.de>
2022-10-05 14:17:18 +0200

    Extract

---
+++ packages/input-composer/src/vanilla/devices/AbstractDevice.ts
@@ -0,0 +1,9 @@
+import { Event } from "../../lib/event"
+
+export abstract class AbstractDevice {
+  onActivity = new Event()
+
+  abstract start(): void
+  abstract stop(): void
+  abstract update(): void
+}
