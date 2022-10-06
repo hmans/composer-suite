0523c9dc9bacfe0de189265bbf8f3b06ad946e35
Hendrik Mans <hendrik@mans.de>
2022-10-06 17:09:36 +0200

    Extract game

--- apps/spacerage/src/App.tsx
+++ apps/spacerage/src/App.tsx
@@ -1,31 +1,11 @@
-import { PerspectiveCamera } from "@react-three/drei"
-import * as AC from "audio-composer"
-import { AudioListener } from "audio-composer"
-import { lazy, Suspense } from "react"
-import * as RC from "render-composer"
 import * as UI from "ui-composer"
-import { PostProcessing } from "./common/PostProcessing"
-import { Stage } from "./configuration"
-import { controller } from "./input"
 import { StartScreen } from "./lib/StartScreen"
-import { useCapture } from "./lib/useCapture"
-import { GameState, SidebarTunnel, store } from "./state"
+import { GameState, SidebarTunnel } from "./state"
 
 /* We need to make sure that this file imports _something_ from @react-three/fiber
 because otherwise Vite gets confused. :( */
 import "@react-three/fiber"
-import { useFrame } from "@react-three/fiber"
-
-const MenuScene = lazy(() => import("./scenes/menu/MenuScene"))
-const GameplayScene = lazy(() => import("./scenes/gameplay/GameplayScene"))
-
-const Controller = () => {
-  useFrame(() => {
-    controller.update()
-  }, Stage.Early)
-
-  return null
-}
+import { Game } from "./Game"
 
 export const App = () => {
   return (
@@ -33,42 +13,7 @@ export const App = () => {
       <UI.Root>
         <UI.HorizontalGroup>
           <div style={{ flex: 2 }}>
-            <RC.Canvas dpr={1}>
-              <Controller />
-              <RC.RenderPipeline updatePriority={Stage.Render}>
-                <AC.AudioContext>
-                  <AC.Compressor>
-                    {/* <AC.Reverb seconds={2} decay={5}> */}
-                    <PostProcessing />
-                    <Suspense>
-                      <PerspectiveCamera
-                        position={[0, 0, 20]}
-                        rotation-y={-0.8}
-                        makeDefault
-                        ref={useCapture(store, "camera")}
-                      >
-                        <AudioListener />
-                      </PerspectiveCamera>
-
-                      <GameState.Match state="menu">
-                        <Suspense>
-                          <MenuScene />
-                        </Suspense>
-                      </GameState.Match>
-
-                      <GameState.Match state="gameplay">
-                        <Suspense>
-                          <GameplayScene />
-                        </Suspense>
-                      </GameState.Match>
-
-                      {/* <Perf matrixUpdate /> */}
-                    </Suspense>
-                    {/* </AC.Reverb> */}
-                  </AC.Compressor>
-                </AC.AudioContext>
-              </RC.RenderPipeline>
-            </RC.Canvas>
+            <Game />
           </div>
 
           <UI.HorizontalResizer />
---
+++ apps/spacerage/src/Game.tsx
@@ -0,0 +1,61 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import * as AC from "audio-composer"
+import { lazy, Suspense } from "react"
+import * as RC from "render-composer"
+import { PostProcessing } from "./common/PostProcessing"
+import { Stage } from "./configuration"
+import { controller } from "./input"
+import { useCapture } from "./lib/useCapture"
+import { GameState, store } from "./state"
+const MenuScene = lazy(() => import("./scenes/menu/MenuScene"))
+const GameplayScene = lazy(() => import("./scenes/gameplay/GameplayScene"))
+
+const Controller = () => {
+  useFrame(() => {
+    controller.update()
+  }, Stage.Early)
+
+  return null
+}
+
+export const Game = () => {
+  return (
+    <RC.Canvas dpr={1}>
+      <Controller />
+      <RC.RenderPipeline updatePriority={Stage.Render}>
+        <AC.AudioContext>
+          <AC.Compressor>
+            {/* <AC.Reverb seconds={2} decay={5}> */}
+            <PostProcessing />
+            <Suspense>
+              <PerspectiveCamera
+                position={[0, 0, 20]}
+                rotation-y={-0.8}
+                makeDefault
+                ref={useCapture(store, "camera")}
+              >
+                <AC.AudioListener />
+              </PerspectiveCamera>
+
+              <GameState.Match state="menu">
+                <Suspense>
+                  <MenuScene />
+                </Suspense>
+              </GameState.Match>
+
+              <GameState.Match state="gameplay">
+                <Suspense>
+                  <GameplayScene />
+                </Suspense>
+              </GameState.Match>
+
+              {/* <Perf matrixUpdate /> */}
+            </Suspense>
+            {/* </AC.Reverb> */}
+          </AC.Compressor>
+        </AC.AudioContext>
+      </RC.RenderPipeline>
+    </RC.Canvas>
+  )
+}
