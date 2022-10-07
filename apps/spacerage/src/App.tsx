6edd0868e57b8cafb2b281da13d4e0679bbafd3c
Hendrik Mans <hendrik@mans.de>
2022-10-07 16:49:54 +0200

    Remove that old hack

--- apps/spacerage/src/App.tsx
+++ apps/spacerage/src/App.tsx
@@ -1,9 +1,5 @@
 import * as UI from "ui-composer"
 import { StartScreen } from "./lib/StartScreen"
-
-/* We need to make sure that this file imports _something_ from @react-three/fiber
-because otherwise Vite gets confused. :( */
-import "@react-three/fiber"
 import { Game } from "./Game"
 import { Sidebar } from "./editor/Sidebar"
 import { useState } from "react"
