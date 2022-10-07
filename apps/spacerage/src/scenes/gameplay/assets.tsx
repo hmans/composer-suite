0632fc02af41af3f26722694554d97a353984868
Hendrik Mans <hendrik@mans.de>
2022-10-07 17:24:14 +0200

    Remove file not found warning

--- apps/spacerage/src/scenes/gameplay/assets.tsx
+++ apps/spacerage/src/scenes/gameplay/assets.tsx
@@ -5,7 +5,7 @@ import { createLoader } from "@hmans/r3f-create-loader"
 /* Use in project, in eg. `assets.ts` */
 export const useAsset = {
   asteroid: createLoader(GLTFLoader, "/models/asteroid03.gltf"),
-  playerShip: createLoader(GLTFLoader, "/models/ship01.gltf"),
+  playerShip: createLoader(GLTFLoader, "/models/spaceship25.gltf"),
   music: createLoader(AudioLoader, "/sounds/taikobeat.mp3"),
   textures: {
     smoke: createLoader(TextureLoader, "/textures/smoke.png"),
