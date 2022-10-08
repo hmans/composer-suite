9cdb2c6bd9b47f68cf13fda3a3d966542cf3deff
Hendrik Mans <hendrik@mans.de>
2022-10-08 19:11:09 +0200

    Make anchors work

--- apps/spacerage/src/scenes/gameplay/HUD.tsx
+++ apps/spacerage/src/scenes/gameplay/HUD.tsx
@@ -1,5 +1,7 @@
 import { PerspectiveCamera } from "@react-three/drei"
 import { GroupProps } from "@react-three/fiber"
+import { number } from "randomish"
+import { createContext, useContext } from "react"
 import { ScenePass } from "../../lib/ScenePass"
 
 export const HUD = () => {
@@ -7,12 +9,25 @@ export const HUD = () => {
     <ScenePass>
       <PerspectiveCamera position={[0, 0, 20]} makeDefault />
 
-      <UIPanel width={4} height={2}>
-        <UIPanel width={1} height={1} pivotY={0.5} />
+      <UIPanel width={4} height={2} position={[-4, 3, 0]}>
+        <UIPanel
+          width={1}
+          height={1}
+          position-x={0.1}
+          position-y={-0.1}
+          pivotX={0}
+          anchorX={0}
+          pivotY={1}
+          anchorY={1}
+        />
       </UIPanel>
     </ScenePass>
   )
 }
+
+export const PanelContext = createContext<{ width: number; height: number }>(
+  null!
+)
 
 export type UIPanelProps = GroupProps & {
   anchorX?: number
@@ -33,19 +48,29 @@ export const UIPanel = ({
   children,
   ...props
 }: UIPanelProps) => {
+  const parent = useContext(PanelContext)
+
+  const offsetX = parent ? parent.width * (anchorX - 0.5) : 0
+  const offsetY = parent ? parent.height * (anchorY - 0.5) : 0
+
   return (
     <group {...props}>
-      <OriginMarker />
+      {/* Apply anchor offset */}
+      <group position={[offsetX, offsetY, 0]}>
+        <OriginMarker />
 
-      {/* Apply pivot */}
-      <group position={[0.5 - pivotX, 0.5 - pivotY, 0]}>
-        {/* Visualize the canvas */}
-        <mesh scale={[width, height, 1]}>
-          <planeGeometry />
-          <meshBasicMaterial color="white" transparent opacity={0.2} />
-        </mesh>
+        {/* Apply pivot */}
+        <group position={[0.5 - pivotX, 0.5 - pivotY, 0]}>
+          {/* Visualize the canvas */}
+          <mesh scale={[width, height, 1]}>
+            <planeGeometry />
+            <meshBasicMaterial color="white" transparent opacity={0.2} />
+          </mesh>
 
-        {children}
+          <PanelContext.Provider value={{ width, height }}>
+            {children}
+          </PanelContext.Provider>
+        </group>
       </group>
     </group>
   )
