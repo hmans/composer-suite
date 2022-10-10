940bf907451aae501f4d719c04f9ed5360d38a20
Hendrik Mans <hendrik@mans.de>
2022-10-10 16:34:41 +0200

    More tests

--- packages/entity-composer/test/bucket.test.ts
+++ packages/entity-composer/test/bucket.test.ts
@@ -53,3 +53,35 @@ describe("write", () => {
     expect(listener).toHaveBeenCalledTimes(1)
   })
 })
+
+describe("remove", () => {
+  it("removes an entity from the bucket", () => {
+    const entity = { count: 1 }
+    const bucket = createBucket()
+
+    bucket.write(entity)
+    expect(bucket.entities).toEqual([entity])
+
+    bucket.remove(entity)
+    expect(bucket.entities).toEqual([])
+  })
+
+  it("is idempotent", () => {
+    const entity = { count: 1 }
+    const bucket = createBucket()
+    bucket.write(entity)
+    bucket.remove(entity)
+    bucket.remove(entity)
+    expect(bucket.entities).toEqual([])
+  })
+
+  it("emits an event when the entity is removed", () => {
+    const bucket = createBucket<{ count: number }>()
+    const listener = jest.fn()
+    bucket.onEntityRemoved.addListener(listener)
+
+    const entity = bucket.write({ count: 1 })
+    bucket.remove(entity)
+    expect(listener).toHaveBeenCalledWith(entity)
+  })
+})
