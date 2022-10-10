37ff60a56c8f73b9c100eaecea2b5b00deb2421e
Hendrik Mans <hendrik@mans.de>
2022-10-10 16:59:45 +0200

    Update with payload

--- packages/entity-composer/src/bucket.ts
+++ packages/entity-composer/src/bucket.ts
@@ -9,14 +9,30 @@ export const createBucket = <E extends IEntity>() => {
 
   const onEntityAdded = new Event<E>()
   const onEntityRemoved = new Event<E>()
+  const onEntityChanged = new Event<E>()
 
-  const write = (entity: E) => {
+  const write = (entity: E, update?: Partial<E> | ((entity: E) => E)) => {
     const index = entities.indexOf(entity)
 
     /* Add the entity if we don't already have it */
     if (index === -1) {
       entities.push(entity)
       onEntityAdded.emit(entity)
+    }
+
+    if (update) {
+      const changes = typeof update === "function" ? update(entity) : update
+
+      /* Check if the update actually changed anything */
+      let updated = false
+      for (const key in changes)
+        if (entity[key] !== changes[key]) updated = true
+
+      /* Update the entity */
+      Object.assign(entity, changes)
+
+      /* Emit an event if the entity changed */
+      if (updated) onEntityChanged.emit(entity)
     }
 
     return entity
@@ -35,5 +51,12 @@ export const createBucket = <E extends IEntity>() => {
     onEntityRemoved.emit(entity)
   }
 
-  return { entities, onEntityAdded, onEntityRemoved, write, remove }
+  return {
+    entities,
+    onEntityAdded,
+    onEntityRemoved,
+    onEntityChanged,
+    write,
+    remove
+  }
 }
--- packages/entity-composer/test/bucket.test.ts
+++ packages/entity-composer/test/bucket.test.ts
@@ -53,6 +53,35 @@ describe("write", () => {
     bucket.write(entity)
     expect(listener).toHaveBeenCalledTimes(1)
   })
+
+  describe("with an update payload", () => {
+    it("updates the entity", () => {
+      const bucket = createBucket<{ count: number }>()
+      const entity = bucket.write({ count: 1 })
+      bucket.write(entity, { count: 2 })
+      expect(bucket.entities).toEqual([{ count: 2 }])
+    })
+
+    it("emits an event when an entity is updated", () => {
+      const bucket = createBucket<{ count: number }>()
+      const listener = jest.fn()
+      bucket.onEntityChanged.addListener(listener)
+
+      const entity = bucket.write({ count: 1 })
+      bucket.write(entity, { count: 2 })
+      expect(listener).toHaveBeenCalledWith(entity)
+    })
+
+    it("does not emit an event when an entity is updated with the same values", () => {
+      const bucket = createBucket<{ count: number }>()
+      const listener = jest.fn()
+      bucket.onEntityChanged.addListener(listener)
+
+      const entity = bucket.write({ count: 1 })
+      bucket.write(entity, { count: 1 })
+      expect(listener).not.toHaveBeenCalled()
+    })
+  })
 })
 
 describe("remove", () => {
