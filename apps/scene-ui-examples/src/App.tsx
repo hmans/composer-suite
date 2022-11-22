dd0354f824e3689f74f085ce44c6f590d20674d4
Hendrik Mans <hendrik@mans.de>
2022-11-22 09:19:39 +0100

    Commit

--- apps/scene-ui-examples/src/App.tsx
+++ apps/scene-ui-examples/src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
     <Application>
       <UI.Rect width={5} height={5} debug>
         <UI.Rect
-          width={3}
+          width={4}
           height={1}
           pivotX={1}
           pivotY={1}
