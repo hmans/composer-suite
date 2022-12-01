6e2652237aaf2aab55f78f343f9754ec03835b35
Hendrik Mans <hendrik@mans.de>
2022-12-01 08:57:18 +0100

    Docs

--- packages/shader-composer-core/src/ticker.ts
+++ packages/shader-composer-core/src/ticker.ts
@@ -1,5 +1,16 @@
+/**
+ * An object containing information about the current frame.
+ */
 export const frame = {
+  /**
+   * The current frame time, in seconds. This value does not change
+   * over the course of a single frame.
+   */
   time: performance.now() / 1000,
+
+  /**
+   * The current frame count.
+   */
   count: 0
 }
 
